bdebd068755b6a985110c89cd5431672b54c9a21
braverock <braverock>
2003-11-24 17:03:49 +0000

    Initial revision

---
+++ admin/campaign-statuses/one.php
@@ -0,0 +1,101 @@
+<?php
+
+require_once('../../include-locations.inc');
+require_once($include_directory . 'vars.php');
+require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-misc.php');
+require_once($include_directory . 'adodb/adodb.inc.php');
+
+$session_user_id = session_check();
+
+$campaign_status_id = $_GET['campaign_status_id'];
+
+$con = &adonewconnection($xrms_db_dbtype);
+$con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+
+$sql = "select * from campaign_statuses where campaign_status_id = $campaign_status_id";
+
+$rst = $con->execute($sql);
+
+if ($rst) {
+
+    $campaign_status_short_name = $rst->fields['campaign_status_short_name'];
+    $campaign_status_pretty_name = $rst->fields['campaign_status_pretty_name'];
+    $campaign_status_pretty_plural = $rst->fields['campaign_status_pretty_plural'];
+    $campaign_status_display_html = $rst->fields['campaign_status_display_html'];
+
+    $rst->close();
+}
+
+$con->close();
+
+$page_title = "One Campaign Status : $campaign_status_pretty_name";
+start_page($page_title);
+
+?>
+
+<table border=0 cellpadding=0 cellspacing=0 width=100%>
+    <tr>
+        <td class=lcol width=25% valign=top>
+
+        <form action=edit-2.php method=post>
+        <input type=hidden name=campaign_status_id value="<?php  echo $account_status_id; ?>">
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=4>Edit Campaign Status Information</td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Short Name</td>
+                <td class=widget_content_form_element><input type=text size=10 name=campaign_status_short_name value="<?php  echo $campaign_status_short_name; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Name</td>
+                <td class=widget_content_form_element><input type=text size=20 name=campaign_status_pretty_name value="<?php  echo $campaign_status_pretty_name; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Plural</td>
+                <td class=widget_content_form_element><input type=text size=20 name=campaign_status_pretty_plural value="<?php  echo $campaign_status_pretty_plural; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Display HTML</td>
+                <td class=widget_content_form_element><input type=text size=30 name=campaign_status_display_html value="<?php  echo $campaign_status_display_html; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_content_form_element colspan=2><input class=button type=submit value="Save Changes"></td>
+            </tr>
+        </table>
+        </form>
+
+        <form action=delete.php method=post>
+        <input type=hidden name=campaign_status_id value="<?php  echo $campaign_status_id; ?>" onsubmit="javascript: return confirm('Delete Opportunity Status?');">
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=4>Delete Opportunity Status</td>
+            </tr>
+            <tr>
+                <td class=widget_content>
+                Click the button below to remove this account status from the system.
+                <p>Note: This action CANNOT be undone!
+                <p><input class=button type=submit value="Delete Opportunity Status">
+                </td>
+            </tr>
+        </table>
+        </form>
+
+        </td>
+
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+
+        <!-- right column //-->
+
+        <td class=rcol width=73% valign=top>
+        &nbsp;
+        </td>
+
+    </tr>
+</table>
+
+<?php end_page(); ?>
---
+++ admin/company-sources/some.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once('../../include-locations.inc');
+
+require_once($include_directory . 'vars.php');
+require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-misc.php');
+require_once($include_directory . 'adodb/adodb.inc.php');
+
+$session_user_id = session_check();
+
+$con = &adonewconnection($xrms_db_dbtype);
+$con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+
+$sql = "select * from company_sources where company_source_record_status = 'a' order by company_source_pretty_name";
+$rst = $con->execute($sql);
+
+if ($rst) {
+    while (!$rst->EOF) {
+        $table_rows .= '<tr>';
+        $table_rows .= '<td class=widget_content><a href=one.php?company_source_id=' . $rst->fields['company_source_id'] . '>' . $rst->fields['company_source_pretty_name'] . '</a></td>';
+        $table_rows .= '</tr>';
+        $rst->movenext();
+    }
+    $rst->close();
+}
+
+$con->close();
+
+$page_title = "Manage Company Sources";
+start_page($page_title);
+
+?>
+
+<table border=0 cellpadding=0 cellspacing=0 width=100%>
+    <tr>
+        <td class=lcol width=65% valign=top>
+
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=4>Company Sources</td>
+            </tr>
+            <tr>
+                <td class=widget_label>Name</td>
+            </tr>
+            <?php  echo $table_rows; ?>
+        </table>
+
+        </td>
+
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+
+        <!-- right column //-->
+
+        <td class=rcol width=33% valign=top>
+
+        <form action="add-2.php" method=post>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=2>Add New Priority Type</td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Short Name</td>
+                <td class=widget_content_form_element><input type=text name=company_source_short_name size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Name</td>
+                <td class=widget_content_form_element><input type=text name=company_source_pretty_name size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Plural Name</td>
+                <td class=widget_content_form_element><input type=text name=company_source_pretty_plural size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Display HTML</td>
+                <td class=widget_content_form_element><input type=text name=company_source_display_html size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Score Adjustment</td>
+                <td class=widget_content_form_element><input type=text name=company_source_score_adjustment size=5></td>
+            </tr>
+            <tr>
+                <td class=widget_content_form_element colspan=2><input class=button type=submit value="Add"></td>
+            </tr>
+        </table>
+        </form>
+
+        </td>
+    </tr>
+</table>
+
+<?php end_page(); ?>
