f9acf5599f9e85c1487809d4e252816f18c3e096
cdwtech <cdwtech>
2003-11-28 20:00:38 +0000

    added missing delete-address page and removed old address columns from companies.sql

---
+++ companies/delete-address.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('../include-locations.inc');
+
+require_once($include_directory . 'vars.php');
+require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-misc.php');
+require_once($include_directory . 'adodb/adodb.inc.php');
+require_once($include_directory . 'utils-accounting.php');
+
+$session_user_id = session_check();
+
+$company_id = $_GET['company_id'];
+$address_id = $_GET['address_id'];
+
+$con = &adonewconnection($xrms_db_dbtype);
+$con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+
+$sql = "update addresses set address_record_status = 'd'";
+$con->execute($sql);
+
+$con->close();
+
+header("Location: addresses.php?msg=address_deleted&company_id=$company_id");
+
+?>
