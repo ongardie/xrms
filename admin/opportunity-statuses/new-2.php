93eb2b9dce871fb18d43013be0e51d437123ced2
braverock <braverock>
2003-11-29 15:14:05 +0000

    - changed referenced to account_status to opportunity_status - added long description field

--- admin/opportunity-statuses/new-2.php
+++ admin/opportunity-statuses/new-2.php
@@ -8,15 +8,16 @@ require_once($include_directory . 'adodb/adodb.inc.php');
 
 $session_user_id = session_check();
 
-$account_status_short_name = $_POST['account_status_short_name'];
-$account_status_pretty_name = $_POST['account_status_pretty_name'];
-$account_status_pretty_plural = $_POST['account_status_pretty_plural'];
-$account_status_display_html = $_POST['account_status_display_html'];
+$opportunity_status_short_name = $_POST['opportunity_status_short_name'];
+$opportunity_status_pretty_name = $_POST['opportunity_status_pretty_name'];
+$opportunity_status_pretty_plural = $_POST['opportunity_status_pretty_plural'];
+$opportunity_status_display_html = $_POST['opportunity_status_display_html'];
+$opportunity_status_long_desc = $_POST['opportunity_status_long_desc'];
 
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 
-$sql = "insert into opportunity_statuses (opportunity_status_short_name, opportunity_status_pretty_name, opportunity_status_pretty_plural, opportunity_status_display_html) values (" . $con->qstr($opportunity_status_short_name, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_pretty_name, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_pretty_plural, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_display_html, get_magic_quotes_gpc()) . ")";
+$sql = "insert into opportunity_statuses (opportunity_status_short_name, opportunity_status_pretty_name, opportunity_status_pretty_plural, opportunity_status_display_html, opportunity_status_display_html) values (" . $con->qstr($opportunity_status_short_name, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_pretty_name, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_pretty_plural, get_magic_quotes_gpc()) . ", " . $con->qstr($opportunity_status_display_html, get_magic_quotes_gpc()). ", " . $con->qstr($opportunity_status_long_desc, get_magic_quotes_gpc()) . ")";
 $con->execute($sql);
 
 $con->close();
