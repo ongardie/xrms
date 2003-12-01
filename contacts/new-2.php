83a286e741aaa3a963ca47773a5fe30612d762d7
cdwtech <cdwtech>
2003-12-01 13:41:13 +0000

    now adds address correctly

--- contacts/new-2.php
+++ contacts/new-2.php
@@ -10,6 +10,7 @@ require_once($include_directory . 'adodb/adodb.inc.php');
 $session_user_id = session_check();
 
 $company_id = $_POST['company_id'];
+$address_id = $_POST['address_id'];
 $last_name = $_POST['last_name'];
 $first_names = $_POST['first_names'];
 $summary = $_POST['summary'];
@@ -31,7 +32,7 @@ $first_names = (strlen($first_names) > 0) ? $first_names : "[first names]";
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 
-$sql = "insert into contacts (company_id, last_name, first_names, summary, title, description, email, work_phone, cell_phone, home_phone, aol_name, yahoo_name, msn_name, interests, entered_at, entered_by, last_modified_at, last_modified_by) values ($company_id, " . $con->qstr($last_name, get_magic_quotes_gpc()) . ", " . $con->qstr($first_names, get_magic_quotes_gpc()) . ", " . $con->qstr($summary, get_magic_quotes_gpc()) . ", " . $con->qstr($title, get_magic_quotes_gpc()) . ", " . $con->qstr($description, get_magic_quotes_gpc()) . ", " . $con->qstr($email, get_magic_quotes_gpc()) . ", " . $con->qstr($work_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($cell_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($home_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($aol_name, get_magic_quotes_gpc()) . ", " . $con->qstr($yahoo_name, get_magic_quotes_gpc()) . ", " . $con->qstr($msn_name, get_magic_quotes_gpc()) . ", " . $con->qstr($interests, get_magic_quotes_gpc()) . ", " . $con->dbtimestamp(mktime()) . ", $session_user_id, " . $con->dbtimestamp(mktime()) . ", $session_user_id)";
+$sql = "insert into contacts (company_id, address_id, last_name, first_names, summary, title, description, email, work_phone, cell_phone, home_phone, aol_name, yahoo_name, msn_name, interests, entered_at, entered_by, last_modified_at, last_modified_by) values ($company_id, $address_id, " . $con->qstr($last_name, get_magic_quotes_gpc()) . ", " . $con->qstr($first_names, get_magic_quotes_gpc()) . ", " . $con->qstr($summary, get_magic_quotes_gpc()) . ", " . $con->qstr($title, get_magic_quotes_gpc()) . ", " . $con->qstr($description, get_magic_quotes_gpc()) . ", " . $con->qstr($email, get_magic_quotes_gpc()) . ", " . $con->qstr($work_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($cell_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($home_phone, get_magic_quotes_gpc()) . ", " . $con->qstr($aol_name, get_magic_quotes_gpc()) . ", " . $con->qstr($yahoo_name, get_magic_quotes_gpc()) . ", " . $con->qstr($msn_name, get_magic_quotes_gpc()) . ", " . $con->qstr($interests, get_magic_quotes_gpc()) . ", " . $con->dbtimestamp(mktime()) . ", $session_user_id, " . $con->dbtimestamp(mktime()) . ", $session_user_id)";
 
 $con->execute($sql);
 $con->close();
