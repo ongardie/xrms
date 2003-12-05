e559fbcd406fd15bf7158f41a1d6759cd2a31565
braverock <braverock>
2003-12-05 21:39:36 +0000

    rearranged script comment tags to eliminate javascript error

--- login.php
+++ login.php
@@ -17,55 +17,56 @@ start_page($page_title, false, $msg);
 ?>
 
 <table border=0 cellpadding=0 cellspacing=0 width=100% height=80%>
-	<tr>
-		<td width=33% valign=top>
+    <tr>
+        <td width=33% valign=top>
 
-		&nbsp;
+        &nbsp;
 
-		</td>
-		
-		<!-- gutter //-->
-		<td align=center valign=middle>
-		
-		<form action=login-2.php method=post>
-		<table class=widget cellspacing=1>
-			<tr>
-				<td class=widget_header colspan=2>Login</td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Username</td>
-				<td class=widget_content_form_element><input type=text name=username></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Password</td>
-				<td class=widget_content_form_element><input type=password name=password></td>
-			</tr>
+        </td>
+
+        <!-- gutter //-->
+        <td align=center valign=middle>
+
+        <form action=login-2.php method=post>
+        <table class=widget cellspacing=1>
+            <tr>
+                <td class=widget_header colspan=2>Login</td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Username</td>
+                <td class=widget_content_form_element><input type=text name=username></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Password</td>
+                <td class=widget_content_form_element><input type=password name=password></td>
+            </tr>
             <tr>
                 <td class=widget_content_form_element_center colspan=2><input class=button type=submit value="Login"></td>
             </tr>
-		</table>
-		</form>
-		
-		</td>
-		
-		<!-- right column //-->
-		<td width=33% valign=top>
+        </table>
+        </form>
 
-		&nbsp;		
+        </td>
 
-		</td>
-	</tr>
+        <!-- right column //-->
+        <td width=33% valign=top>
+
+        &nbsp;
+
+        </td>
+    </tr>
 </table>
 
 <script language=javascript>
 <!--
 
 function initialize() {
-	document.forms[0].username.focus();
+    document.forms[0].username.focus();
 }
 
 initialize();
-
+//-->
 </script>
+
 
 <?php end_page(); ?>
