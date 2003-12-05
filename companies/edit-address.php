4dfee327ac679330aca223ec1bd1ec5da8dc8d33
braverock <braverock>
2003-12-05 21:57:36 +0000

    added company http link to edit address screen ('back' link)

--- companies/edit-address.php
+++ companies/edit-address.php
@@ -20,17 +20,17 @@ $sql = "select a.*, c.company_name from companies c, addresses a where c.company
 $rst = $con->execute($sql);
 
 if ($rst) {
-	$country_id = $rst->fields['country_id'];
-	$company_name = $rst->fields['company_name'];
-	$address_name = $rst->fields['address_name'];
-	$line1 = $rst->fields['line1'];
-	$line2 = $rst->fields['line2'];
-	$city = $rst->fields['city'];
-	$province = $rst->fields['province'];
-	$postal_code = $rst->fields['postal_code'];
-	$address_body = $rst->fields['address_body'];
-	$use_pretty_address = $rst->fields['use_pretty_address'];
-	$rst->close();
+    $country_id = $rst->fields['country_id'];
+    $company_name = $rst->fields['company_name'];
+    $address_name = $rst->fields['address_name'];
+    $line1 = $rst->fields['line1'];
+    $line2 = $rst->fields['line2'];
+    $city = $rst->fields['city'];
+    $province = $rst->fields['province'];
+    $postal_code = $rst->fields['postal_code'];
+    $address_body = $rst->fields['address_body'];
+    $use_pretty_address = $rst->fields['use_pretty_address'];
+    $rst->close();
 }
 
 $sql = "select country_name, country_id from countries where country_record_status = 'a' order by country_name";
@@ -46,64 +46,68 @@ start_page($page_title, true, $msg);
 ?>
 
 <table border=0 cellpadding=0 cellspacing=0 width=100%>
-	<tr>
-		<td class=lcol width=55% valign=top>
+    <tr>
+        <td class=lcol width=55% valign=top>
 
-		<form action=edit-address-2.php method=post>
-		<input type=hidden name=company_id value=<?php echo $company_id; ?>>
-		<input type=hidden name=address_id value=<?php echo $address_id; ?>>
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=2>Edit Address</td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Address Name</td>
-				<td class=widget_content_form_element><input type=text size=30 name=address_name value="<?php echo $address_name; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Line 1</td>
-				<td class=widget_content_form_element><input type=text size=30 name=line1 value="<?php echo $line1; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Line 2</td>
-				<td class=widget_content_form_element><input type=text size=30 name=line2 value="<?php echo $line2; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>City</td>
-				<td class=widget_content_form_element><input type=text size=30 name=city value="<?php echo $city; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>State/Province</td>
-				<td class=widget_content_form_element><input type=text size=20 name=province value="<?php echo $province; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Postal Code</td>
-				<td class=widget_content_form_element><input type=text size=10 name=postal_code value="<?php echo $postal_code; ?>"></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Country</td>
-				<td class=widget_content_form_element><?php echo $country_menu; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right_91px>Address Body</td>
-				<td class=widget_content_form_element><textarea rows=5 cols=60 name=address_body><?php echo $address_body; ?></textarea> <input type="checkbox" name="use_pretty_address"<?php if ($use_pretty_address == 't') {echo " checked";} ?>> Use</td>
-			</tr>
-			<tr>
-				<td class=widget_content_form_element colspan=2><input class=button type=submit value="Save Changes"> <input class=button type=button value="Delete Address" onclick="javascript: location.href='delete-address.php?company_id=<?php echo $company_id ?>&address_id=<?php echo $address_id ?>';"></td>
-			</tr>
-		</table>
-		</form>
+        <form action=edit-address-2.php method=post>
+        <input type=hidden name=company_id value=<?php echo $company_id; ?>>
+        <input type=hidden name=address_id value=<?php echo $address_id; ?>>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=2>Edit Address</td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Company</td>
+                <td class=widget_content><a href="../companies/one.php?company_id=<?php echo $company_id; ?>"><?php  echo $company_name; ?></a></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Address Name</td>
+                <td class=widget_content_form_element><input type=text size=30 name=address_name value="<?php echo $address_name; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Line 1</td>
+                <td class=widget_content_form_element><input type=text size=30 name=line1 value="<?php echo $line1; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Line 2</td>
+                <td class=widget_content_form_element><input type=text size=30 name=line2 value="<?php echo $line2; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>City</td>
+                <td class=widget_content_form_element><input type=text size=30 name=city value="<?php echo $city; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>State/Province</td>
+                <td class=widget_content_form_element><input type=text size=20 name=province value="<?php echo $province; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Postal Code</td>
+                <td class=widget_content_form_element><input type=text size=10 name=postal_code value="<?php echo $postal_code; ?>"></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Country</td>
+                <td class=widget_content_form_element><?php echo $country_menu; ?></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right_91px>Address Body</td>
+                <td class=widget_content_form_element><textarea rows=5 cols=60 name=address_body><?php echo $address_body; ?></textarea> <input type="checkbox" name="use_pretty_address"<?php if ($use_pretty_address == 't') {echo " checked";} ?>> Use</td>
+            </tr>
+            <tr>
+                <td class=widget_content_form_element colspan=2><input class=button type=submit value="Save Changes"> <input class=button type=button value="Delete Address" onclick="javascript: location.href='delete-address.php?company_id=<?php echo $company_id ?>&address_id=<?php echo $address_id ?>';"></td>
+            </tr>
+        </table>
+        </form>
 
-		</td>
-		<!-- gutter //-->
-		<td class=gutter width=2%>
-		&nbsp;
-		</td>
-		<!-- right column //-->
-		<td class=rcol width=43% valign=top>
-		
-		</td>
-	</tr>
+        </td>
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+        <!-- right column //-->
+        <td class=rcol width=43% valign=top>
+
+        </td>
+    </tr>
 </table>
 
 <?php end_page();; ?>
