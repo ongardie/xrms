7c64a49d5f23e159bdd374e886ccbb13c2137159
braverock <braverock>
2003-12-24 15:04:26 +0000

    changed category_pretty_name to category_display_html resolves SF bug 861167 reported by Beth Macknik (maulani)

--- companies/categories.php
+++ companies/categories.php
@@ -23,58 +23,60 @@ $company_name = fetch_company_name($con, $company_id);
 
 /*
 
-$sql = "select category_id, category_pretty_name 
-from categories 
-where category_record_status = 'a' 
-and category_id in (select ccsm.category_id from category_category_scope_map ccsm, category_scopes cs where ccsm.category_scope_id = cs.category_scope_id and cs.on_what_table = 'companies') 
-and category_id in (select category_id from entity_category_map where on_what_table = 'companies' and on_what_id = $company_id) 
+$sql = "select category_id, category_pretty_name, category_display_html
+from categories
+where category_record_status = 'a'
+and category_id in (select ccsm.category_id from category_category_scope_map ccsm, category_scopes cs where ccsm.category_scope_id = cs.category_scope_id and cs.on_what_table = 'companies')
+and category_id in (select category_id from entity_category_map where on_what_table = 'companies' and on_what_id = $company_id)
 order by category_pretty_name";
 
 */
 
-$sql = "select c.category_id, category_pretty_name 
-from categories c, category_scopes cs, category_category_scope_map ccsm, entity_category_map ecm 
-where ecm.on_what_table = 'companies' 
-and ecm.on_what_id = $company_id 
-and ecm.category_id = c.category_id 
-and cs.category_scope_id = ccsm.category_scope_id 
-and c.category_id = ccsm.category_id 
-and cs.on_what_table = 'companies' 
-and category_record_status = 'a' 
+$sql = "select c.category_id, category_pretty_name, category_display_html
+from categories c, category_scopes cs, category_category_scope_map ccsm, entity_category_map ecm
+where ecm.on_what_table = 'companies'
+and ecm.on_what_id = $company_id
+and ecm.category_id = c.category_id
+and cs.category_scope_id = ccsm.category_scope_id
+and c.category_id = ccsm.category_id
+and cs.on_what_table = 'companies'
+and category_record_status = 'a'
 order by category_pretty_name";
+
+// $con->debug=1;
 
 $rst = $con->execute($sql);
 $array_of_categories = array();
 array_push($array_of_categories, 0);
 
 if ($rst) {
-	while (!$rst->EOF) {
-		$associated_with .= "<a href='remove-category.php?company_id=$company_id&category_id=" . $rst->fields['category_id'] . "'>" . $rst->fields['category_pretty_name'] . "</a><br>";
-		array_push($array_of_categories, $rst->fields['category_id']);
-		$rst->movenext();
-	}
-	$rst->close();
+    while (!$rst->EOF) {
+        $associated_with .= "<a href='remove-category.php?company_id=$company_id&category_id=" . $rst->fields['category_id'] . "'>" . $rst->fields['category_display_html'] . "</a><br>";
+        array_push($array_of_categories, $rst->fields['category_id']);
+        $rst->movenext();
+    }
+    $rst->close();
 }
 
 // not associated with
 
-$sql = "select c.category_id, category_pretty_name 
-from categories c, category_scopes cs, category_category_scope_map ccsm 
-where cs.category_scope_id = ccsm.category_scope_id 
-and c.category_id = ccsm.category_id 
-and cs.on_what_table = 'companies' 
-and c.category_id not in (" . implode($array_of_categories, ',') . ") 
-and category_record_status = 'a' 
+$sql = "select c.category_id, category_pretty_name, category_display_html
+from categories c, category_scopes cs, category_category_scope_map ccsm
+where cs.category_scope_id = ccsm.category_scope_id
+and c.category_id = ccsm.category_id
+and cs.on_what_table = 'companies'
+and c.category_id not in (" . implode($array_of_categories, ',') . ")
+and category_record_status = 'a'
 order by category_pretty_name";
 
 $rst = $con->execute($sql);
 
 if ($rst) {
-	while (!$rst->EOF) {
-		$not_associated_with .= "<a href='add-category.php?company_id=$company_id&category_id=" . $rst->fields['category_id'] . "'>" . $rst->fields['category_pretty_name'] . "</a><br>";
-		$rst->movenext();
-	}
-	$rst->close();
+    while (!$rst->EOF) {
+        $not_associated_with .= "<a href='add-category.php?company_id=$company_id&category_id=" . $rst->fields['category_id'] . "'>" . $rst->fields['category_display_html'] . "</a><br>";
+        $rst->movenext();
+    }
+    $rst->close();
 }
 
 $con->close();
@@ -85,36 +87,36 @@ start_page($page_title, true, $msg);
 ?>
 
 <table border=0 cellpadding=0 cellspacing=0 width=100%>
-	<tr>
-		<td class=lcol width=55% valign=top>
+    <tr>
+        <td class=lcol width=55% valign=top>
 
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=2>Manage Categories</td>
-			</tr>
-			<tr>
-				<td width=50% class=widget_label>Associated With</td>
-				<td class=widget_label>Not Associated With</td>
-			</tr>
-			<tr>
-				<td class=widget_content valign=top><?php  echo $associated_with; ?></td>
-				<td class=widget_content valign=top><?php  echo $not_associated_with; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_content_form_element colspan=2><input type=button class=button onclick="javascript: location.href='one.php?company_id=<?php  echo $company_id; ?>';" value="Finished"></td>
-			</tr>
-		</table>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=2>Manage Categories</td>
+            </tr>
+            <tr>
+                <td width=50% class=widget_label>Associated With</td>
+                <td class=widget_label>Not Associated With</td>
+            </tr>
+            <tr>
+                <td class=widget_content valign=top><?php  echo $associated_with; ?></td>
+                <td class=widget_content valign=top><?php  echo $not_associated_with; ?></td>
+            </tr>
+            <tr>
+                <td class=widget_content_form_element colspan=2><input type=button class=button onclick="javascript: location.href='one.php?company_id=<?php  echo $company_id; ?>';" value="Finished"></td>
+            </tr>
+        </table>
 
-		</td>
-		<!-- gutter //-->
-		<td class=gutter width=2%>
-		&nbsp;
-		</td>
-		<!-- right column //-->
-		<td class=rcol width=43% valign=top>
-		
-		</td>
-	</tr>
+        </td>
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+        <!-- right column //-->
+        <td class=rcol width=43% valign=top>
+
+        </td>
+    </tr>
 </table>
 
 <?php end_page();; ?>
