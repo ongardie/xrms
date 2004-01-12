7deae871bb0b430060b728636c750ec1f6a982f7
cdwtech <cdwtech>
2004-01-12 08:53:04 +0000

    date_of_birth can't be null

--- sql/mysql/upgrade-2003Dec22-2004Jan12.sql
+++ sql/mysql/upgrade-2003Dec22-2004Jan12.sql
@@ -3,7 +3,7 @@ alter table companies add tax_id varchar(255) not null;
 
 alter table contacts add salutation varchar(20) not null;
 alter table contacts add gender char(1) not null;
-alter table contacts add date_of_birth varchar(20);
+alter table contacts add date_of_birth varchar(20) not null;
 
 alter table contacts alter column gender set default 'm';
 
