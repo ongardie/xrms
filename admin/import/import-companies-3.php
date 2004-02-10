8f6e0fd6dcdf4265bd2f3f0484bdcfbac56374a0
braverock <braverock>
2004-02-10 22:45:56 +0000

    - fixed typo noticed by Beth

--- admin/import/import-companies-3.php
+++ admin/import/import-companies-3.php
@@ -24,7 +24,7 @@
  * @todo modify CSVtoArray fn to do a trim, strtolower, and replace spaces with underscores in array element names
  * @todo could better accomodate microsoft Outlook by looking for outlook field names
  *
- * $Id: import-companies-3.php,v 1.11 2004/02/10 22:26:49 braverock Exp $
+ * $Id: import-companies-3.php,v 1.12 2004/02/10 22:45:56 braverock Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -329,7 +329,7 @@ foreach ($filearray as $row) {
         }
         //now set the where clause if the company existed
         if ($company_id) {
-            $sql_insert_company .= " where compay_id = $company_id";
+            $sql_insert_company .= " where company_id = $company_id";
         }
 
         $con->execute($sql_insert_company);
@@ -537,6 +537,9 @@ end_page();
 
 /**
  * $Log: import-companies-3.php,v $
+ * Revision 1.12  2004/02/10 22:45:56  braverock
+ * - fixed typo noticed by Beth
+ *
  * Revision 1.11  2004/02/10 22:26:49  braverock
  * - fixed syntax error when updating data on existing company
  *
