27ae1c7ee3d6cc444eb265618b5e3814ddf5b327
braverock <braverock>
2004-02-14 15:40:18 +0000

    - change table to rating_display_html per a SF bug - add phpdoc

--- admin/ratings/some.php
+++ admin/ratings/some.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Show all the ratings for editing
+ *
+ * @todo add sort order for display purposes
+ *
+ * $Id: some.php,v 1.2 2004/02/14 15:40:18 braverock Exp $
+ */
+
 
 require_once('../../include-locations.inc');
 require_once($include_directory . 'vars.php');
@@ -15,13 +23,13 @@ $sql = "select * from ratings where rating_record_status = 'a' order by rating_i
 $rst = $con->execute($sql);
 
 if ($rst) {
-	while (!$rst->EOF) {
-		$table_rows .= '<tr>';
-		$table_rows .= '<td class=widget_content><a href=one.php?rating_id=' . $rst->fields['rating_id'] . '>' . $rst->fields['rating_pretty_name'] . '</a></td>';
-		$table_rows .= '</tr>';
-		$rst->movenext();
-	}
-	$rst->close();
+    while (!$rst->EOF) {
+        $table_rows .= '<tr>';
+        $table_rows .= '<td class=widget_content><a href=one.php?rating_id=' . $rst->fields['rating_id'] . '>' . $rst->fields['rating_display_html'] . '</a></td>';
+        $table_rows .= '</tr>';
+        $rst->movenext();
+    }
+    $rst->close();
 }
 
 $con->close();
@@ -32,59 +40,70 @@ start_page($page_title);
 ?>
 
 <table border=0 cellpadding=0 cellspacing=0 width=100%>
-	<tr>
-		<td class=lcol width=65% valign=top>
-		
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=4>Ratings</td>
-			</tr>
-			<tr>
-				<td class=widget_label>Name</td>
-			</tr>
-			<?php  echo $table_rows; ?>
-		</table>
-		
-		</td>
-		
-		<!-- gutter //-->
-		<td class=gutter width=2%>
-		&nbsp;
-		</td>
-		
-		<!-- right column //-->
-		
-		<td class=rcol width=33% valign=top>
+    <tr>
+        <td class=lcol width=65% valign=top>
 
-		<form action=add-2.php method=post>
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=2>Add New Rating</td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Short Name</td>
-				<td class=widget_content_form_element><input type=text name=rating_short_name size=30></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Full Name</td>
-				<td class=widget_content_form_element><input type=text name=rating_pretty_name size=30></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Full Plural Name</td>
-				<td class=widget_content_form_element><input type=text name=rating_pretty_plural size=30></td>
-			</tr>
-			<tr>
-				<td class=widget_label_right>Display HTML</td>
-				<td class=widget_content_form_element><input type=text name=rating_display_html size=30></td>
-			</tr>
-			<tr>
-				<td class=widget_content_form_element colspan=2><input class=button type=submit value="Add"></td>
-			</tr>
-		</table>
-		</form>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=4>Ratings</td>
+            </tr>
+            <tr>
+                <td class=widget_label>Name</td>
+            </tr>
+            <?php  echo $table_rows; ?>
+        </table>
 
-		</td>
-	</tr>
+        </td>
+
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+
+        <!-- right column //-->
+
+        <td class=rcol width=33% valign=top>
+
+        <form action=add-2.php method=post>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=2>Add New Rating</td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Short Name</td>
+                <td class=widget_content_form_element><input type=text name=rating_short_name size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Name</td>
+                <td class=widget_content_form_element><input type=text name=rating_pretty_name size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Full Plural Name</td>
+                <td class=widget_content_form_element><input type=text name=rating_pretty_plural size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_label_right>Display HTML</td>
+                <td class=widget_content_form_element><input type=text name=rating_display_html size=30></td>
+            </tr>
+            <tr>
+                <td class=widget_content_form_element colspan=2><input class=button type=submit value="Add"></td>
+            </tr>
+        </table>
+        </form>
+
+        </td>
+    </tr>
 </table>
 
-<?php end_page();; ?>
+<?php
+
+end_page();
+
+/**
+ * $Log: some.php,v $
+ * Revision 1.2  2004/02/14 15:40:18  braverock
+ * - change table to rating_display_html per a SF bug
+ * - add phpdoc
+ *
+ */
+?>
