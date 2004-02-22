6ded9b5ec952a03d7b6a44252b7754adc8c3ed9d
braverock <braverock>
2004-02-22 16:01:04 +0000

    changed company_legal_name to legal_name to be consistent with current CVS

--- sql/mysql/upgrade-2003Dec22-2004Jan12.sql
+++ sql/mysql/upgrade-2003Dec22-2004Jan12.sql
@@ -1,4 +1,4 @@
-alter table companies add company_legal_name varchar(255) not null;
+alter table companies add legal_name varchar(255) not null;
 alter table companies add tax_id varchar(255) not null;
 
 alter table contacts add salutation varchar(20) not null;
@@ -9,5 +9,5 @@ alter table contacts alter column gender set default 'm';
 
 alter table opportunity_statuses add opportunity_status_long_desc varchar(255) not null;
 
-update companies set company_legal_name = company_name;
+update companies set legal_name = company_name;
 update contacts set gender = 'u';
