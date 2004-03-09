0c4881e321de1f260e575efc34bc1c3b9bad873d
maulani <maulani>
2004-03-09 15:21:59 +0000

    - Added phpdoc - Widened Audit Items layout to display date on one line - Added New This Week and This Month columns

--- admin/reports/dashboard.php
+++ admin/reports/dashboard.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+ * admin/reports/dashboard.php - Administrator's Dashboard 
+ * 
+ * Displays Audit entries and new activity counts.  Needs work.
+ * 
+ * $Id: dashboard.php,v 1.2 2004/03/09 15:21:59 maulani Exp $ 
+ */ 
 
 require_once('../../include-locations.inc');
 require_once($include_directory . 'vars.php');
@@ -33,46 +40,133 @@ if ($rst) {
 	$rst->close();
 }
 
-// how many activities were created today
 
-$sql = "select count(*) as activity_count from activities where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+// What is the first day of the week
+$sql = 'Select DATE_SUB(CURRENT_DATE,INTERVAL (DAYOFWEEK(CURRENT_DATE)-1) DAY) AS first_day_week';
 $rst = $con->execute($sql);
-$activity_count = $rst->fields['activity_count'];
+$first_day_week = $rst->fields['first_day_week'] . ' 00:00:00';
+$rst->close();
+
+// What is the first day of the month
+$sql = 'Select DATE_SUB(CURRENT_DATE,INTERVAL (DAYOFWEEK(CURRENT_DATE)-1) DAY) AS first_day_month';
+$rst = $con->execute($sql);
+$first_day_month = $rst->fields['first_day_month'] . ' 00:00:00';
+$rst->close();
+
+
+$begin_today = $con->dbdate(date('Y-m-d 00:00:00'));
+$begin_week = $con->dbdate(date($first_day_week));
+$begin_month = $con->dbdate(date($first_day_month));
+
+//show_test_values($sql);
+
+
+// how many activities were created today
+$sql = "select count(*) as activity_count from activities where entered_at >= " . $begin_today;
+$rst = $con->execute($sql);
+$activity_count_today = $rst->fields['activity_count'];
+$rst->close();
+
+// how many activities were created this week
+$sql = "select count(*) as activity_count from activities where entered_at >= " . $begin_week;
+$rst = $con->execute($sql);
+$activity_count_week = $rst->fields['activity_count'];
+$rst->close();
+
+// how many activities were created this month
+$sql = "select count(*) as activity_count from activities where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$activity_count_month = $rst->fields['activity_count'];
 $rst->close();
 
 // how many companies were created today
-
-$sql = "select count(*) as company_count from companies where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+$sql = "select count(*) as company_count from companies where entered_at >= " . $begin_today;
 $rst = $con->execute($sql);
-$company_count = $rst->fields['company_count'];
+$company_count_today = $rst->fields['company_count'];
+$rst->close();
+
+// how many companies were created this week
+$sql = "select count(*) as company_count from companies where entered_at >= " . $begin_week;
+$rst = $con->execute($sql);
+$company_count_week = $rst->fields['company_count'];
+$rst->close();
+
+// how many companies were created this month
+$sql = "select count(*) as company_count from companies where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$company_count_month = $rst->fields['company_count'];
 $rst->close();
 
 // how many contacts were created today
-
-$sql = "select count(*) as contact_count from contacts where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+$sql = "select count(*) as contact_count from contacts where entered_at >= " . $begin_today;
 $rst = $con->execute($sql);
-$contact_count = $rst->fields['contact_count'];
+$contact_count_today = $rst->fields['contact_count'];
+$rst->close();
+
+// how many contacts were created this week
+$sql = "select count(*) as contact_count from contacts where entered_at >= " . $begin_week;
+$rst = $con->execute($sql);
+$contact_count_week = $rst->fields['contact_count'];
+$rst->close();
+
+// how many contacts were created this month
+$sql = "select count(*) as contact_count from contacts where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$contact_count_month = $rst->fields['contact_count'];
 $rst->close();
 
 // how many opportunities were created today
-
-$sql = "select count(*) as opportunity_count from opportunities where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+$sql = "select count(*) as opportunity_count from opportunities where entered_at >= " . $begin_today;
 $rst = $con->execute($sql);
-$opportunity_count = $rst->fields['opportunity_count'];
+$opportunity_count_today = $rst->fields['opportunity_count'];
 $rst->close();
 
-// how much were those opportunities worth?
-
-$sql = "select sum(size) as opportunity_total from opportunities where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+// how many opportunities were created this week
+$sql = "select count(*) as opportunity_count from opportunities where entered_at >= " . $begin_week;
 $rst = $con->execute($sql);
-$opportunity_total = '$' . number_format($rst->fields['opportunity_total'], 2);
+$opportunity_count_week = $rst->fields['opportunity_count'];
+$rst->close();
+
+// how many opportunities were created this month
+$sql = "select count(*) as opportunity_count from opportunities where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$opportunity_count_month = $rst->fields['opportunity_count'];
+$rst->close();
+
+// how much were those opportunities worth today?
+$sql = "select sum(size) as opportunity_total from opportunities where entered_at >= " . $begin_today;
+$rst = $con->execute($sql);
+$opportunity_total_today = '$' . number_format($rst->fields['opportunity_total'], 2);
+$rst->close();
+
+// how much were those opportunities worth this week?
+$sql = "select sum(size) as opportunity_total from opportunities where entered_at >= " . $begin_week;
+$rst = $con->execute($sql);
+$opportunity_total_week = '$' . number_format($rst->fields['opportunity_total'], 2);
+$rst->close();
+
+// how much were those opportunities worth this month?
+$sql = "select sum(size) as opportunity_total from opportunities where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$opportunity_total_month = '$' . number_format($rst->fields['opportunity_total'], 2);
 $rst->close();
 
 // how many cases were created today
-
-$sql = "select count(*) as case_count from cases where entered_at >= " . $con->dbdate(date('Y-m-d 00:00:00'));
+$sql = "select count(*) as case_count from cases where entered_at >= " . $begin_today;
 $rst = $con->execute($sql);
-$case_count = $rst->fields['case_count'];
+$case_count_today = $rst->fields['case_count'];
+$rst->close();
+
+// how many cases were created this week
+$sql = "select count(*) as case_count from cases where entered_at >= " . $begin_week;
+$rst = $con->execute($sql);
+$case_count_week = $rst->fields['case_count'];
+$rst->close();
+
+// how many cases were created this month
+$sql = "select count(*) as case_count from cases where entered_at >= " . $begin_month;
+$rst = $con->execute($sql);
+$case_count_month = $rst->fields['case_count'];
 $rst->close();
 
 $con->close();
@@ -84,72 +178,99 @@ start_page($page_title);
 
 <table border=0 cellpadding=0 cellspacing=0 width=100%>
 	<tr>
-		<td class=lcol width=75% valign=top>
-
-        <!-- new today //-->
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header>Something</td>
-			</tr>
-			<tr>
-				<td class=widget_content>Not sure what to put here yet.</td>
-			</tr>
-		</table>
-
+		<!-- left column //-->
+		<td class=lcol width=63% valign=top>
+	
+			<!-- new today //-->
+			<table class=widget cellspacing=1 width=100%>
+				<tr>
+					<td class=widget_header>Something</td>
+				</tr>
+				<tr>
+					<td class=widget_content>Not sure what to put here yet.</td>
+				</tr>
+			</table>
+	
     	</td>
+    	
 		<!-- gutter //-->
 		<td class=gutter width=2%>
-		&nbsp;
+			&nbsp;
 		</td>
+		
 		<!-- right column //-->
-		<td class=rcol width=23% valign=top>
-        
-        <!-- new today //-->
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=8>New Today</td>
-			</tr>
-			<tr>
-				<td class=widget_label>Item</td>
-				<td class=widget_label>Count</td>
-			</tr>
-			<tr>
-				<td class=widget_content>Activities</td>
-				<td class=widget_content><?php  echo $activity_count; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_content>Companies</td>
-				<td class=widget_content><?php  echo $company_count; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_content>Contacts</td>
-				<td class=widget_content><?php  echo $contact_count; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_content>Opportunities</td>
-				<td class=widget_content><?php  echo $opportunity_count; ?> for <?php echo $opportunity_total; ?></td>
-			</tr>
-			<tr>
-				<td class=widget_content>Cases</td>
-				<td class=widget_content><?php  echo $case_count; ?></td>
-			</tr>
-		</table>
-
-        <!-- system snapshot //-->
-        <table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=4>Audit Items</td>
-			</tr>
-			<tr>
-				<td class=widget_label>User</td>
-				<td class=widget_label>Action</td>
-				<td class=widget_label>Date</td>
-			</tr>
-			<?php  echo $audit_items; ?>
-		</table>
-        
+		<td class=rcol width=35% valign=top>
+			
+			<!-- new today //-->
+			<table class=widget cellspacing=1 width=100%>
+				<tr>
+					<td class=widget_header colspan=8>New</td>
+				</tr>
+				<tr>
+					<td class=widget_label>Item</td>
+					<td class=widget_label>Today</td>
+					<td class=widget_label>This Week</td>
+					<td class=widget_label>This Month</td>
+				</tr>
+				<tr>
+					<td class=widget_content>Activities</td>
+					<td class=widget_content><?php  echo $activity_count_today; ?></td>
+					<td class=widget_content><?php  echo $activity_count_week; ?></td>
+					<td class=widget_content><?php  echo $activity_count_month; ?></td>
+				</tr>
+				<tr>
+					<td class=widget_content>Companies</td>
+					<td class=widget_content><?php  echo $company_count_today; ?></td>
+					<td class=widget_content><?php  echo $company_count_week; ?></td>
+					<td class=widget_content><?php  echo $company_count_month; ?></td>
+				</tr>
+				<tr>
+					<td class=widget_content>Contacts</td>
+					<td class=widget_content><?php  echo $contact_count_today; ?></td>
+					<td class=widget_content><?php  echo $contact_count_week; ?></td>
+					<td class=widget_content><?php  echo $contact_count_month; ?></td>
+				</tr>
+				<tr>
+					<td class=widget_content>Opportunities</td>
+					<td class=widget_content><?php  echo $opportunity_count_today; ?> for <?php echo $opportunity_total_today; ?></td>
+					<td class=widget_content><?php  echo $opportunity_count_week; ?> for <?php echo $opportunity_total_week; ?></td>
+					<td class=widget_content><?php  echo $opportunity_count_month; ?> for <?php echo $opportunity_total_month; ?></td>
+				</tr>
+				<tr>
+					<td class=widget_content>Cases</td>
+					<td class=widget_content><?php  echo $case_count_today; ?></td>
+					<td class=widget_content><?php  echo $case_count_week; ?></td>
+					<td class=widget_content><?php  echo $case_count_month; ?></td>
+				</tr>
+			</table>
+	
+			<!-- system snapshot //-->
+			<table class=widget cellspacing=1 width=100%>
+				<tr>
+					<td class=widget_header colspan=4>Audit Items</td>
+				</tr>
+				<tr>
+					<td class=widget_label>User</td>
+					<td class=widget_label>Action</td>
+					<td class=widget_label>Date</td>
+				</tr>
+				<?php  echo $audit_items; ?>
+			</table>
+			
 		</td>
 	</tr>
 </table>
 
-<?php end_page();; ?>
+<?php 
+
+end_page();
+
+/** 
+ * $Log: dashboard.php,v $
+ * Revision 1.2  2004/03/09 15:21:59  maulani
+ * - Added phpdoc
+ * - Widened Audit Items layout to display date on one line
+ * - Added New This Week and This Month columns
+ *
+ */ 
+?> 
