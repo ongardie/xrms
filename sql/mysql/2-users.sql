f316c10a3f0d96f69f2da32af60de8418b65b9c8
braverock <braverock>
2004-03-15 14:23:45 +0000

    - add Admin default value to roles table for Beth's stop-gap security

--- sql/mysql/2-users.sql
+++ sql/mysql/2-users.sql
@@ -3,32 +3,35 @@
 -- 
 
 create table roles (
-	role_id														int not null primary key auto_increment,
-	role_short_name												varchar(10) not null default '',
-	role_pretty_name											varchar(100) not null default '',
-	role_pretty_plural											varchar(100) not null default '',
-	role_display_html											varchar(100) not null default '',
-	role_record_status											char(1) default 'a'
+	role_id				int not null primary key auto_increment,
+	role_short_name			varchar(10) not null default '',
+	role_pretty_name		varchar(100) not null default '',
+	role_pretty_plural		varchar(100) not null default '',
+	role_display_html		varchar(100) not null default '',
+	role_record_status		char(1) default 'a'
 );
 
 insert into roles (role_short_name, role_pretty_name, role_pretty_plural, role_display_html) values ('User', 'User', 'Users', 'User');
+insert into roles (role_short_name, role_pretty_name, role_pretty_plural, role_display_html) values ('Admin', 'Admin', 'Admin', 'Admin');
 
 --
 -- typical users table
 --
 
 create table users (
-	user_id														int not null primary key auto_increment,
-	role_id														int not null default 0,
-	username													varchar(100) not null default '',
-	password													varchar(100) not null default '',
-	last_name													varchar(100) not null default '',
-	first_names													varchar(100) not null default '',
-	email														varchar(100) not null default '',
-	language													varchar(50) not null default 'english',
-	gmt_offset													int not null default 0,
-	last_hit													datetime,
-	user_record_status											char(1) default 'a'
+	user_id				int not null primary key auto_increment,
+	role_id				int not null default 0,
+	username			varchar(100) not null default '',
+	password			varchar(100) not null default '',
+	last_name			varchar(100) not null default '',
+	first_names			varchar(100) not null default '',
+	email				varchar(100) not null default '',
+	language			varchar(50) not null default 'english',
+	gmt_offset			int not null default 0,
+	last_hit			datetime,
+	user_record_status		char(1) default 'a'
 );
+
+ALTER TABLE `users` ADD UNIQUE (`username` );
 
 insert into users (role_id, username, password, last_name, first_names, email, language) values (1, 'user1', '24c9e15e52afc47c225b757e7bee1f9d', 'One', 'User', 'user1@somecompany.com', 'english');
