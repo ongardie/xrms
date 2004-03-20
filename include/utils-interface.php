6b0b23605409639e8f94837d7cd8868489abcbf8
braverock <braverock>
2004-03-20 20:03:24 +0000

    - add code to enable plugins - add menuline and end_page hooks to start

--- include/utils-interface.php
+++ include/utils-interface.php
@@ -3,8 +3,10 @@
  * Common user interface functions file.
  *
  *
- * $Id: utils-interface.php,v 1.6 2004/03/12 15:46:52 maulani Exp $
+ * $Id: utils-interface.php,v 1.7 2004/03/20 20:03:24 braverock Exp $
  */
+
+require_once ($include_locations.'plugin.php');
 
 /**
  * function status_msg
@@ -61,6 +63,8 @@ function start_page($page_title = '', $show_navbar = true, $msg = '') {
         </tr>
 EOQ;
 
+
+
     if ($show_navbar) echo <<<EOQ
 
         <tr>
@@ -74,6 +78,13 @@ EOQ;
                 <a href="$http_site_root/campaigns/some.php">Campaigns</a> &bull;
                 <a href="$http_site_root/opportunities/some.php">Opportunities</a> &bull;
                 <a href="$http_site_root/cases/some.php">Cases</a> &bull;
+                <a href="$http_site_root/files/some.php">Files</a> &bull;
+EOQ;
+            //place the munu_line hook before Reports and Adminstration link
+            do_hook ('menuline');
+
+            echo <<<EOQ
+
                 <a href="$http_site_root/reports/">Reports</a> &bull;
                 <a href="$http_site_root/admin/routing.php">Administration</a>
             </td>
@@ -102,15 +113,24 @@ EOQ;
  *
  * This function closes off the page structure.
  *
+ * This function also contains the end_page hook to allow
+ * for adding stuff to the page footer via a hook.
+ *
  * Any common page footer would end this.
  */
 function end_page() {
 
-    echo <<<EOQ
+    /**
+     * place the end_page hook before we close the body and html
+     * I don't think any of the tables should still be open, so a
+     * hook writer would need to add thier own structure.
+     */
+    do_hook ('end_page');
 
+    echo "
     </body>
-    </html>
-EOQ;
+    </html>\n";
+
 } //end end_page fn
 
 /**
@@ -145,6 +165,10 @@ function build_salutation_menu($salutation) {
 
 /**
  * $Log: utils-interface.php,v $
+ * Revision 1.7  2004/03/20 20:03:24  braverock
+ * - add code to enable plugins
+ * - add menuline and end_page hooks to start
+ *
  * Revision 1.6  2004/03/12 15:46:52  maulani
  * Temporary change for use until full access control is implemented
  * - Block non-admin users from the administration screen
