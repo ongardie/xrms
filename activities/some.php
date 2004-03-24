cd500b4a65ded5e29d9eabff62e78a21d8ca2542
maulani <maulani>
2004-03-24 18:04:13 +0000

    - cleanup code formatting

--- activities/some.php
+++ activities/some.php
@@ -4,7 +4,7 @@
  *
  * View a list of activities
  *
- * $Id: some.php,v 1.3 2004/03/24 17:18:37 maulani Exp $
+ * $Id: some.php,v 1.4 2004/03/24 18:04:13 maulani Exp $
  */
 
 require_once('../include-locations.inc');
@@ -32,38 +32,38 @@ $con->connect($db_server, $db_username, $db_password, $db_dbname);
 $rst = $con->execute($sql);
 
 if ($rst) {
-	while (!$rst->EOF) {
-		
-    	$open_p = $rst->fields['activity_status'];
-		$scheduled_at = $rst->unixtimestamp($rst->fields['scheduled_at']);
-		$is_overdue = $rst->fields['is_overdue'];
+    while (!$rst->EOF) {
         
-		if ($open_p == 'o') {
-			if ($is_overdue) {
-				$classname = 'overdue_activity';
-			} else {
-				$classname = 'open_activity';
-			}
-		} else {
-			$classname = 'closed_activity';
-		}
-		
-		$open_activities .= '<tr>';
-		$open_activities .= '<td class=' . $classname . '><a href=/activities/one.php?activity_id=' . $rst->fields['activity_id'] . '>' . $rst->fields['activity_title'] . '</a></td>';
-		$open_activities .= '<td class=' . $classname . '>' . $rst->fields['activity_type_pretty_name'] . '</td>';
-		$open_activities .= '<td class=' . $classname . '>' . $rst->fields['company_name'] . '</td>';
-		$open_activities .= '<td class=' . $classname . '>' . $rst->fields['first_names'] . " " . $rst->fields['last_name'] . '</td>';
-		$open_activities .= '<td class=' . $classname . '>' . $con->userdate($rst->fields['scheduled_at']) . '</td>';
-    	$open_activities .= '</tr>';
-		$rst->movenext();
-	}
-	$rst->close();
+        $open_p = $rst->fields['activity_status'];
+        $scheduled_at = $rst->unixtimestamp($rst->fields['scheduled_at']);
+        $is_overdue = $rst->fields['is_overdue'];
+        
+        if ($open_p == 'o') {
+            if ($is_overdue) {
+                $classname = 'overdue_activity';
+            } else {
+                $classname = 'open_activity';
+            }
+        } else {
+            $classname = 'closed_activity';
+        }
+        
+        $open_activities .= '<tr>';
+        $open_activities .= '<td class=' . $classname . '><a href=/activities/one.php?activity_id=' . $rst->fields['activity_id'] . '>' . $rst->fields['activity_title'] . '</a></td>';
+        $open_activities .= '<td class=' . $classname . '>' . $rst->fields['activity_type_pretty_name'] . '</td>';
+        $open_activities .= '<td class=' . $classname . '>' . $rst->fields['company_name'] . '</td>';
+        $open_activities .= '<td class=' . $classname . '>' . $rst->fields['first_names'] . " " . $rst->fields['last_name'] . '</td>';
+        $open_activities .= '<td class=' . $classname . '>' . $con->userdate($rst->fields['scheduled_at']) . '</td>';
+        $open_activities .= '</tr>';
+        $rst->movenext();
+    }
+    $rst->close();
 }
 
 $con->close();
 
 if (!strlen($open_activities) > 0) {
-	$open_activities = "<tr><td class=widget_content colspan=5>No open activities</td></tr>";
+    $open_activities = "<tr><td class=widget_content colspan=5>No open activities</td></tr>";
 }
 
 $page_title = "Open Activities";
@@ -72,33 +72,33 @@ start_page($page_title);
 ?>
 
 <table border=0 cellpadding=0 cellspacing=0 width=100%>
-	<tr>
-		<td class=lcol width=65% valign=top>
+    <tr>
+        <td class=lcol width=65% valign=top>
 
-		<table class=widget cellspacing=1 width=100%>
-			<tr>
-				<td class=widget_header colspan=5>Activities</td>
-			</tr>
-			<tr>
-				<td class=widget_label>Activity</td>
-				<td class=widget_label>Type</td>
-				<td class=widget_label>Company</td>
-				<td class=widget_label>Contact</td>
-				<td class=widget_label>Scheduled At</td>
-			</tr>
-			<?php  echo $open_activities; ?>
-		</table>
+        <table class=widget cellspacing=1 width=100%>
+            <tr>
+                <td class=widget_header colspan=5>Activities</td>
+            </tr>
+            <tr>
+                <td class=widget_label>Activity</td>
+                <td class=widget_label>Type</td>
+                <td class=widget_label>Company</td>
+                <td class=widget_label>Contact</td>
+                <td class=widget_label>Scheduled At</td>
+            </tr>
+            <?php  echo $open_activities; ?>
+        </table>
 
-		</td>
-		<!-- gutter //-->
-		<td class=gutter width=2%>
-		&nbsp;
-		</td>
-		<!-- right column //-->
-		<td class=rcol width=33% valign=top>
-		
-		</td>
-	</tr>
+        </td>
+        <!-- gutter //-->
+        <td class=gutter width=2%>
+        &nbsp;
+        </td>
+        <!-- right column //-->
+        <td class=rcol width=33% valign=top>
+        
+        </td>
+    </tr>
 </table>
 
 <?php
@@ -107,6 +107,9 @@ end_page();
 
 /**
  * $Log: some.php,v $
+ * Revision 1.4  2004/03/24 18:04:13  maulani
+ * - cleanup code formatting
+ *
  * Revision 1.3  2004/03/24 17:18:37  maulani
  * - add phpdoc
  *
