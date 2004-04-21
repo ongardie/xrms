875343587b05322ae705fbde5a55afd8591f06b7
braverock <braverock>
2004-04-21 05:16:22 +0000

    - set default address id for companies without address   - fixes loop logic error in original code   - from patch submitted by Glenn Powers

--- admin/data_clean.php
+++ admin/data_clean.php
@@ -7,7 +7,8 @@
  * must be made.
  *
  * @author Beth Macknik
- * $Id: data_clean.php,v 1.3 2004/04/13 15:47:12 maulani Exp $
+ *
+ * $Id: data_clean.php,v 1.4 2004/04/21 05:16:22 braverock Exp $
  */
 
 // where do we include from
@@ -26,6 +27,7 @@ $session_user_id = session_check();
 // make a database connection
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+//$con->debug = 1;
 
 $msg = '';
 
@@ -49,16 +51,16 @@ if ($companies_to_fix > 0) {
     while (!$rst->EOF) {
         $company_id = $rst->fields['company_id'];
         $company_record_status = $rst->fields['company_record_status'];
-		$sql = "insert into contacts set
-				company_id = $company_id,
-				last_name = 'Contact',
-				first_names = 'Default',
-				contact_record_status = '$company_record_status',
-				entered_by = $session_user_id,
-				entered_at = " . $con->dbtimestamp(mktime()) . ",
-				last_modified_at = " . $con->dbtimestamp(mktime()) . ",
-				last_modified_by = $session_user_id"
-				;
+        $sql = "insert into contacts set
+                company_id = $company_id,
+                last_name = 'Contact',
+                first_names = 'Default',
+                contact_record_status = '$company_record_status',
+                entered_by = $session_user_id,
+                entered_at = " . $con->dbtimestamp(mktime()) . ",
+                last_modified_at = " . $con->dbtimestamp(mktime()) . ",
+                last_modified_by = $session_user_id";
+
         $con->execute($sql);
 
         $rst->movenext();
@@ -78,15 +80,15 @@ if ($companies_to_fix > 0) {
     $msg .= "Need to create active contacts for $companies_to_fix active companies<BR><BR>";
     while (!$rst->EOF) {
         $company_id = $rst->fields['company_id'];
-		$sql = "insert into contacts set
-				company_id = $company_id,
-				last_name = 'Contact',
-				first_names = 'Default',
-				entered_by = $session_user_id,
-				entered_at = " . $con->dbtimestamp(mktime()) . ",
-				last_modified_at = " . $con->dbtimestamp(mktime()) . ",
-				last_modified_by = $session_user_id"
-				;
+        $sql = "insert into contacts set
+                company_id = $company_id,
+                last_name = 'Contact',
+                first_names = 'Default',
+                entered_by = $session_user_id,
+                entered_at = " . $con->dbtimestamp(mktime()) . ",
+                last_modified_at = " . $con->dbtimestamp(mktime()) . ",
+                last_modified_by = $session_user_id";
+
         $con->execute($sql);
 
         $rst->movenext();
@@ -94,27 +96,40 @@ if ($companies_to_fix > 0) {
 }
 
 // There needs to be at least one address for each company
-$sql = "SELECT companies.company_id, companies.company_record_status ";
+$sql = "SELECT companies.company_id, companies.default_primary_address, companies.company_record_status ";
 $sql .= "FROM companies ";
 $sql .= "LEFT JOIN addresses ON companies.company_id = addresses.company_id ";
-$sql .= "WHERE addresses.company_id IS NULL";
+$sql .= "WHERE companies.default_primary_address = 0";
 $rst = $con->execute($sql);
-$companies_to_fix = $rst->RecordCount();
-if ($companies_to_fix > 0) {
+$rst->RecordCount();
+if ($rst) {
     $msg .= "Need to create addresses for $companies_to_fix companies<BR><BR>";
     while (!$rst->EOF) {
         $company_id = $rst->fields['company_id'];
         $company_record_status = $rst->fields['company_record_status'];
-		$sql = "insert into addresses set
-				company_id = $company_id,
-				country_id = $default_country_id,
-				address_name = 'Main',
-				address_record_status = '$company_record_status'"
-				;
-        $con->execute($sql);
+        $sql = "insert into addresses set
+                company_id = $company_id,
+                country_id = $default_country_id,
+                address_name = 'Main',
+                address_record_status = '$company_record_status'";
 
+        $rst3 = $con->execute($sql);
+        $address_id = $con->insert_id();
+
+        if (($company_id) && ($address_id)) {
+            $sql = "update companies set
+                default_primary_address=$address_id,
+                default_billing_address=$address_id,
+                default_shipping_address=$address_id,
+                default_payment_address=$address_id
+                where company_id=$company_id";
+            $rst2 = $con->execute($sql);
+            if ($rst2) {
+                $rst2->close();
+            }
+        }
         $rst->movenext();
-    }
+   }
 }
 
 // There needs to be at least one active contact for each active company
@@ -130,11 +145,11 @@ if ($companies_to_fix > 0) {
     $msg .= "Need to create active addresses for $companies_to_fix active companies<BR><BR>";
     while (!$rst->EOF) {
         $company_id = $rst->fields['company_id'];
-		$sql = "insert into addresses set
-				company_id = $company_id,
-				country_id = $default_country_id,
-				address_name = 'Main'"
-				;
+        $sql = "insert into addresses set
+                company_id = $company_id,
+                country_id = $default_country_id,
+                address_name = 'Main'";
+
         $con->execute($sql);
 
         $rst->movenext();
@@ -161,6 +176,11 @@ end_page();
 
 /**
  * $Log: data_clean.php,v $
+ * Revision 1.4  2004/04/21 05:16:22  braverock
+ * - set default address id for companies without address
+ *   - fixes loop logic error in original code
+ *   - from patch submitted by Glenn Powers
+ *
  * Revision 1.3  2004/04/13 15:47:12  maulani
  * - add data integrity check so all companies have addresses
  *
