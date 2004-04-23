2437980dd651016af6ca06b0b00bc70ce5d87db8
gpowers <gpowers>
2004-04-23 16:28:31 +0000

    changed $account_status_... to  $campagin_status_... r     (this appeared to be a bug) added support for status_open_indicator,     which is needed for reports/open-items.php and     reports/completed-items.php

--- admin/campaign-statuses/new-2.php
+++ admin/campaign-statuses/new-2.php
@@ -8,15 +8,29 @@ require_once($include_directory . 'adodb/adodb.inc.php');
 
 $session_user_id = session_check();
 
-$account_status_short_name = $_POST['account_status_short_name'];
-$account_status_pretty_name = $_POST['account_status_pretty_name'];
-$account_status_pretty_plural = $_POST['account_status_pretty_plural'];
-$account_status_display_html = $_POST['account_status_display_html'];
+$campaign_status_short_name = $_POST['campaign_status_short_name'];
+$campaign_status_pretty_name = $_POST['campaign_status_pretty_name'];
+$campaign_status_pretty_plural = $_POST['campaign_status_pretty_plural'];
+$campaign_status_display_html = $_POST['campaign_status_display_html'];
+$status_open_indicator = $_POST['status_open_indicator'];
 
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+//$con->debug = 1;
 
-$sql = "insert into campaign_statuses (campaign_status_short_name, campaign_status_pretty_name, campaign_status_pretty_plural, campaign_status_display_html) values (" . $con->qstr($campaign_status_short_name, get_magic_quotes_gpc()) . ", " . $con->qstr($campaign_status_pretty_name, get_magic_quotes_gpc()) . ", " . $con->qstr($campaign_status_pretty_plural, get_magic_quotes_gpc()) . ", " . $con->qstr($campaign_status_display_html, get_magic_quotes_gpc()) . ")";
+$sql = "insert into campaign_statuses (
+        campaign_status_short_name,
+        campaign_status_pretty_name,
+        campaign_status_pretty_plural,
+        campaign_status_display_html,
+        status_open_indicator
+        ) values (" .
+        $con->qstr($campaign_status_short_name, get_magic_quotes_gpc()) . ", " .
+        $con->qstr($campaign_status_pretty_name, get_magic_quotes_gpc()) . ", " .
+        $con->qstr($campaign_status_pretty_plural, get_magic_quotes_gpc()) . ", " .
+        $con->qstr($campaign_status_display_html, get_magic_quotes_gpc()) . ", " .
+        $con->qstr($status_open_indicator, get_magic_quotes_gpc()) . ")";
+
 $con->execute($sql);
 
 $con->close();
