9f156707d267a4d9052c463c31f79a9292f0ed54
gpowers <gpowers>
2004-05-06 13:45:45 +0000

    This plugin implements listing and listening of Asterisk PBX voicemail.

---
+++ plugins/voicemail/README.txt
@@ -0,0 +1,14 @@
+XRMS Voice Mail Plugin v0.1
+for use with the Asterisk Open Source PBX http://www.asterisk.org/
+and XRMS http://xrms.sf.net/
+
+copyright 2004 Glenn Powers <glenn@net127.com>
+Licensed Under the Open Software License v. 2.0
+
+see INSTALL.txt 
+
+The XRMS username is used to look up the correct extension in
+/etc/asterisk/voicemail.conf If the XRMS username is not in voicemail.conf,
+this plugin will not show any messages.  The *last* match is used.
+This is a hack. Patches welcomed.
+
---
+++ plugins/voicemail/setup.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *  setup.php
+ *
+ * Copyright (c) 2004 The XRMS Project Team
+ *
+ *  init plugin into xrms
+ *
+ * @example Create a function called
+ *      xrms_plugin_init_pluginname
+ *      where pluginname is the name of your pluign directory
+ *      inside this function, you will register all the hooks
+ *      that you wish your plugin to be called by
+ *
+ * You should also put the called functions in your setup,php file
+ * Please take care to keep this file as small as possible, as it
+ * is included on every page load.  Place your actualy functionality
+ * in another file.  It will improve the performance of the entire
+ * system.
+ *
+ * $Id: setup.php,v 1.1 2004/05/06 13:45:45 gpowers Exp $
+ */
+
+
+function xrms_plugin_init_voicemail() {
+    global $xrms_plugin_hooks;
+    $xrms_plugin_hooks['menuline']['voicemail'] = 'voicemail';
+}
+
+
+function voicemail() {
+
+    global $http_site_root;
+
+    //Add Demo link to upper menu
+    echo "&nbsp;<a href='$http_site_root/plugins/voicemail/voicemail.php'>Voice Mail</a>&nbsp;&bull;\n";
+}
+
