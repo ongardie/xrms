74abab7829db4d2588cbc8c2698f03ca9b6d8a8a
gpowers <gpowers>
2004-05-06 13:35:54 +0000

    This implements the deletion of Former Names.

---
+++ companies/delete-former-name.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Add Former Name
+ *
+ * $Id: delete-former-name.php,v 1.1 2004/05/06 13:35:54 gpowers Exp $
+ */
+require_once('../include-locations.inc');
+
+require_once($include_directory . 'vars.php');
+require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-misc.php');
+require_once($include_directory . 'adodb/adodb.inc.php');
+
+$session_user_id = session_check();
+
+$company_id = $_GET['company_id'];
+$former_name = $_GET['former_name'];
+
+$con = &adonewconnection($xrms_db_dbtype);
+$con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+// $con->debug = 1;
+
+$sql = "delete from company_former_names where company_id = " . $company_id  . " AND former_name = " . $con->qstr($former_name, get_magic_quotes_gpc());
+
+$con->execute($sql);
+
+add_audit_item($con, $session_user_id, 'deleted former name', 'companies', $company_id);
+
+$con->close();
+
+header("Location: former-names.php?company_id=$company_id");
+
+/**
+ * $Log: delete-former-name.php,v $
+ * Revision 1.1  2004/05/06 13:35:54  gpowers
+ * This implements the deletion of Former Names.
+ *
+ * Revision 1.2  2004/03/26 20:55:59  maulani
+ * - Add audit trail to company-related items
+ * - Add phpdoc
+ *
+ *
+ */
+?>
