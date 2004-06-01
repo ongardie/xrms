bd69fdc0fdccc7b815772c09af6c4a3640e2472b
gpowers <gpowers>
2004-06-01 18:33:14 +0000

    Site Weather XRMS Plugin v0.1 Uses wx200d from http://wx200d.sourceforge.net/

---
+++ plugins/sitewx/README.txt
@@ -0,0 +1,17 @@
+Site Weather XRMS Plugin v0.1
+http://wx200d.sourceforge.net/
+
+copyright 2004 Glenn Powers <glenn@net127.com>
+Licensed Under the Open Software License v. 2.0
+
+This is a simple plugin for including a wx200d page in XRMS.
+
+"wx200d is a weather station data collector and server daemon for the WX200 /
+WM918 / WMR918 / WMR968 weather station hardware. wx200d can log weather data
+to files and/or a database."
+
+This plugin can be easily modified to include any other system call.
+
+Download the lastest version of wx200d from http://wx200d.sourceforge.net/
+and follow the install instructions.
+
---
+++ plugins/sitewx/sitewx.php
@@ -0,0 +1,44 @@
+<?php
+/*
+*
+* Site Weather XRMS Plugin v0.1
+* uses wx200 from:
+* http://wx200d.sourceforge.net/
+*
+* copyright 2004 Glenn Powers <glenn@net127.com>
+* Licensed Under the Open Software License v. 2.0
+*
+*/
+
+
+// include the common files
+require_once('../../include-locations.inc');
+
+require_once($include_directory . 'vars.php');
+require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-misc.php');
+
+//set target and see if we are logged in
+$this = $_SERVER['REQUEST_URI'];
+$session_user_id = session_check( $this );
+
+$msg = $_GET['msg'];
+
+$page_title = "Site Weather";
+start_page($page_title);
+
+?>
+
+<div id="Main">
+    <div id="Content">
+        <pre>
+        <?php system('/usr/local/bin/wx200');  // Edit this if needed ?>
+        </pre>
+    </div>
+</div>
+
+<?php
+
+end_page();
+
+?>
