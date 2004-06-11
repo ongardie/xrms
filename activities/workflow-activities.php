c04f345282f850ce3c5305d16e900d990da2157a
introspectshun <introspectshun>
2004-06-11 21:22:21 +0000

    - Add adodb-params.php include for multi-db compatibility.

--- activities/workflow-activities.php
+++ activities/workflow-activities.php
@@ -18,73 +18,58 @@ $sql = "select * from activity_templates
 
 $rst = $con->execute($sql);
 
-
-//the beginning of the insert statement to add values
-$sql_insert = "insert into activities
-    (activity_type_id,
-    activity_description,
-    ends_at,
-    user_id,
-    company_id,
-    contact_id,
-    on_what_table,
-    on_what_id,
-    on_what_status,
-    activity_title,
-    entered_at,
-    entered_by,
-    scheduled_at,
-    activity_status,
-    activity_record_status) values ";
-
 //generates insert statement to add activities to the current list
 $cnt = 0;
 if ($rst) {
     while (!$rst->EOF) {
 
-    //puts a comma before the set of values (skips the first set)
-    if ($cnt == 0) {
-        $cnt++;
-    } else {
-        $sql_insert .= ", ";
-    }
+        //puts a comma before the set of values (skips the first set)
+        if ($cnt == 0) {
+            $cnt++;
+        } else {
+            $sql_insert .= ", ";
+        }
+    
+        //get the field values from the next record in the query
+        $activity_type_id = $rst->fields['activity_type_id'];
+        $activity_title = $rst->fields['activity_title'];
+        $activity_description = $rst->fields['activity_description'];
+        $duration = $rst->fields['duration'];
+    
+    
+        //calculate ends_at, based on duration and current date
+        if ( is_numeric("$duration") ) {
+            $duration = $duration.' days';
+        }
+    
+        $ends_at = date('Y-m-d',strtotime($duration));
+        
+        $sql2 = "SELECT * FROM activities WHERE 1 = 2"; //select empty record as placeholder
+        $rst2 = $con->execute($sql2);
 
-    //get the field values from the next record in the query
-    $activity_type_id = $rst->fields['activity_type_id'];
-    $activity_title = $rst->fields['activity_title'];
-    $activity_description = $rst->fields['activity_description'];
-    $duration = $rst->fields['duration'];
-
-
-    //calculate ends_at, based on duration and current date
-    if ( is_numeric("$duration") ) {
-        $duration = $duration.' days';
-    }
-
-    $ends_at = date('Y-m-d',strtotime($duration));
-
-    //generate SQL insert statement
-    $sql_insert .= "($activity_type_id,
-        '',
-        '$ends_at 23:59:59',
-        '$user_id',
-        '$company_id',
-        '$contact_id',
-        '$on_what_table',
-        '$on_what_id',
-        '$on_what_id_template',
-        '$activity_title',
-        " . $con->DBTimeStamp(date ('Y-m-d H:i:s')) . ",
-        $user_id,
-        " . $con->DBTimeStamp(date ('Y-m-d H:i:s')) . ",
-        'o',
-        'a')";
-
-    $rst->movenext();
+        $rec = array();
+        $rec['activity_type_id'] = $activity_type_id;
+        $rec['activity_description'] = '';
+        $rec['ends_at'] = $con->DBTimeStamp(date('Y-m-d 23:59:59', strtotime($ends_at)));
+        $rec['user_id'] = $user_id;
+        $rec['company_id'] = $company_id;
+        $rec['contact_id'] = $contact_id;
+        $rec['on_what_table'] = $on_what_table;
+        $rec['on_what_id'] = $on_what_id;
+        $rec['on_what_status'] = $on_what_id_template;
+        $rec['activity_title'] = $activity_title;
+        $rec['entered_at'] = $con->DBTimeStamp(date('Y-m-d H:i:s'));
+        $rec['entered_by'] = $user_id;
+        $rec['scheduled_at'] = $con->DBTimeStamp(date('Y-m-d H:i:s'));
+        $rec['activity_status'] = 'o';
+        $rec['activity_record_status'] = 'a';
+        
+        $ins = $con->GetInsertSQL($rst2, $rec, get_magic_quotes_gpc());
+        $con->execute($ins);
+        
+        $rst->movenext();
     }
     $rst->close();
 }
-
-$con->execute($sql_insert);
 
 ?>
