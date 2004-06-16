49961ab242023d6b100d4fbb8777c71f41934d07
introspectshun <introspectshun>
2004-06-16 22:09:10 +0000

    - removed double quoting from t/f

--- companies/edit-address-2.php
+++ companies/edit-address-2.php
@@ -2,7 +2,7 @@
 /**
  * Database updates for Edit address for a company
  *
- * $Id: edit-address-2.php,v 1.6 2004/06/16 18:30:22 gpowers Exp $
+ * $Id: edit-address-2.php,v 1.7 2004/06/16 22:09:10 introspectshun Exp $
  */
 
 
@@ -28,7 +28,7 @@ $province = $_POST['province'];
 $postal_code = $_POST['postal_code'];
 $use_pretty_address = $_POST['use_pretty_address'];
 
-$use_pretty_address = ($use_pretty_address == 'on') ? "'t'" : "'f'";
+$use_pretty_address = ($use_pretty_address == 'on') ? "t" : "f";
 
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
@@ -57,6 +57,9 @@ header("Location: addresses.php?msg=saved&company_id=$company_id");
 
 /**
  * $Log: edit-address-2.php,v $
+ * Revision 1.7  2004/06/16 22:09:10  introspectshun
+ * - removed double quoting from t/f
+ *
  * Revision 1.6  2004/06/16 18:30:22  gpowers
  * - added (commented out) debug line
  *
