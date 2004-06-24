51d3e4b88efc6ea880a8f5e4f6e11fed8dceb75f
gpowers <gpowers>
2004-06-24 20:58:21 +0000

    - Radius Test (radtest) XRMS Plugin v0.1   - uses radtest from: http://www.freeradius.org/

---
+++ plugins/radtest/README.txt
@@ -0,0 +1,10 @@
+Radius Test (radtest) XRMS Plugin v0.1
+uses radtest from:
+http://www.freeradius.org/
+
+copyright 2004 Glenn Powers <glenn@net127.com>
+Licensed Under the Open Software License v. 2.0
+
+This plugin assumes usernames are stored in custom1 and password is in
+custom2. It uses radtest from: http://www.freeradius.org/ to do the test.
+
---
+++ plugins/radtest/setup.php
@@ -0,0 +1,26 @@
+<?php
+/*
+*
+* Radius Test (radtest) XRMS Plugin v0.1
+* uses radtest from:
+* http://www.freeradius.org/
+*
+* copyright 2004 Glenn Powers <glenn@net127.com>
+* Licensed Under the Open Software License v. 2.0
+*
+*/
+
+function xrms_plugin_init_radtest() {
+    global $xrms_plugin_hooks;
+    $xrms_plugin_hooks['one_contact_buttons']['radtest'] = 'radtest';
+}
+
+function radtest() {
+    global $http_site_root;
+    global $custom1;
+    global $custom2;
+    global $contact_id;
+    echo "<input class=button type=button value=\"Radius Test\" onclick=\"javascript: location.href='" . $http_site_root . "/plugins/radtest/radtest.php?username=" . $custom1 . "&password=" . $custom2 . "&contact_id=<?php echo $contact_id; ?>';\">";
+}
+
+?>
