8597db966cb4167647c75317383082d620bb6c7a
braverock <braverock>
2004-06-25 03:11:47 +0000

    - add error handling to avoid empty result sets and endless loops

--- activities/browse-next.php
+++ activities/browse-next.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Save the updated activity information to the database
+ * Browser to the next Activity in the list
  *
+ * @author Neil Roberts
  *
- * $Id: browse-next.php,v 1.4 2004/06/24 20:19:01 introspectshun Exp $
+ * $Id: browse-next.php,v 1.5 2004/06/25 03:11:47 braverock Exp $
  */
 
 //include required files
@@ -60,18 +61,29 @@ else {
             //Find items within activity_type_id, that have expired
             //Important because it's sorted by lateness first, not probability
             $sql = "select a.activity_id
-                from activities as a, $current_on_what_table
-                where a.activity_type_id=$activity_type
-                and a.activity_status = 'o'
+                from activities as a ";
+            if (strlen($current_on_what_table)>0) {
+                $sql .=   " , $current_on_what_table ";
+            }
+            $sql .= "
+                where a.activity_status = 'o'
                 and a.activity_record_status='a'
                 and a.ends_at < " . $con->DBTimestamp(time()) . "
-                and a.user_id = $session_user_id
-                and a.on_what_table='$current_on_what_table'
-                and a.on_what_id=" . $current_on_what_table_singular . "_id
-                order by a.ends_at asc";
-            if($on_what_table == "opportunities") {
+                and a.user_id = $session_user_id ";
+
+            if ($on_what_table == "opportunities") {
                 $sql .= ", o.probability desc";
             }
+            if (strlen($activity_type)>0) {
+                $sql .= "\n and a.activity_type_id=$activity_type";
+            }
+            if (strlen($current_on_what_table)>0) {
+                $sql .= "\n and a.on_what_table='$current_on_what_table'
+                            and a.on_what_id="
+                            . $current_on_what_table_singular . "_id ";
+            }
+
+            $sql .= "\n order by a.ends_at asc";
 
             $rst = $con->execute($sql);
             if(!$rst) {
@@ -84,23 +96,40 @@ else {
                     $rst->movenext();
                 }
                 $more_to_check = false;
+                $rst->close();
             }
-            $rst->close();
+            else {
+                $more_to_check = false;
+            }
 
             //Get the remaining activities, sorting by probability(if applicable), then date
             $sql = "select a.activity_id
-                from activities a, $current_on_what_table
-                where a.activity_type_id=$activity_type
-                and a.activity_status = 'o'
+                from activities as a ";
+            if (strlen($current_on_what_table)>0) {
+                $sql .=   " , $current_on_what_table ";
+            }
+            $sql .= "
+                where a.activity_status = 'o'
                 and a.activity_record_status='a'
                 and a.ends_at >= " . $con->DBTimestamp(time()) . "
-                and a.user_id = $session_user_id
-                and a.on_what_table = '$current_on_what_table'
-                and a.on_what_id=" . $current_on_what_table_singular . "_id";
+                and a.user_id = $session_user_id ";
+
+            if ($on_what_table == "opportunities") {
+                $sql .= ", o.probability desc";
+            }
+            if (strlen($activity_type)>0) {
+                $sql .= "\n and a.activity_type_id=$activity_type";
+            }
+            if (strlen($current_on_what_table)>0) {
+                $sql .= "\n and a.on_what_table='$current_on_what_table'
+                            and a.on_what_id="
+                            . $current_on_what_table_singular . "_id ";
+            }
+
             if($current_on_what_table == "opportunities") {
                 $sql .= " order by probability desc, a.ends_at asc";
             }
-            else { 
+            else {
                 $sql .= " order by a.ends_at asc";
             }
             $rst = $con->execute($sql);
@@ -114,15 +143,21 @@ else {
                     $rst->movenext();
                 }
                 $more_to_check = false;
+                $rst->close();
             }
-            $rst->close();
+            else {
+                $more_to_check = false;
+            }
+
         }
 
         if($more_to_check) {
             $sql = "select activity_type_id
-                from activity_types
-                where activity_type_id < $activity_type
-                order by activity_type_id desc";
+                from activity_types ";
+            if ($activity_type > 0) {
+                $sql .= "\n where activity_type_id < $activity_type ";
+            }
+            $sql .= "\n order by activity_type_id desc";
             $rst = $con->execute($sql);
             if(!$rst) {
                 db_error_handler($con, $sql);
@@ -130,15 +165,14 @@ else {
             elseif($rst->rowcount() > 0) {
                 $pos = 0;
                 $activity_type = $rst->fields['activity_type_id'];
+                $rst->close();
             }
             else {
                 $more_to_check = false;
                 header("Location: some.php");
             }
-            $rst->close();
         }
         else {
-            $_SESSION['activity_type'] = $activity_type;
             $_SESSION['next_to_check'] = $next_to_check;
             $_SESSION['pos'] = 1;
             header("Location: one.php?activity_id=" . $next_to_check[0]);
@@ -146,10 +180,15 @@ else {
     }
 }
 
+$_SESSION['activity_type'] = $activity_type;
+
 $con->close();
 
 /**
  * $Log: browse-next.php,v $
+ * Revision 1.5  2004/06/25 03:11:47  braverock
+ * - add error handling to avoid empty result sets and endless loops
+ *
  * Revision 1.4  2004/06/24 20:19:01  introspectshun
  * - Updated time formats in SELECTs to use DBTimestamp()
  *
