e164a958b943bba3611eb82efcbbb9e42ac706d1
braverock <braverock>
2004-06-30 11:38:16 +0000

    - assign default address to contact after transfer   - applies patch provided by David Uhlman

--- contacts/transfer-2.php
+++ contacts/transfer-2.php
@@ -2,7 +2,7 @@
 /**
  * Database updates for transfer contact
  *
- * $Id: transfer-2.php,v 1.2 2004/06/15 17:26:21 introspectshun Exp $
+ * $Id: transfer-2.php,v 1.3 2004/06/30 11:38:16 braverock Exp $
  */
 
 
@@ -23,12 +23,21 @@ $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 // $con->debug = 1;
 
+//the order by ensure that the first address entered is used, this would normally be the main address except in unusual cirumstances
+//if no record if found then the transfer contact will have there new adddress_id set as 0 which is better than still showing up in the used by field of the old company
+$sql = "SELECT * FROM addresses where company_id = $new_company_id ORDER BY address_id ASC";
+$rst = $con->execute($sql);
+$new_address_id = 0;
+if (!$rst->EOF) {
+	$new_address_id = $rst->fields['address_id'];
+}
+
 $sql = "SELECT * FROM contacts WHERE contact_id = $contact_id";
 $rst = $con->execute($sql);
 
 $rec = array();
 $rec['company_id'] = $new_company_id;
-
+$rec['address_id'] = $new_address_id;
 $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
 $con->execute($upd);
 
@@ -41,6 +50,10 @@ header("Location: one.php?msg=saved&contact_id=$contact_id");
 
 /**
  * $Log: transfer-2.php,v $
+ * Revision 1.3  2004/06/30 11:38:16  braverock
+ * - assign default address to contact after transfer
+ *   - applies patch provided by David Uhlman
+ *
  * Revision 1.2  2004/06/15 17:26:21  introspectshun
  * - Add adodb-params.php include for multi-db compatibility.
  * - Corrected order of arguments to implode() function.
