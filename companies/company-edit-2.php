231c8d3debe26219ced516e9482e23ae4f95bc0e
introspectshun <introspectshun>
2004-07-05 21:54:54 +0000

    - Now uses GetUpdateSQL

--- companies/company-edit-2.php
+++ companies/company-edit-2.php
@@ -13,6 +13,7 @@ require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
+require_once($include_directory . 'adodb-params.php');
 
 $session_user_id = session_check();
 
@@ -23,27 +24,37 @@ $return_url = $_POST['return_url'];
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 
-$rst = $con->execute("select * from relationships where relationship_id='$relationship_id'");
+$sql = "SELECT * FROM relationships WHERE relationship_id = '$relationship_id'";
+$rst = $con->execute($sql);
 
 if($_POST['unassociate']) {
-    $sql = "update relationships set
-        relationship_status = 'd'
-        where relationship_id = $relationship_id";
+    $rec = array();
+    $rec['relationship_status'] = 'd';
+
+    $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
+    $con->execute($upd);
 }
 elseif($_POST['make_default']) {
-    $sql = "update contacts
-        set company_id = " . $rst->fields['to_what_id'] . "
-        where contact_id = " . $rst->fields['from_what_id'];
+    $sql = "SELECT * FROM contacts WHERE contact_id = '" . $rst->fields['from_what_id'] . "'";
+    $rst2 = $con->execute($sql);
+
+    $rec = array();
+    $rec['company_id'] = $rst->fields['to_what_id'];
+
+    $upd = $con->GetUpdateSQL($rst2, $rec, false, get_magic_quotes_gpc());
+    $con->execute($upd);
 }
  //$con->debug=1;
 
-$con->execute($sql);
 $con->close();
 
 header("Location: " . $http_site_root . "/" . $return_url);
 
 /**
  * $Log: company-edit-2.php,v $
+ * Revision 1.2  2004/07/05 21:54:54  introspectshun
+ * - Now uses GetUpdateSQL
+ *
  * Revision 1.1  2004/07/01 19:48:10  braverock
  * - add new configurable relationships code
  *   - adapted from patches submitted by Neil Roberts
