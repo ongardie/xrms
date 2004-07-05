5d4d700ceb16cbe71098f83d84961df662d8e12d
introspectshun <introspectshun>
2004-07-05 21:17:01 +0000

    - Now uses GetInsertSQL - Updated add_audit_item to reflect new relationship table

--- companies/add-relationship.php
+++ companies/add-relationship.php
@@ -2,10 +2,9 @@
 /**
  * Add Relationship
  *
- * $Id: add-relationship.php,v 1.7 2004/07/01 19:49:13 braverock Exp $
+ * $Id: add-relationship.php,v 1.8 2004/07/05 21:17:01 introspectshun Exp $
  *
  * @todo put back in established at date parsing
- * @todo modify to use GetInsertSQL
  */
 
 require_once('../include-locations.inc');
@@ -14,6 +13,7 @@ require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
+require_once($include_directory . 'adodb-params.php');
 
 $session_user_id = session_check();
 
@@ -34,11 +34,19 @@ $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 // $con->debug = 1;
 
-$sql = "insert into relationships (from_what_id, to_what_id, relationship_type_id, established_at)
-    values (" . $from_what_id . ", " . $to_what_id . ", " . $relationship_type_id . ", now())";
-$con->execute($sql);
+$sql = "SELECT * FROM relationships WHERE 1 = 2"; //select empty record as placeholder
+$rst = $con->execute($sql);
 
-add_audit_item($con, $session_user_id, 'created', 'company_relationship', $company_id, 1);
+$rec = array();
+$rec['from_what_id'] = $from_what_id;
+$rec['to_what_id'] = $to_what_id;
+$rec['relationship_type_id'] = $relationship_type_id;
+$rec['established_at'] = time();
+
+$ins = $con->GetInsertSQL($rst, $rec, get_magic_quotes_gpc());
+$con->execute($ins);
+
+add_audit_item($con, $session_user_id, 'created', 'relationships', $company_id, 1);
 
 $con->close();
 
@@ -46,9 +54,24 @@ header("Location: relationships.php?company_id=$company_id");
 
 /**
  * $Log: add-relationship.php,v $
+ * Revision 1.8  2004/07/05 21:17:01  introspectshun
+ * - Now uses GetInsertSQL
+ * - Updated add_audit_item to reflect new relationship table
+ *
  * Revision 1.7  2004/07/01 19:49:13  braverock
  * - add new configurable relationships code
  *   - adapted from patches submitted by Neil Roberts
+ *
+ * Revision 1.6  2004/06/15 14:17:01  gpowers
+ * - correct time formats
+ *
+ * Revision 1.5  2004/06/12 17:10:24  gpowers
+ * - removed DBTimeStamp() calls for compatibility with GetInsertSQL() and
+ *   GetUpdateSQL()
+ *
+ * Revision 1.4  2004/06/12 05:03:16  introspectshun
+ * - Now use ADODB GetInsertSQL, GetUpdateSQL, date and Concat functions.
+ * - Corrected order of arguments to implode() function.
  *
  * Revision 1.3  2004/05/10 13:09:14  maulani
  * - add level to audit trail
@@ -56,5 +79,7 @@ header("Location: relationships.php?company_id=$company_id");
  * Revision 1.2  2004/03/26 20:55:59  maulani
  * - Add audit trail to company-related items
  * - Add phpdoc
+ *
+ *
  */
 ?>
