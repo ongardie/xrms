3b3d754738161a10555ec5130bc05a71036ff776
cpsource <cpsource>
2004-07-14 21:34:44 +0000

    - Attempt to fix undefine usage for     $contact_id     $company_id

--- notes/sidebar.php
+++ notes/sidebar.php
@@ -9,7 +9,7 @@ if ( !defined('IN_XRMS') )
 /**
  * Sidebar box for notes
  *
- * $Id: sidebar.php,v 1.10 2004/07/14 19:04:41 gpowers Exp $
+ * $Id: sidebar.php,v 1.11 2004/07/14 21:34:44 cpsource Exp $
  */
 
 $note_rows = '<div id="note_sidebar">
@@ -34,7 +34,7 @@ $note_rows = '<div id="note_sidebar">
 
 //build the notes sql query
 if (strlen($on_what_table)>0){
-    $note_sql = "select note_id, note_description, entered_by, entered_at, username from notes, users
+    $note_sql = "select note_id, note_description, entered_by, entered_at, username, user_contact_id from notes, users
             where notes.entered_by = users.user_id
             and on_what_table = '$on_what_table'
             and on_what_id = $on_what_id
@@ -42,7 +42,7 @@ if (strlen($on_what_table)>0){
             order by entered_at desc";
     $rst = $con->execute($note_sql);
 } else {
-    $note_sql = "select note_id, note_description, entered_by, entered_at, on_what_table, on_what_id, username from notes, users
+    $note_sql = "select note_id, note_description, entered_by, entered_at, on_what_table, on_what_id, username, user_contact_id from notes, users
             where notes.entered_by = '$session_user_id'
             and notes.entered_by = users.user_id
             and note_record_status = 'a'
@@ -53,63 +53,78 @@ if (strlen($on_what_table)>0){
 //uncomment the debug line to see what's going on with the query
 //$con->debug=1;
 
+//
+// Note:
+//
+// 1) derive $company_id from if notes.on_what_table = 'companies'
+//      from notes.on_what_id
+// 2) derive $contact_id from if notes.on_what_table = 'contacts'
+//      from notes.on_what_id
+//
+
 $rst = $con->execute($note_sql);
 
 if (strlen($rst->fields['username']) > 0) {
-    while (!$rst->EOF) {
-        $attached_to_link ='';
-        $on_what_name     ='';
-        $on_what_table    ='';
+  while (!$rst->EOF) {
+    $attached_to_link ='';
+    $on_what_name     ='';
+    $on_what_table    ='';
+    $company_id       ='';
+    $contact_id       ='';
 
-        if ($contact_id) {
-            $return_url = "&return_url=/contacts/one.php?contact_id=" . $contact_id;
-        } elseif ($company_id) {
-            $return_url = "&return_url=/companies/one.php?company_id=" . $company_id;
-        } else {
-            $return_url = "&return_url=/private/home.php";
-        }
-        if (strlen($rst->fields['on_what_table']) > 0) {
-            switch ($rst->fields['on_what_table']) {
-                case 'companies':
-                    $on_what_table = 'company';
-                    break;
-                case 'contacts':
-                    $on_what_table = 'contact';
-                    $on_what_name = " " . $con->Concat("last_name","', '","first_names") . " AS on_what_name ";
-                    break;
-                case 'opportunities':
-                    $on_what_table = 'opportunity';
-                    $on_what_name = 'opportunity_title AS on_what_name ';
-                    break;
-                case 'cases':
-                    $on_what_table = 'case';
-                    $on_what_name = 'case_title AS on_what_name ';
-                    break;
-                case 'campaigns':
-                    $on_what_table = 'campaign';
-                    $on_what_name = 'campaign_title AS on_what_name ';
-                    break;
-                case 'users':
-                    $on_what_table = 'user';
-                    $on_what_name = 'username AS on_what_name ';
-                    break;
-            }
-            if (!$on_what_name) { $on_what_name = $on_what_table.'_name as on_what_name '; }
-            $attached_sql = 'select '
-                           . $on_what_name.', '
-                           . $on_what_table.'_id '
-                           . ' from '.$rst->fields['on_what_table']
-                           . ' where '
-                           . $on_what_table.'_id = '. $rst->fields['on_what_id'];
-            $attached_rst = $con->SelectLimit($attached_sql, 1, 0);
-            if ($attached_rst) {
-                $attached_to_link = "<a href=\"$http_site_root/". $rst->fields['on_what_table']
-                                        .'/one.php?'. $on_what_table.'_id='
-                                        .$rst->fields['on_what_id']
-                                        .'">'.$attached_rst->fields['on_what_name'].'</a>';
-            }
-        }
-        $note_rows .= "
+    if (strlen($rst->fields['on_what_table']) > 0) {
+      switch ($rst->fields['on_what_table']) {
+      case 'companies':
+	$on_what_table = 'company';
+	$company_id    = $rst->fields['on_what_id'];
+	break;
+      case 'contacts':
+	$on_what_table = 'contact';
+	$on_what_name = " " . $con->Concat("last_name","', '","first_names") . " AS on_what_name ";
+	$contact_id   = $rst->fields['on_what_id'];
+	break;
+      case 'opportunities':
+	$on_what_table = 'opportunity';
+	$on_what_name = 'opportunity_title AS on_what_name ';
+	break;
+      case 'cases':
+	$on_what_table = 'case';
+	$on_what_name = 'case_title AS on_what_name ';
+	break;
+      case 'campaigns':
+	$on_what_table = 'campaign';
+	$on_what_name = 'campaign_title AS on_what_name ';
+	break;
+      case 'users':
+	$on_what_table = 'user';
+	$on_what_name = 'username AS on_what_name ';
+	break;
+      }
+      if (!$on_what_name) { $on_what_name = $on_what_table.'_name as on_what_name '; }
+      $attached_sql = 'select '
+	. $on_what_name.', '
+	. $on_what_table.'_id '
+	. ' from '.$rst->fields['on_what_table']
+	. ' where '
+	. $on_what_table.'_id = '. $rst->fields['on_what_id'];
+      $attached_rst = $con->SelectLimit($attached_sql, 1, 0);
+      if ($attached_rst) {
+	$attached_to_link = "<a href=\"$http_site_root/". $rst->fields['on_what_table']
+	  .'/one.php?'. $on_what_table.'_id='
+	  .$rst->fields['on_what_id']
+	  .'">'.$attached_rst->fields['on_what_name'].'</a>';
+      }
+    } // if (strlen($rst->fields['on_what_table']) > 0) ...
+
+    if ($contact_id) {
+      $return_url = "&return_url=/contacts/one.php?contact_id=" . $contact_id;
+    } elseif ($company_id) {
+      $return_url = "&return_url=/companies/one.php?company_id=" . $company_id;
+    } else {
+      $return_url = "&return_url=/private/home.php";
+    }
+
+    $note_rows .= "
              <tr>
                  <td class=widget_content>
                  <font class=note_label>
@@ -117,33 +132,33 @@ if (strlen($rst->fields['username']) > 0) {
                  </td>
                  <td class=widget_content>
                  <font class=note_label>"
-               . $con->userdate($rst->fields['entered_at'])
-               . "</td>\n\t<td class=widget_content>
+      . $con->userdate($rst->fields['entered_at'])
+      . "</td>\n\t<td class=widget_content>
                  <font class=note_label>"
-               . $rst->fields['username']
-               . "</td>\n\t<td class=widget_content>
+      . $rst->fields['username']
+      . "</td>\n\t<td class=widget_content>
                  <font class=note_label>
                  <a href='" . $http_site_root . "/notes/edit.php?note_id=" . $rst->fields['note_id'] . $return_url . "'>"
-               . _("View/Edit")
-               . "</a>
+      . _("View/Edit")
+      . "</a>
                  </font>
                  </td>
              </tr>";
-        $note_rows .= "
+    $note_rows .= "
              <tr>
                  <td class=widget_content colspan=4>
                  <font class=note_label>"
-               . nl2br(substr($rst->fields['note_description'],0,255)) .'
+      . nl2br(substr($rst->fields['note_description'],0,255)) .'
                  </td>
              </tr>';
-        $rst->movenext();
-    }
-    $rst->close();
+    $rst->movenext();
+  } // while (!$rst->EOF) ..
+  $rst->close();
 } else {
     $note_rows .= "\n            <tr> <td class=widget_content colspan=4> "
                  . _("No attached notes")
                  . " </td> </tr>\n";
-}
+} // if (strlen($rst->fields['username']) > 0) ...
 
 //put in the new button
 if (strlen($on_what_table)>0){
@@ -165,6 +180,11 @@ $note_rows .= "        </table>\n</div>";
 
 /**
  * $Log: sidebar.php,v $
+ * Revision 1.11  2004/07/14 21:34:44  cpsource
+ * - Attempt to fix undefine usage for
+ *     $contact_id
+ *     $company_id
+ *
  * Revision 1.10  2004/07/14 19:04:41  gpowers
  * - added $http_site_root to Edit link
  *   - needed for calls from plugins
