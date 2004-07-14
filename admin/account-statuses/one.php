bfd9bbfc88b03bafbc2f14eac06d55fd2ada20f2
braverock <braverock>
2004-07-14 16:28:09 +0000

    - applied modified version of i18n conversion submitted by Sebastian Becker (hyperpac)

--- admin/account-statuses/one.php
+++ admin/account-statuses/one.php
@@ -4,7 +4,7 @@
  *
  * Edit account-status
  *
- * $Id: one.php,v 1.4 2004/06/14 18:17:43 introspectshun Exp $
+ * $Id: one.php,v 1.5 2004/07/14 16:28:09 braverock Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -26,18 +26,18 @@ $sql = "select * from account_statuses where account_status_id = $account_status
 $rst = $con->execute($sql);
 
 if ($rst) {
-    
+
     $account_status_short_name = $rst->fields['account_status_short_name'];
     $account_status_pretty_name = $rst->fields['account_status_pretty_name'];
     $account_status_pretty_plural = $rst->fields['account_status_pretty_plural'];
     $account_status_display_html = $rst->fields['account_status_display_html'];
-    
+
     $rst->close();
 }
 
 $con->close();
 
-$page_title = "One Account Status : $account_status_pretty_name";
+$page_title = _("One Account Status:")." $account_status_pretty_name";
 start_page($page_title);
 
 ?>
@@ -45,64 +45,70 @@ start_page($page_title);
 <table border=0 cellpadding=0 cellspacing=0>
     <tr>
         <td class=lcol width=25% valign=top>
-        
+
         <form action=edit-2.php method=post>
-        <input type=hidden name=account_status_id value="<?php  echo $account_status_id; ?>">
+        <input type=hidden name=account_status_id value="<?php echo $account_status_id; ?>">
         <table class=widget cellspacing=1>
             <tr>
-                <td class=widget_header colspan=4>Edit Account Status Information</td>
+                <td class=widget_header colspan=4><?php echo _("Edit Account Status Information"); ?></td>
             </tr>
             <tr>
-                <td class=widget_label_right>Short Name</td>
-                <td class=widget_content_form_element><input type=text size=10 name=account_status_short_name value="<?php  echo $account_status_short_name; ?>"></td>
+                <td class=widget_label_right><?php echo _("Short Name"); ?></td>
+                <td class=widget_content_form_element>
+                    <input type=text size=10 name=account_status_short_name value="<?php echo $account_status_short_name; ?>">
+                </td>
             </tr>
             <tr>
-                <td class=widget_label_right>Full Name</td>
+                <td class=widget_label_right><?php echo _("Full Name"); ?></td>
                 <td class=widget_content_form_element><input type=text size=20 name=account_status_pretty_name value="<?php  echo $account_status_pretty_name; ?>"></td>
             </tr>
             <tr>
-                <td class=widget_label_right>Full Plural</td>
+                <td class=widget_label_right>_("Full Plural")</td>
                 <td class=widget_content_form_element><input type=text size=20 name=account_status_pretty_plural value="<?php  echo $account_status_pretty_plural; ?>"></td>
             </tr>
             <tr>
-                <td class=widget_label_right>Display HTML</td>
+                <td class=widget_label_right><?php echo _("Display HTML"); ?></td>
                 <td class=widget_content_form_element><input type=text size=30 name=account_status_display_html value="<?php  echo $account_status_display_html; ?>"></td>
             </tr>
             <tr>
-                <td class=widget_content_form_element colspan=2><input class=button type=submit value="Save Changes"></td>
+                <td class=widget_content_form_element colspan=2><input class=button type=submit value="<?php echo _("Save Changes"); ?>"></td>
             </tr>
         </table>
         </form>
 
         <form action=delete.php method=post>
-        <input type=hidden name=account_status_id value="<?php  echo $account_status_id; ?>" onsubmit="javascript: return confirm('Delete Account Status?');">
+        <input type=hidden name=account_status_id value="<?php  echo $account_status_id; ?>" onsubmit="javascript: return confirm('<?php echo _("Delete Account Status?"); ?>');">
         <table class=widget cellspacing=1>
             <tr>
-                <td class=widget_header colspan=4>Delete Account Status</td>
+                <td class=widget_header colspan=4><?php echo _("Delete Account Status"); ?></td>
             </tr>
             <tr>
                 <td class=widget_content>
-                Click the button below to remove this account status from the system.
-                <p>Note: This action CANNOT be undone!
-                <p><input class=button type=submit value="Delete Account Status">
+                <?php
+                    echo _("Click the button below to remove this account status from the system.")
+                       . '<p>'
+                       . _("Note: This action CANNOT be undone!");
+                ?>
+                <p>
+                <input class=button type=submit value="<?php echo _("Delete Account Status"); ?>">
                 </td>
             </tr>
         </table>
         </form>
-        
+
         </td>
-        
+
         <!-- gutter //-->
         <td class=gutter width=2%>
         &nbsp;
         </td>
-        
+
         <!-- right column //-->
-        
+
         <td class=rcol width=73% valign=top>
         &nbsp;
         </td>
-        
+
     </tr>
 </table>
 
@@ -112,6 +118,9 @@ end_page();
 
 /**
  * $Log: one.php,v $
+ * Revision 1.5  2004/07/14 16:28:09  braverock
+ * - applied modified version of i18n conversion submitted by Sebastian Becker (hyperpac)
+ *
  * Revision 1.4  2004/06/14 18:17:43  introspectshun
  * - Add adodb-params.php include for multi-db compatibility.
  * - Now use ADODB GetInsertSQL, GetUpdateSQL functions.
@@ -121,7 +130,6 @@ end_page();
  *
  * Revision 1.2  2004/03/24 18:12:44  maulani
  * - add phpdoc
- *
  *
  */
 ?>
