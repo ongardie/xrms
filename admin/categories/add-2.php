bbc0f80370f24941283fd1799caed329b249df47
introspectshun <introspectshun>
2004-07-15 21:51:56 +0000

    - Now passes a table name instead of a recordset into GetInsertSQL

--- admin/categories/add-2.php
+++ admin/categories/add-2.php
@@ -18,16 +18,15 @@ $session_user_id = session_check();
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 
-$sql = "SELECT * FROM categories WHERE 1 = 2"; //select empty record as placeholder
-$rst = $con->execute($sql);
-
+//save to database
 $rec = array();
 $rec['category_short_name'] = $category_short_name;
 $rec['category_pretty_name'] = $category_pretty_name;
 $rec['category_pretty_plural'] = $category_pretty_plural;
 $rec['category_display_html'] = $category_display_html;
 
-$ins = $con->GetInsertSQL($rst, $rec, get_magic_quotes_gpc());
+$tbl = "categories";
+$ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
 $con->execute($ins);
 
 $con->close();
