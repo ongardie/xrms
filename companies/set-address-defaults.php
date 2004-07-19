f374f2e1b4f06d0aa11ce8a94d4cdab2ee6ee0c0
cpsource <cpsource>
2004-07-19 13:56:15 +0000

    - Get rid of uninitialized variable usage.

--- companies/set-address-defaults.php
+++ companies/set-address-defaults.php
@@ -11,16 +11,17 @@ require_once($include_directory . 'adodb-params.php');
 $session_user_id = session_check();
 
 $company_id = $_POST['company_id'];
-$default_primary_address = $_POST['default_primary_address'];
+
+$default_primary_address = isset($_POST['default_primary_address']) ? $_POST['default_primary_address'] : '';
 if (!$default_primary_address) {$default_primary_address=0;}
 
-$default_billing_address = $_POST['default_billing_address'];
+$default_billing_address = isset($_POST['default_billing_address']) ? $_POST['default_billing_address'] : '';
 if (!$default_billing_address) {$default_billing_address=0;}
 
-$default_shipping_address = $_POST['default_shipping_address'];
+$default_shipping_address = isset($_POST['default_shipping_address']) ? $_POST['default_shipping_address'] : '';
 if (!$default_shipping_address) {$default_shipping_address=0;}
 
-$default_payment_address = $_POST['default_payment_address'];
+$default_payment_address = isset($_POST['default_payment_address']) ? $_POST['default_payment_address'] : '';
 if (!$default_payment_address) {$default_payment_address=0;}
 
 $con = &adonewconnection($xrms_db_dbtype);
