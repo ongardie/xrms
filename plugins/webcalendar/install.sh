a2dbb2ad1d355d4311e58ec9c891c4085033cd07
gpowers <gpowers>
2004-07-22 16:24:42 +0000

    - this is an install shell script for webcalendar.   - it will download the webcalendar source and copy the patched files

---
+++ plugins/webcalendar/install.sh
@@ -0,0 +1,23 @@
+echo "This script should install WebCalendar and patch it to work with XRMS."
+echo "If you have a problem, please contact gpowers@users.sourceforge.net"
+echo ""
+echo "Trying to get WebCalendar from easynews mirror."
+echo "If this fails, get the current download link from"
+echo "http://sourceforge.net/projects/webcalendar/"
+wget http://easynews.dl.sourceforge.net/sourceforge/webcalendar/WebCalendar-0.9.43.tgz
+echo "Untarring Archive:"
+tar xzvf WebCalendar-0.9.43.tgz
+echo "Creating Symbolic link to src"
+ln -s WebCalendar-0.9.43 src
+echo ""
+echo "your php binary is at:"
+which php
+echo "check php binary location (on the first line) in tools/send_reminders.php"echo "it should match the location above. Launching vi for you."
+echo "If you don't know how to use vi, presss : q ! to exit vi and use pico"
+echo "Waiting for you to read this."
+sleep 5
+vi src/tools/send_reminders.php
+echo "Copying patched files to src. I'm not making a backup. You have the tarball."
+cp config.php src/include
+cp user-xrms.php src/include
+cp init.php src/include
