e2c43f652f1194dda12aebd8a97bbc4ca5bc1d2c
gpowers <gpowers>
2004-07-22 12:57:27 +0000

    - Removed references to 'demo' plugin.

--- plugins/mrtg/setup.php
+++ plugins/mrtg/setup.php
@@ -18,14 +18,13 @@
  * in another file.  It will improve the performance of the entire
  * system.
  *
- * $Id: setup.php,v 1.1 2004/05/06 15:13:58 gpowers Exp $
+ * $Id: setup.php,v 1.2 2004/07/22 12:57:27 gpowers Exp $
  */
 
 
 function xrms_plugin_init_mrtg() {
     global $xrms_plugin_hooks;
     $xrms_plugin_hooks['menuline']['mrtg'] = 'mrtg';
-    //$xrms_plugin_hooks['opportunity_detail']['demo'] = 'need_function';
 }
 
 
@@ -33,29 +32,14 @@ function mrtg() {
 
     global $http_site_root;
 
-    //Add Demo link to upper menu
-    echo "&nbsp;<a href='$http_site_root/plugins/mrtg/mrtg.php'>MRTG</a>&nbsp;&bull;\n";
+    //Add link to upper menu
+    echo "&nbsp;<a href='$http_site_root/plugins/mrtg/mrtg.php'>" . _("MRTG") . "</a>&nbsp;&bull;\n";
 }
 
 /**
  * $Log: setup.php,v $
- * Revision 1.1  2004/05/06 15:13:58  gpowers
- * This is a simple plugin for including an MRTG page in XRMS.
- *
- * Revision 1.5  2004/05/06 14:10:18  gpowers
- * Oops. Was the wrong file. Corrected.
- *
- * Revision 1.4  2004/03/29 21:14:40  maulani
- * - Add opportunity_detail hook as a commented-out example
- *
- * Revision 1.3  2004/03/20 22:49:42  braverock
- * - need global $http_site_root in some cases
- *
- * Revision 1.2  2004/03/20 22:43:02  braverock
- * - changed to use $http_site_root
- *
- * Revision 1.1  2004/03/20 20:09:35  braverock
- * Initial Revision of Demo plugin to demonstrate using hooks
+ * Revision 1.2  2004/07/22 12:57:27  gpowers
+ * - Removed references to 'demo' plugin.
  *
  */
 ?>
