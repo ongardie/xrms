cb7b56527bfe69dbcd5441bfc29fa3016e63d559
gpowers <gpowers>
2004-07-22 18:52:18 +0000

    - hid unimplemented features on menu

--- plugins/useradmin/menu.php
+++ plugins/useradmin/menu.php
@@ -1,8 +1,8 @@
     &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/onlineusers.php">Online Users</a>&nbsp;&bull;
     &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/lastusers.php">Last Users</a>&nbsp;&bull;
     &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/lastlogins.php">Last Logins</a>&nbsp;&bull;
-    &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/fingeruser.php">Finger User</a>&nbsp;&bull;
+    &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/fingeruser.php">Finger User</a>&nbsp;<!-- &bull;
     &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/adduser.php">Add User</a>&nbsp;&bull;
     &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/chpass.php">Change Password</a>&nbsp;&bull;
-    &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/deluser.php">Delete User</a>&nbsp;
+    &nbsp;<a href="<?php echo $http_site_root; ?>/plugins/useradmin/deluser.php">Delete User</a>&nbsp;-->
 <p>
