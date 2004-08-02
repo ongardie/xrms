32047080d15246a6281fe12f464ac39937767074
gpowers <gpowers>
2004-08-02 14:44:59 +0000

    - merged plugins/voicemail into cti/

--- plugins/cti/setup.php
+++ plugins/cti/setup.php
@@ -11,13 +11,20 @@
 function xrms_plugin_init_cti() {
     global $xrms_plugin_hooks;
     $xrms_plugin_hooks['one_contact_buttons']['cti'] = 'asteriskdial';
+//    $xrms_plugin_hooks['menuline']['cti'] = 'voicemail';
 }
 
 function asteriskdial() {
     global $http_site_root;
     global $work_phone;
     global $contact_id;
-    echo "<input class=button type=button value=\"" . _("Dial") . "\" onclick=\"javascript: location.href='" . $http_site_root . "/plugins/cti/asteriskdial.php?contact_id=" . $contact_id . "&phone=" . $work_phone . "';\">";
+    global $company_id;
+    echo "<input class=button type=button value=\"" . _("Dial") . "\" onclick=\"javascript: location.href='" . $http_site_root . "/plugins/cti/asteriskdial.php?company_id=" . $company_id . "&contact_id=" . $contact_id . "&phone=" . $work_phone . "';\">";
+}
+
+function voicemail() {
+    global $http_site_root;
+    echo "&nbsp;<a href='$http_site_root/plugins/cti/voicemail.php'>" . _("Voice Mail") . "</a>&nbsp;&bull;\n";
 }
 
 ?>
