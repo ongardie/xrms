7a0452da22fb2e4b2bca3dce55b6e4ba0427d593
gpowers <gpowers>
2004-08-02 15:51:14 +0000

    - moved Vcard button link from contacts/one.php to this plugin   - we don't use this function and don't want it cluttering the interface

---
+++ plugins/vcard/setup.php
@@ -0,0 +1,22 @@
+<?php
+/*
+*
+* Vcard plugin
+*
+*/
+
+function xrms_plugin_init_vcard() {
+    global $xrms_plugin_hooks;
+    $xrms_plugin_hooks['one_contact_buttons']['vcard'] = 'vcard';
+}
+
+function vcard() {
+    global $http_site_root;
+    global $custom1;
+    global $custom2;
+    global $contact_id;
+    echo " <input class=button type=button value=\"" .  _("Vcard") ."\" onclick=\"javascript: location.href='vcard.php?contact_id=" . $contact_id . "';\">";
+
+}
+
+?>
