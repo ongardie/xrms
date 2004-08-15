f54898e9d8444a7aa57701e62cff13ff2c0a6a6e
niclowe <niclowe>
2004-08-15 07:18:11 +0000

    added javascript form validation, increased/decreased certain field lengths.

--- plugins/webform/contact.php
+++ plugins/webform/contact.php
@@ -16,6 +16,34 @@ $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 $con->debug = 1;
 ?>
+<script language="JavaScript">
+<!--
+function MM_findObj(n, d) { //v4.0
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && document.getElementById) x=document.getElementById(n); return x;
+}
+
+function MM_validateForm() { //v4.0
+  var i,p,q,nm,test,num,min,max,errors='',args=MM_validateForm.arguments;
+  for (i=0; i<(args.length-2); i+=3) { test=args[i+2]; val=MM_findObj(args[i]);
+    if (val) { nm=val.name; if ((val=val.value)!="") {
+      if (test.indexOf('isEmail')!=-1) { p=val.indexOf('@');
+        if (p<1 || p==(val.length-1)) errors+='- '+nm+' must contain an e-mail address.\n';
+      } else if (test!='R') {
+        if (isNaN(val)) errors+='- '+nm+' must contain a number.\n';
+        if (test.indexOf('inRange') != -1) { p=test.indexOf(':');
+          min=test.substring(8,p); max=test.substring(p+1);
+          if (val<min || max<val) errors+='- '+nm+' must contain a number between '+min+' and '+max+'.\n';
+    } } } else if (test.charAt(0) == 'R') errors += '- '+nm+' is required.\n'; }
+  } if (errors) alert('The following error(s) occurred:\n'+errors);
+  document.MM_returnValue = (errors == '');
+}
+//-->
+</script>
+
 <form enctype='multipart/form-data' action='<? echo $http_site_root; ?>/plugins/webform/new-form.php' method='post'>
 <input type="hidden" name="user_id" value="1">
 <input type="hidden" name="crm_status_id" value="1">
@@ -27,14 +55,14 @@ $con->debug = 1;
     <tr> 
       <td>First Name</td>
       <td class=widget_content_form_element> 
-        <input type=text name='first_names' size=40>
+        <input type=text name='first_names' size=35>
         <?php echo $required_indicator; ?>
       </td>
     </tr>
     <tr> 
       <td>Last Name 
       <td class=widget_content_form_element> 
-        <input type=text name='last_name' size=40>
+        <input type=text name='last_name' size=35>
         <?php echo $required_indicator; ?>
       </td>
     </tr>
@@ -54,19 +82,20 @@ $con->debug = 1;
     <tr> 
       <td>Contact Phone</td>
       <td class=widget_content_form_element> 
-        <input type=text name='phone' size=35>
+        <input type=text name='phone' size=15>
+        <?php echo $required_indicator; ?>
       </td>
     </tr>
     <tr> 
       <td>Post Code </td>
       <td class=widget_content_form_element> 
-        <input type="text" name="postal_code" size="8" maxlength="4">
+        <input type="text" name="postal_code" size="4" maxlength="4">
       </td>
     </tr>
     <tr> 
       <td>Suburb</td>
       <td  class=widget_content_form_element> 
-        <input type="text" name="city" size="8" maxlength="100">
+        <input type="text" name="city" size="35" maxlength="100">
         <?php echo $required_indicator; ?>
       </td>
     </tr>
@@ -115,7 +144,8 @@ $rst->close()
     </tr>
   </table>
                  
-                <input type=submit value='Submit Form' name="submit">
+                
+  <input type=submit value='Submit Form' name="submit" onClick="MM_validateForm('first_names','','R','last_name','','R','email','','RisEmail','phone','','R','city','','R','YourMessage','','R');return document.MM_returnValue">
                 <input type=reset value='Reset Form' name="reset">
                  
               </form>
