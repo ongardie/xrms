388f3cc48d79eb22d973f7832e0ef4902c5a566c
johnfawcett <johnfawcett>
2004-08-17 10:42:55 +0000

    - strings extracted from install/data.php (for the moment just industries   table)

---
+++ locale/datastrings.php
@@ -0,0 +1,23 @@
+<?php
+//File generated automatically by getdbstrings.php. Do not modify directly.
+$s=_( 'Other');
+$s=_( 'Advertising');
+$s=_( 'Architecture');
+$s=_( 'Chemicals');
+$s=_( 'Communications');
+$s=_( 'Computers');
+$s=_( 'Construction');
+$s=_( 'Consulting');
+$s=_( 'Distribution');
+$s=_( 'Education');
+$s=_( 'Finance');
+$s=_( 'Government');
+$s=_( 'Healthcare');
+$s=_( 'Insurance');
+$s=_( 'Legal');
+$s=_( 'Manufacturing');
+$s=_( 'Non-Profit');
+$s=_( 'Real Estate');
+$s=_( 'Restaurant');
+$s=_( 'Retail');
+?>
