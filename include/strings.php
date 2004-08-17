308ba40c420f5543a5129e27e7a7fb0a2af6e32b
johnfawcett <johnfawcett>
2004-08-17 10:50:42 +0000

    - contains strings which are not picked up by gettext extraction.

---
+++ include/strings.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Strings
+ *
+ * This file is never used by the application. It contains
+ * the strings which are partly or totally composed from
+ * database lookups and which otherwise would not get into
+ * the string translation file.
+ * Any strings which are configurable by the admin user are not
+ * included.
+ *
+ * @author John Fawcett
+ */
+
+// new-relationship.php
+$s = _("Add Contact");
+$s = _("Add Company");
+$s = _("Search for Contact");
+$s = _("Search for Company");
+// new-relationship-2.php
+$s = _("Add Contact");
+$s = _("Add Company");
+$s = _("There is no contact by that name");
+$s = _("There is no company by that name");
+// sidebar.php
+$s = _("Associated Contacts");
+$s = _("Associated Companies");
+$s = _("Other Contacts");
+$s = _("Other Companies");
+$s = _("Contacts");
+$s = _("Companies");
+$s = _("No attached contacts");
+$s = _("No attached companies");
+
+?>
