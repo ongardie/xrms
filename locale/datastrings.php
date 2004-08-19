7a748ecb7f9590bfd8dc65b549accee33c6ac9d8
braverock <braverock>
2004-08-19 15:05:34 +0000

    - added new strings from additional tables - NOTE: some php code is also gettign extracted by the getdatastrings.php script,                we need to review and correct this

--- locale/datastrings.php
+++ locale/datastrings.php
@@ -1,23 +1,150 @@
 <?php
-//File generated automatically by getdbstrings.php. Do not modify directly.
-$s=_( 'Other');
-$s=_( 'Advertising');
-$s=_( 'Architecture');
-$s=_( 'Chemicals');
-$s=_( 'Communications');
-$s=_( 'Computers');
-$s=_( 'Construction');
-$s=_( 'Consulting');
-$s=_( 'Distribution');
-$s=_( 'Education');
-$s=_( 'Finance');
-$s=_( 'Government');
-$s=_( 'Healthcare');
-$s=_( 'Insurance');
-$s=_( 'Legal');
-$s=_( 'Manufacturing');
-$s=_( 'Non-Profit');
-$s=_( 'Real Estate');
-$s=_( 'Restaurant');
-$s=_( 'Retail');
+/**
+ * File generated automatically by getdatastrings.php. Do not modify directly.
+ *
+ * This file is included in XRMS CVS as a convenience to translators.
+ * generate a new version of this file by running
+ * php ./getdatastrings.php
+ * from the locale directory
+ *
+ * $Id: datastrings.php,v 1.2 2004/08/19 15:05:34 braverock Exp $
+ */
+$s=_('N/A');
+$s=_('Closed');
+$s=_('Hold');
+$s=_('Approved');
+$s=_('<font color=#999999><b>N/A</b></font>');
+$s=_('<font color=#cc0000><b>Closed</b></font>');
+$s=_('<font color=#ff9933><b>Hold</b></font>');
+$s=_('<font color=#009900><b>Approved</b></font>');
+$s=_('call to');
+$s=_('call from');
+$s=_('e-mail to');
+$s=_('e-mail from');
+$s=_('fax to');
+$s=_('fax from');
+$s=_('letter to');
+$s=_('letter from');
+$s=_('internal');
+$s=_('process');
+$s=_('calls to');
+$s=_('calls from');
+$s=_('e-mails to');
+$s=_('e-mails from');
+$s=_('faxes to');
+$s=_('faxes from');
+$s=_('call to',1);
+$s=_('call from',2);
+$s=_('e-mail to',3);
+$s=_('e-mail from',4);
+$s=_('fax to',5);
+$s=_('fax from',6);
+$s=_('letter to',7);
+$s=_('letter from',8);
+$s=_('internal',9);
+$s=_('process',10);
+$s=_('Other');
+$s=_('E-Mail');
+$s=_('Phone');
+$s=_('Mail');
+$s=_('Event');
+$s=_('Magazine');
+$s=_('Television');
+$s=_('Critical');
+$s=_('High');
+$s=_('Medium');
+$s=_('Low');
+$s=_('o');
+$s=_('c');
+$s=_('NEW');
+$s=_('OPEN');
+$s=_('PRO');
+$s=_('FIN');
+$s=_('CLO');
+$s=_('New', 'New', 'New');
+$s=_('Open', 'Open', 'Open');
+$s=_('In Progress', 'In Progress', 'In Progress');
+$s=_('Finished', 'Finished', 'Finished');
+$s=_('Closed', 'Closed', 'Closed');
+$s=_('Help Item');
+$s=_('Bug');
+$s=_('Feature Request');
+$s=_('Help Items');
+$s=_('Bugs');
+$s=_('Feature Requests');
+$s=_('Lead');
+$s=_('Prospect');
+$s=_('Qualified');
+$s=_('Developed');
+$s=_('Leads');
+$s=_('Prospects');
+$s=_('Customer');
+$s=_('Vendor');
+$s=_('Partner');
+$s=_('Competitor');
+$s=_('Special');
+$s=_('Customers');
+$s=_('Vendors');
+$s=_('Partners');
+$s=_('Competitors');
+$s=_(-5)";
+        $rst = $con->execute($sql);
+        $sql ="insert into system_parameters (param_id);
+$s=_('Fast')";
+        $rst = $con->execute($sql);
+    }
+
+    // categories
+    if (confirm_no_records($con);
+$s=_(int_val) values ('Audit Level');
+$s=_('categories')) {
+        $sql ="insert into categories (category_short_name);
+$s=_(4);
+$s=_(category_pretty_name, category_pretty_plural, category_display_html) values ('TEST1', 'Test Category 1', 'Test Category 1', 'Test Category 1');
+$s=_('Advertisement');
+$s=_('Direct Mail');
+$s=_('Radio');
+$s=_('Search Engine');
+$s=_('Seminar');
+$s=_('Telemarketing');
+$s=_('Trade Show');
+$s=_('Web Site');
+$s=_('Word of Mouth');
+$s=_('Advertisements');
+$s=_('Search Engines');
+$s=_('Seminars');
+$s=_('Telemarketings');
+$s=_('Trade Shows');
+$s=_('Web Sites');
+$s=_('w');
+$s=_('l');
+$s=_('PRE');
+$s=_('DIS');
+$s=_('NEG');
+$s=_('CLW');
+$s=_('CLL');
+$s=_('Preliminaries', 'Preliminaries', 'Preliminaries');
+$s=_('Discussion', 'Discussion', 'Discussion');
+$s=_('Negotiation', 'Negotiation', 'Negotiation');
+$s=_('Closed/Won', 'Closed/Won', 'Closed/Won');
+$s=_('Closed/Lost', 'Closed/Lost', 'Closed/Lost')";        $rst = $con->execute($sql);
+$s=_('Advertising');
+$s=_('Architecture');
+$s=_('Chemicals');
+$s=_('Communications');
+$s=_('Computers');
+$s=_('Construction');
+$s=_('Consulting');
+$s=_('Distribution');
+$s=_('Education');
+$s=_('Finance');
+$s=_('Government');
+$s=_('Healthcare');
+$s=_('Insurance');
+$s=_('Legal');
+$s=_('Manufacturing');
+$s=_('Non-Profit');
+$s=_('Real Estate');
+$s=_('Restaurant');
+$s=_('Retail');
 ?>
