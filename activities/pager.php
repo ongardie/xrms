730562c77374049a84c36861f844bca0593215e1
maulani <maulani>
2004-08-19 13:09:40 +0000

    - Reorder code - Add comments

--- activities/pager.php
+++ activities/pager.php
@@ -1,18 +1,17 @@
 <?php
+/**
+ * /activities/pager.php
+ *
+ * An include file to override ADODB_Pager to implement activities specific functions
+ *
+ * $Id: pager.php,v 1.3 2004/08/19 13:09:40 maulani Exp $
+ */
 
 if ( !defined('IN_XRMS') )
 {
   die('Hacking attempt');
   exit;
 }
-
-/**
- * /activities/pager.php
- *
- * An include file to override ADODB_Pager to implement activities specific functions
- *
- * $Id: pager.php,v 1.2 2004/08/18 08:09:46 cpsource Exp $
- */
 
 // include adodb pager class
 //show_test_values('sdklfl');
@@ -21,11 +20,15 @@ require_once($include_directory . 'adodb/adodb-pager.inc.php');
 // Create activities_pager as a child of adodb_pager
 class Activities_Pager extends ADODB_Pager{
 
+    //------------------------------------------------------
+    // Constructor calls the parent constructor
     function Activities_Pager(&$db, $sql, $selected_column=1, $selected_column_html='*')
     {
          $this->ADODB_Pager($db, $sql, 'activities', false, $selected_column, $selected_column_html);
     }
 
+    //------------------------------------------------------
+    // overridden to add export and mail merge
     function RenderLayout($header,$grid,$footer,$attributes='class=widget cellspacing=1 cellpadding=0 border=0 width="100%"')
     {
         echo "<table " . $attributes . ">",
@@ -54,6 +57,10 @@ class Activities_Pager extends ADODB_Pager{
 
 /**
  * $Log: pager.php,v $
+ * Revision 1.3  2004/08/19 13:09:40  maulani
+ * - Reorder code
+ * - Add comments
+ *
  * Revision 1.2  2004/08/18 08:09:46  cpsource
  * - Added hacking attempt code
  *   Added <nl> at eof
@@ -61,7 +68,6 @@ class Activities_Pager extends ADODB_Pager{
  * Revision 1.1  2004/08/16 14:39:23  maulani
  * - Override ADODB_Pager class for activities to allow customization
  * - Customization still todo
- *
  * 
  */
 ?>
