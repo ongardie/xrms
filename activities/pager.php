095e1815a570e2eca881142d38f24c9e0a381be2
introspectshun <introspectshun>
2004-08-25 15:49:02 +0000

    - Localized strings for i18n/l10n support

--- activities/pager.php
+++ activities/pager.php
@@ -4,12 +4,12 @@
  *
  * An include file to override ADODB_Pager to implement activities specific functions
  *
- * $Id: pager.php,v 1.3 2004/08/19 13:09:40 maulani Exp $
+ * $Id: pager.php,v 1.4 2004/08/25 15:49:02 introspectshun Exp $
  */
 
 if ( !defined('IN_XRMS') )
 {
-  die('Hacking attempt');
+  die(_("Hacking attempt"));
   exit;
 }
 
@@ -32,7 +32,7 @@ class Activities_Pager extends ADODB_Pager{
     function RenderLayout($header,$grid,$footer,$attributes='class=widget cellspacing=1 cellpadding=0 border=0 width="100%"')
     {
         echo "<table " . $attributes . ">",
-        "<tr><td colspan=13 class=widget_header>Search Results</td></tr>\n";
+        "<tr><td colspan=13 class=widget_header>" . _("Search Results") . "</td></tr>\n";
 
         if ($header != '&nbsp;') {
             echo "<tr><td colspan=13>",
@@ -46,8 +46,8 @@ class Activities_Pager extends ADODB_Pager{
 
         if ($this->how_many_rows > 0)           
         {
-            echo "<tr><td class=widget_content_form_element colspan=10><input type=button class=button onclick=\"javascript: exportIt();\" value='Export'> ";
-            echo "<input type=button class=button onclick=\"javascript: bulkEmail();\" value='Mail Merge'></td></tr>";
+            echo "<tr><td class=widget_content_form_element colspan=10><input type=button class=button onclick=\"javascript: exportIt();\" value='" . _("Export") . "'> ";
+            echo "<input type=button class=button onclick=\"javascript: bulkEmail();\" value='" . _("Mail Merge") . "'></td></tr>";
         }
 
         echo "</table>";
@@ -57,6 +57,9 @@ class Activities_Pager extends ADODB_Pager{
 
 /**
  * $Log: pager.php,v $
+ * Revision 1.4  2004/08/25 15:49:02  introspectshun
+ * - Localized strings for i18n/l10n support
+ *
  * Revision 1.3  2004/08/19 13:09:40  maulani
  * - Reorder code
  * - Add comments
