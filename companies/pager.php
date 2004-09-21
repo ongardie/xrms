d74762f02698ac4b923d4c9c6865fbafa28528d6
introspectshun <introspectshun>
2004-09-21 18:29:34 +0000

    - Finished localizing strings for i18n compatibility

--- companies/pager.php
+++ companies/pager.php
@@ -4,12 +4,12 @@
  *
  * An include file to override ADODB_Pager to implement companies specific functions
  *
- * $Id: pager.php,v 1.2 2004/08/26 22:35:07 niclowe Exp $
+ * $Id: pager.php,v 1.3 2004/09/21 18:29:34 introspectshun Exp $
  */
 
 if ( !defined('IN_XRMS') )
 {
-  die('Hacking attempt');
+  die(_("Hacking attempt"));
   exit;
 }
 
@@ -32,7 +32,7 @@ class Companies_Pager extends ADODB_Pager{
     function RenderLayout($header,$grid,$footer,$attributes='class=widget cellspacing=1 cellpadding=0 border=0 width="100%"')
     {
         echo "<table " . $attributes . ">",
-        "<tr><td colspan=13 class=widget_header>Search Results</td></tr>\n";
+        "<tr><td colspan=13 class=widget_header>" . _("Search Results") . "</td></tr>\n";
 
         if ($header != '&nbsp;') {
             echo "<tr><td colspan=13>",
@@ -46,7 +46,7 @@ class Companies_Pager extends ADODB_Pager{
 
         if ($this->how_many_rows > 0)           
         {
-            echo "<tr><td class=widget_content_form_element colspan=10><input type=button class=button onclick=\"javascript: exportIt();\" value=\"". _('Export')."-Not Coded Yet\"> ";
+            echo "<tr><td class=widget_content_form_element colspan=10><input type=button class=button onclick=\"javascript: exportIt();\" value=\"". _('Export') ." - " . _("Not Coded Yet") . "\"> ";
             echo "<input type=button class=button onclick=\"javascript: bulkEmail();\" value=". _('Mail Merge')." ></td></tr>";
         }
 
@@ -57,6 +57,9 @@ class Companies_Pager extends ADODB_Pager{
 
 /**
  * $Log: pager.php,v $
+ * Revision 1.3  2004/09/21 18:29:34  introspectshun
+ * - Finished localizing strings for i18n compatibility
+ *
  * Revision 1.2  2004/08/26 22:35:07  niclowe
  * localised the mail merge & export buttons, and enabled mail merge functionality for companies
  *
