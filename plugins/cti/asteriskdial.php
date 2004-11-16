ee68405fa4072ef0b28b76633a4f43a5898781a9
gpowers <gpowers>
2004-11-16 01:38:58 +0000

    - added code to use contact.custom1..3 fields for Channel, Extension and CalledID for each XRMS user.

--- plugins/cti/asteriskdial.php
+++ plugins/cti/asteriskdial.php
@@ -12,6 +12,7 @@
 
 /*
  *
+ * If you are using the sip.conf based lookupCID,
  * Be sure to add crm_username(s) to your sip.conf file. See below.
  *
  * IF Asterisk is running on the same server as XRMS,
@@ -29,8 +30,6 @@
  *
  * ismaeljcarlo@users.sourceforge.net created this function which looks up
  * the value of crm_username from sip.conf and returns the proper extension.
- * Also, his call files had a different syntax due to how my
- * server is configured.
  *
  */
 
@@ -59,29 +58,67 @@ require_once('../../include-locations.inc');
 require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
+require_once($include_directory . 'adodb/adodb.inc.php');
+require_once($include_directory . 'adodb-params.php');
 
-$session_user_id = session_check();
+$con = &adonewconnection($xrms_db_dbtype);
+$con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
+// $con->debug = 1;
+
+$session_user_id = session_check(); 
 $session_username = $_SESSION['username'];
 $msg = $_GET['msg'];
 
 $contact_id = $_GET['contact_id'];
+$company_id = $_GET['company_id'];
 $phone = $_GET['phone'];
+$phone_dial_prefix = "1";
 
 $msg = urlencode(_("Dialing Phone Number: ") . $phone);
 
-$sipCID = lookupCID($session_username);
+// Get contact name 
 
-# assume all phone numbers can be dialed by adding a 91
-# calls will be placed in the sippstn asterisk context
-$dial_file_contents = "Channel:SIP/$sipCID
+$sql = "SELECT first_names,last_name from contacts
+        WHERE contact_id = " . $contact_id . " LIMIT 1";
+$rst = $con->execute($sql);
+
+if ($rst) {
+    if (!$rst->EOF) {
+        $contact_name = urlencode($rst->fields['first_names'] . " "
+              . $rst->fields['last_name']);
+    }
+}
+
+// Get variables from the custom fields of the user's contact id.
+
+$sql = "SELECT custom1, custom2, custom3 from contacts, users
+        WHERE  users.user_id = " . $session_user_id . "
+        AND contacts.contact_id = users.user_contact_id
+        LIMIT 1";
+$rst = $con->execute($sql);
+
+if ($rst) {
+    if (!$rst->EOF) {
+        $channel = $rst->fields['custom1'];
+        $extension_to_dial = $rst->fields['custom2'];
+        $CID = $rst->fields['custom3'];
+    }
+}
+
+// $sipCID = lookupCID($session_username);
+
+// This is the file that will be passed to Asterisk
+
+$dial_file_contents = "Channel:$channel$extension_to_dial
 MaxRetries: 1
 RetryTime: 60
 WaitTime: 30
-Callerid: $sipCID
-Context: sippstn
-Extension: 91$phone
+Callerid: $CID
+Context: xrms
+Extension: $phone_dial_prefix$phone
 Priority: 1
 ";
+
 $filename = $xrms_file_root . "/tmp/outdial-$phone";
 
    if (!$handle = fopen($filename, 'w')) {
@@ -98,17 +135,21 @@ $filename = $xrms_file_root . "/tmp/outdial-$phone";
    fclose($handle);
 
 // Create an Activity on Dial
-// This doesn't work right now:
-// header("Location: ../../activities/new-2.php?user_id=" . $user_id
-//    . "&activity_status=o&activity_type_id=1&on_what_id=1&contact_id=" . $contact_id
-//	. "&company_id=" . $company_id . "&activity_title=" . _("Call%20To%20") . $name
-//	. "&return_url=/contacts/one.php?contact_id=" . $contact_id);
+header("Location: ../../activities/new-2.php?user_id=" . $session_user_id
+    . "&activity_status=o&activity_type_id=1&contact_id="
+    . $contact_id . "&company_id=" . $company_id . "&activity_title="
+    . _("Call%20To%20") . $contact_name
+    . "&return_url=/contacts/one.php?contact_id=" . $contact_id);
+
 
 // if you don't want to create an activity on dial, use this instead:
-header("Location: $http_site_root/contacts/one.php?contact_id=$contact_id&msg=$msg");
+// header("Location: $http_site_root/contacts/one.php?contact_id=$contact_id&msg=$msg");
 
 /**
  * $Log: asteriskdial.php,v $
+ * Revision 1.3  2004/11/16 01:38:58  gpowers
+ * - added code to use contact.custom1..3 fields for Channel, Extension and CalledID for each XRMS user.
+ *
  * Revision 1.2  2004/10/22 08:18:44  gpowers
  * - added patch by ismaeljcarlo @ SF
  *   - lookup crm_username from /etc/asterisk/sip.conf
