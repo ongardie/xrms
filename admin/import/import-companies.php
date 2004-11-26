21d4d237787860d44f1de08df41e770b11a2a124
braverock <braverock>
2004-11-26 15:55:45 +0000

    - add translate_menu call to all drop-down menus for i18n - fix unitialized variable Notices

--- admin/import/import-companies.php
+++ admin/import/import-companies.php
@@ -13,7 +13,7 @@
  * @author Chris Woofter
  * @author Brian Peterson
  *
- * $Id: import-companies.php,v 1.12 2004/10/01 14:11:37 introspectshun Exp $
+ * $Id: import-companies.php,v 1.13 2004/11/26 15:55:45 braverock Exp $
  */
 require_once('../../include-locations.inc');
 
@@ -26,6 +26,7 @@ require_once($include_directory . 'adodb-params.php');
 $session_user_id = session_check( 'Admin' );
 
 $page_title = _("Import");
+if (!isset($msg)) { $msg=''; };
 start_page($page_title, true, $msg);
 
 $con = &adonewconnection($xrms_db_dbtype);
@@ -33,42 +34,42 @@ $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_db
 
 $sql2 = "select username, user_id from users where user_record_status = 'a' order by username";
 $rst = $con->execute($sql2);
-$user_menu = $rst->getmenu2('user_id', $user_id, false);
+$user_menu = $rst->getmenu2('user_id', $session_user_id, false);
 $rst->close();
 
 $sql2 = "select crm_status_pretty_name, crm_status_id from crm_statuses where
          crm_status_record_status = 'a' order by crm_status_id";
 $rst = $con->execute($sql2);
-$crm_status_menu = $rst->getmenu2('crm_status_id', $crm_status_id, false);
+$crm_status_menu = translate_menu($rst->getmenu2('crm_status_id', '', false));
 $rst->close();
 
 $sql2 = "select company_source_pretty_name, company_source_id from company_sources where
          company_source_record_status = 'a' order by company_source_pretty_name";
 $rst = $con->execute($sql2);
-$company_source_menu = $rst->getmenu2('company_source_id', $company_source_id, false);
+$company_source_menu = translate_menu($rst->getmenu2('company_source_id', '', false));
 $rst->close();
 
 $sql2 = "select category_pretty_name, category_id from categories where
          category_record_status = 'a' order by category_pretty_name";
 $rst = $con->execute($sql2);
-$category_menu = $rst->getmenu2('category_id', $category_id, true);
+$category_menu = $rst->getmenu2('category_id', '', true);
 $rst->close();
 
 $sql2 = "select industry_pretty_name, industry_id from industries where
          industry_record_status = 'a' order by industry_pretty_name";
 $rst = $con->execute($sql2);
-$industry_menu = $rst->getmenu2('industry_id', $industry_id, false);
+$industry_menu = translate_menu($rst->getmenu2('industry_id', '', false));
 $rst->close();
 
 $sql = "select account_status_pretty_name, account_status_id from account_statuses where
         account_status_record_status = 'a'";
 $rst = $con->execute($sql);
-$account_status_menu = $rst->getmenu2('account_status_id', $account_status_id, false);
+$account_status_menu = translate_menu($rst->getmenu2('account_status_id', '', false));
 $rst->close();
 
 $sql = "select rating_pretty_name, rating_id from ratings where rating_record_status = 'a'";
 $rst = $con->execute($sql);
-$rating_menu = $rst->getmenu2('rating_id', $rating_id, false);
+$rating_menu = translate_menu($rst->getmenu2('rating_id', '', false));
 $rst->close();
 
 $con->close();
@@ -162,6 +163,10 @@ if ($handle = opendir('.')) {
 <?php end_page();
 /**
  * $Log: import-companies.php,v $
+ * Revision 1.13  2004/11/26 15:55:45  braverock
+ * - add translate_menu call to all drop-down menus for i18n
+ * - fix unitialized variable Notices
+ *
  * Revision 1.12  2004/10/01 14:11:37  introspectshun
  * - Fine-tuned regex to limit list of import templates to
  *   just those that start with 'import-template' and end with 'php'
