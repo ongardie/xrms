2f441622eea72d8418f5d09408799d68840f2154
braverock <braverock>
2004-12-31 17:23:04 +0000

    - cleaned up code formatting - add db_error_handler - prep for workflow extensions

--- admin/opportunity-statuses/some.php
+++ admin/opportunity-statuses/some.php
@@ -5,7 +5,7 @@
  *
  * @todo modify all opportunity status uses to use a sort order
  *
- * $Id: some.php,v 1.9 2004/07/16 23:51:37 cpsource Exp $
+ * $Id: some.php,v 1.10 2004/12/31 17:23:04 braverock Exp $
  */
 
 //include required XRMS common files
@@ -32,44 +32,47 @@ $maxcnt = $rst->rowcount();
 
 //get rows, place them in table form 
 if ($rst) {
-    while (!$rst->EOF) {
-	$sort_order = $rst->fields['sort_order'];
-        $table_rows .= "\n<tr cellspacing=0>";
-	    $table_rows .= '<td class=widget_content>'
-                         . '<a href=one.php?opportunity_status_id='
-                         . $rst->fields['opportunity_status_id']
-                         . '>';
+   while (!$rst->EOF) {
+      $sort_order = $rst->fields['sort_order'];
+      $table_rows .= "\n<tr cellspacing=0>";
+      $table_rows .= '<td class=widget_content>'
+                     . '<a href=one.php?opportunity_status_id='
+                     . $rst->fields['opportunity_status_id']
+                     . '>';
+      
+      if (strlen ($opportunity_status_display_html) > 0) {
+         $table_rows .= $rst->fields['opportunity_status_pretty_html'];
+      } else {
+         $table_rows .= $rst->fields['opportunity_status_pretty_name'];
+      }
 
-            if (strlen ($opportunity_status_display_html) > 0) {
-                $table_rows .= $rst->fields['opportunity_status_pretty_html'];
-            } else {
-                $table_rows .= $rst->fields['opportunity_status_pretty_name'];
-            }
+      $table_rows .= '</a></td>'
+                  . '<td class=widget_content>'
+                  . htmlspecialchars($rst->fields['opportunity_status_long_desc'])
+                  . '</td>'
+                  . '<td class=widget_content>';
 
-            $table_rows .= '</a></td>'
-                         . '<td class=widget_content>'
-                         . htmlspecialchars($rst->fields['opportunity_status_long_desc'])
-                         . '</td>'
-                         . '<td class=widget_content>';
-	    //sets up ordering links in the table
-            if ($sort_order != $cnt) {
-                $table_rows .= '<a href="' . $http_site_root
-                    . '/admin/sort.php?direction=up&sort_order='
-                    . $sort_order . '&table_name=opportunity_status'
-                    . '&return_url=/admin/opportunity-statuses/some.php">up</a> &nbsp; ';
-            }
-	    if ($sort_order != $maxcnt) {
-		$table_rows .= '<a href="' . $http_site_root 
-		    . '/admin/sort.php?direction=down&sort_order=' 
-		    . $sort_order . '&table_name=opportunity_status' 
-		    . '&return_url=/admin/opportunity-statuses/some.php">down</a>';
-	    } 
-	    $table_rows .= '</td>';
+       //sets up ordering links in the table
+       if ($sort_order != $cnt) {
+           $table_rows .= '<a href="' . $http_site_root
+               . '/admin/sort.php?direction=up&sort_order='
+               . $sort_order . '&table_name=opportunity_status'
+               . '&return_url=/admin/opportunity-statuses/some.php">up</a> &nbsp; ';
+       }
+       if ($sort_order != $maxcnt) {
+           $table_rows .= '<a href="' . $http_site_root
+                        . '/admin/sort.php?direction=down&sort_order='
+                        . $sort_order . '&table_name=opportunity_status'
+                        . '&return_url=/admin/opportunity-statuses/some.php">down</a>';
+       }
+       $table_rows .= '</td>';
 
-        $table_rows .= '</tr>';
-        $rst->movenext();
-    }
+       $table_rows .= '</tr>';
+       $rst->movenext();
+    } //end while
     $rst->close();
+} else {
+   db_error_handler($con,$sql);
 }
 
 $con->close();
@@ -81,51 +84,51 @@ start_page($page_title);
 ?>
 
 <div id="Main">
-    <div id="Content">
+   <div id="Content">
 
-	<form action=../sort.php method=post>
+   <form action=../sort.php method=post>
         <table class=widget cellspacing=1>
             <tr>
                 <td class=widget_header colspan=4><?php echo _("Opportunity Statuses"); ?></td>
             </tr>
             <tr>
-		<td class=widget_label><?php echo _("Name"); ?></td>
-		<td class=widget_label width=50%><?php echo _("Description"); ?></td>
+                <td class=widget_label><?php echo _("Name"); ?></td>
+                <td class=widget_label width=50%><?php echo _("Description"); ?></td>
                 <td class=widget_label width=15%><?php echo _("Move"); ?></td>
             </tr>
             <?php  echo $table_rows; ?>
         </table>
-	</form>
+   </form>
 
-    </div>
+   </div>
 
-        <!-- right column //-->
-    <div id="Sidebar">
+   <!-- right column //-->
+   <div id="Sidebar">
 
-        <form action=new-2.php method=post>
-        <table class=widget cellspacing=1>
-	    <tr>
-                <td class=widget_header colspan=2><?php echo _("Add New Opportunity Status"); ?></td>
-            </tr>
-            <tr>
-                <td class=widget_label_right><?php echo _("Short Name"); ?></td>
-                <td class=widget_content_form_element><input type=text name=opportunity_status_short_name size=10></td>
-            </tr>
-            <tr>
-                <td class=widget_label_right><?php echo _("Full Name"); ?></td>
-                <td class=widget_content_form_element><input type=text name=opportunity_status_pretty_name size=20></td>
-            </tr>
-            <tr>
-                <td class=widget_label_right><?php echo _("Full Plural Name"); ?></td>
-                <td class=widget_content_form_element><input type=text name=opportunity_status_pretty_plural size=20></td>
-            </tr>
-            <tr>
-                <td class=widget_label_right><?php echo _("Display HTML"); ?></td>
-                <td class=widget_content_form_element><input type=text name=opportunity_status_display_html size=30></td>
-            </tr>            
-            <tr>
-                <td class=widget_label_right><?php echo _("Description"); ?></td>
-                <td class=widget_content_form_element><input type=text size=30 name=opportunity_status_long_desc value="<?php  echo $opportunity_status_long_desc; ?>"></td>
+       <form action=new-2.php method=post>
+       <table class=widget cellspacing=1>
+           <tr>
+               <td class=widget_header colspan=2><?php echo _("Add New Opportunity Status"); ?></td>
+           </tr>
+           <tr>
+               <td class=widget_label_right><?php echo _("Short Name"); ?></td>
+               <td class=widget_content_form_element><input type=text name=opportunity_status_short_name size=10></td>
+           </tr>
+           <tr>
+               <td class=widget_label_right><?php echo _("Full Name"); ?></td>
+               <td class=widget_content_form_element><input type=text name=opportunity_status_pretty_name size=20></td>
+           </tr>
+           <tr>
+               <td class=widget_label_right><?php echo _("Full Plural Name"); ?></td>
+               <td class=widget_content_form_element><input type=text name=opportunity_status_pretty_plural size=20></td>
+           </tr>
+           <tr>
+               <td class=widget_label_right><?php echo _("Display HTML"); ?></td>
+               <td class=widget_content_form_element><input type=text name=opportunity_status_display_html size=30></td>
+           </tr>
+           <tr>
+               <td class=widget_label_right><?php echo _("Description"); ?></td>
+               <td class=widget_content_form_element><input type=text size=30 name=opportunity_status_long_desc value="<?php  echo $opportunity_status_long_desc; ?>"></td>
             </tr>
             <tr>
                 <td class=widget_label_right><?php echo _("Open Status"); ?></td>
@@ -140,10 +143,10 @@ start_page($page_title);
             <tr>
                 <td class=widget_content_form_element colspan=2><input class=button type=submit value="<?php echo _("Add"); ?>"></td>
             </tr>
-        </table>
-        </form>
+      </table>
+      </form>
 
-    </div>
+   </div>
 </div>
 
 <?php
@@ -152,6 +155,11 @@ end_page();
 
 /**
  * $Log: some.php,v $
+ * Revision 1.10  2004/12/31 17:23:04  braverock
+ * - cleaned up code formatting
+ * - add db_error_handler
+ * - prep for workflow extensions
+ *
  * Revision 1.9  2004/07/16 23:51:37  cpsource
  * - require session_check ( 'Admin' )
  *
@@ -177,6 +185,5 @@ end_page();
  * Revision 1.3  2004/01/25 18:39:41  braverock
  * - fixed insert bugs so long_desc will be disoplayed and inserted properly
  * - added phpdoc
- *
  */
 ?>
