e4717bead5964f6a59b97c38aa8ea0d807de5f6f
vanmer <vanmer>
2005-01-10 21:40:46 +0000

    - added case_type, needed for distinguishing between statuses - added dropdown for selecting type, for use in creating new status - only show new status box when case_type_id is set (type selected with dropdown)

--- admin/case-statuses/some.php
+++ admin/case-statuses/some.php
@@ -2,7 +2,7 @@
 /**
 * Manage Case Statuses
 *
-* $Id: some.php,v 1.9 2004/12/31 17:52:56 braverock Exp $
+* $Id: some.php,v 1.10 2005/01/10 21:40:46 vanmer Exp $
 *
 * @todo manage case statuses by type
 *       will require working on creating the rows by type,
@@ -21,58 +21,88 @@ $session_user_id = session_check( 'Admin' );
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 
-$sql = "select * from case_statuses where case_status_record_status = 'a' order by case_type_id, sort_order";
+//print_r($_SESSION);
+getGlobalVar($acase_type_id,'acase_type_id');
+
+$sql = "select case_type_pretty_name,case_type_id FROM case_types";
 $rst = $con->execute($sql);
+if (!$rst) { db_error_handler($con, $sql); }
+else { $type_menu= $rst->getmenu2('acase_type_id',$acase_type_id, true, false, 1, "id=acase_type_id onchange=javascript:restrictByCaseType();"); }
 
 
-//get first row count and last row count
-$cnt = 1;
-$maxcnt = $rst->rowcount();
-                                                                                                                           
-//get rows, place them in table form
-if ($rst) {
-   while (!$rst->EOF) {
-      
-      $sort_order = $rst->fields['sort_order'];
-      $table_rows .= '<tr>'
-                  . '<td class=widget_content><a href=one.php?case_status_id=' . $rst->fields['case_status_id'] . '>'
-                  . $rst->fields['case_status_pretty_name'] . '</a></td>';
+if ($acase_type_id) {
+    $sql = "select * from case_statuses where case_status_record_status = 'a' AND case_type_id=$acase_type_id order by case_type_id, sort_order";
+    $rst = $con->execute($sql);
+    if (!$rst) { db_error_handler($con, $sql); }
 
-      //add descriptions
-      $table_rows .= '<td class=widget_content>'
-                  . htmlspecialchars($rst->fields['case_status_long_desc'])
-                  . '</td>';
-                  
-      //sets up ordering links in the table
-      
-      $table_rows .= '<td class=widget_content>';
-      if ($sort_order != $cnt) {
-          $table_rows .= '<a href="' . $http_site_root
-                      . '/admin/sort.php?direction=up&sort_order='
-                      . $sort_order . '&table_name=case_status&return_url=/admin/case-statuses/some.php">'._("up").'</a> &nbsp; ';
-      }
-      if ($sort_order != $maxcnt) {
-          $table_rows .= '<a href="' . $http_site_root
-                      . '/admin/sort.php?direction=down&sort_order='
-                      . $sort_order . '&table_name=case_status&return_url=/admin/case-statuses/some.php">'._("down").'</a>';
-      }
-      $table_rows .= '</td></tr>';
 
-      $rst->movenext();
-	}
-	$rst->close();
-}
-
+    //get first row count and last row count
+    $cnt = 1;
+    $maxcnt = $rst->rowcount();
+                                                                                                                            
+    //get rows, place them in table form
+    $table_rows='';
+    if ($rst) {
+        while (!$rst->EOF) {
+            
+            $sort_order = $rst->fields['sort_order'];
+            $table_rows .= '<tr>'
+                        . '<td class=widget_content><a href=one.php?case_status_id=' . $rst->fields['case_status_id'] . '>'
+                        . $rst->fields['case_status_pretty_name'] . '</a></td>';
+        
+            //add descriptions
+            $table_rows .= '<td class=widget_content>'
+                        . htmlspecialchars($rst->fields['case_status_long_desc'])
+                        . '</td>';
+                        
+            //sets up ordering links in the table
+            
+            $table_rows .= '<td class=widget_content>';
+            if ($sort_order != $cnt) {
+                $table_rows .= '<a href="' . $http_site_root
+                            . '/admin/sort.php?direction=up&sort_order='
+                            . $sort_order . "&table_name=case_status&case_type_id=$acase_type_id&return_url=/admin/case-statuses/some.php?acase_type_id=$acase_type_id\">"._("up").'</a> &nbsp; ';
+            }
+            if ($sort_order != $maxcnt) {
+                $table_rows .= '<a href="' . $http_site_root
+                            . '/admin/sort.php?direction=down&sort_order='
+                            . $sort_order . "&table_name=case_status&case_type_id=$acase_type_id&return_url=/admin/case-statuses/some.php?acase_type_id=$acase_type_id\">"._("down").'</a>';
+            }
+            $table_rows .= '</td></tr>';
+        
+            $rst->movenext();
+        }
+        $rst->close();
+        if (!$table_rows) { 
+            $table_rows='<tr><td colspan=3 class=widget_content>'._("No statuses defined for specified status type") . '</td></tr>';
+        }
+    }
+} else { $table_rows='<tr><td colspan=3 class=widget_content>'._("Select a status type") . '</td></tr>'; }
 $con->close();
 
 $page_title = _("Manage Case Statuses");
 start_page($page_title);
 
 ?>
+    <script language=JavaScript>
+    <!--
+        function restrictByCaseType() {
+            select=document.getElementById('acase_type_id');
+            location.href = 'some.php?acase_type_id=' + select.value;
+        }
+     //-->
+    </script>
 
 <div id="Main">
    <div id="Content">
-
+                <table class=widget classpacing=1>
+                    <tr>
+                        <td class=widget_header><?php echo _("Status Type"); ?></td>
+                    </tr>
+                    <tr>
+                        <td class=widget_content><?php echo $type_menu; ?></td>
+                    </tr>
+                </table>
 		<table class=widget cellspacing=1>
 			<tr>
 				<td class=widget_header colspan=3><?php echo _("Case Statuses"); ?></td>
@@ -91,7 +121,9 @@ start_page($page_title);
       <!-- right column //-->
    <div id="Sidebar">
 
+<?php if ($acase_type_id) { ?>    
 	<form action=new-2.php method=post>
+        <input type=hidden name=case_type_id value="<?php echo $acase_type_id; ?>">
 	<table class=widget cellspacing=1>
 		<tr>
 			<td class=widget_header colspan=2>
@@ -146,7 +178,7 @@ start_page($page_title);
 		</tr>
 	</table>
 	</form>
-
+<?php } ?>
    </div>
 </div>
 
@@ -156,6 +188,11 @@ end_page();
 
 /**
 * $Log: some.php,v $
+* Revision 1.10  2005/01/10 21:40:46  vanmer
+* - added case_type, needed for distinguishing between statuses
+* - added dropdown for selecting type, for use in creating new status
+* - only show new status box when case_type_id is set (type selected with dropdown)
+*
 * Revision 1.9  2004/12/31 17:52:56  braverock
 * - add description for consistency
 *
