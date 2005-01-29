43ac3975287f14c42561ec114b142deaeee9ec8a
braverock <braverock>
2005-01-29 13:29:37 +0000

    - updated to reflect new strings in current code

--- locale/datastrings.php
+++ locale/datastrings.php
@@ -7,7 +7,7 @@
  * php ./getdatastrings.php
  * from the locale directory
  *
- * $Id: datastrings.php,v 1.3 2004/08/19 19:56:29 johnfawcett Exp $
+ * $Id: datastrings.php,v 1.4 2005/01/29 13:29:37 braverock Exp $
  */
 $s=_('Activities Default Behavior');
 $s=_('Advertisement');
@@ -32,6 +32,7 @@ $s=_('Critical');
 $s=_('Customer');
 $s=_('Customers');
 $s=_('Default GST Offset');
+$s=_('Default timezone offset');
 $s=_('Developed');
 $s=_('Direct Mail');
 $s=_('Discussion');
@@ -52,15 +53,20 @@ $s=_('Help Item');
 $s=_('Help Items');
 $s=_('High');
 $s=_('Hold');
+$s=_('How will activities behave.  Options are Fast or Long.  Fast will keep user within current screen while Long will move to the activites/one screen for detailed description entry.');
 $s=_('In Progress');
 $s=_('Insurance');
+$s=_('LDAP Version');
 $s=_('Lead');
 $s=_('Leads');
 $s=_('Legal');
+$s=_('Long');
 $s=_('Low');
 $s=_('Magazine');
 $s=_('Mail');
 $s=_('Manufacturing');
+$s=_('Maximum RSS Feed Entries');
+$s=_('Maximum number of entries to include in any RSS feed.');
 $s=_('Medium');
 $s=_('N/A');
 $s=_('Negotiation');
@@ -75,7 +81,9 @@ $s=_('Poor');
 $s=_('Preliminaries');
 $s=_('Prospect');
 $s=_('Prospects');
+$s=_('Provide RSS Feeds.  y or n.');
 $s=_('Qualified');
+$s=_('RSS Feeds Enabled');
 $s=_('Radio');
 $s=_('Real Estate');
 $s=_('Restaurant');
@@ -92,8 +100,10 @@ $s=_('Trade Show');
 $s=_('Trade Shows');
 $s=_('Vendor');
 $s=_('Vendors');
+$s=_('Version of the LDAP server.  Will usually be 2 or 3.');
 $s=_('Web Site');
 $s=_('Web Sites');
+$s=_('What level of audit logging should be performed.  0 - no logging);
 $s=_('Word of Mouth');
 $s=_('call from');
 $s=_('call to');
@@ -111,4 +121,8 @@ $s=_('internal');
 $s=_('letter from');
 $s=_('letter to');
 $s=_('process');
+$s=_(1 - inserts & updates);
+$s=_(2 - and login/logout);
+$s=_(3 - and views);
+$s=_(4 - and searches');
 ?>
