f7d02c07cf79bb391148d513340095766c623113
daturaarutad <daturaarutad>
2005-03-01 15:48:09 +0000

    changed name from GUP_Pager_TD_Classname to Pager_TD_CSS_All_Rows

--- activities/activities-pager-functions.php
+++ activities/activities-pager-functions.php
@@ -2,26 +2,32 @@
 /**
  * Shared activity pager functions
  *
- * $Id: activities-pager-functions.php,v 1.2 2005/02/24 00:17:39 braverock Exp $
+ * $Id: activities-pager-functions.php,v 1.3 2005/03/01 15:48:09 daturaarutad Exp $
  */
 
+/**
+* This is the callback function to be used with GUP_Pager
+
+*/
 function GetActivitiesPagerData($row) {
     global $http_site_root;
     global $con;
 
+	// Set the CSS classes for the rows
     if ($row['activity_status'] == 'o') {
         if ($row['is_overdue']) {
             $row['activity_status'] = _('Overdue');
-            $row['GUP_Pager_TD_Classname'] = 'overdue_activity';
+            $row['Pager_TD_CSS_All_Rows'] = 'overdue_activity';
         } else {
             $row['activity_status'] = _('Open');
-            $row['GUP_Pager_TD_Classname'] = 'open_activity';
+            $row['Pager_TD_CSS_All_Rows'] = 'open_activity';
         }
     } else {
         $row['activity_status'] = _('Closed');
-        $row['GUP_Pager_TD_Classname'] = 'closed_activity';
+        $row['Pager_TD_CSS_All_Rows'] = 'closed_activity';
     }
 
+	// Query for the About field
     if ($row['on_what_table'] == 'opportunities') {
         $row['activity_about'] = "<a href='$http_site_root/opportunities/one.php?opportunity_id={$row['on_what_id']}'>";
         $sql2 = "select opportunity_title as attached_to_name
@@ -48,6 +54,9 @@ function GetActivitiesPagerData($row) {
 
 /**
  * $Log: activities-pager-functions.php,v $
+ * Revision 1.3  2005/03/01 15:48:09  daturaarutad
+ * changed name from GUP_Pager_TD_Classname to Pager_TD_CSS_All_Rows
+ *
  * Revision 1.2  2005/02/24 00:17:39  braverock
  * - add phpdoc
  *
