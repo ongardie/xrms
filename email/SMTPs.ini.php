be0f850aeb63f71998914f6c27a310d9ce92f578
jswalter <jswalter>
2005-03-17 19:59:11 +0000

    - initial commit  - INI file for SMTPs.php Class Object

---
+++ email/SMTPs.ini.php
@@ -0,0 +1,28 @@
+<?php
+
+// =============================================================
+// CVS Id Info
+// $Id: SMTPs.ini.php,v 1.1 2005/03/17 19:59:11 jswalter Exp $
+
+
+    $this->_smtpsHost   = 'localhost';
+    $this->_smtpsPort   = 25; //587;
+    $this->_smtpsID     = 'userID';
+    $this->_smtpsPW     = 'iserPW!';
+    $this->_msgReplyTo  = '';
+
+
+// =============================================================
+// =============================================================
+// ** CSV Version Control Info
+
+ /**
+  * $Log: SMTPs.ini.php,v $
+  * Revision 1.1  2005/03/17 19:59:11  jswalter
+  *  - initial commit
+  *  - INI file for SMTPs.php Class Object
+  *
+  *
+  */
+
+?>
