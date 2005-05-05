262eff9df492b47520a620806af91b18713c14fd
daturaarutad <daturaarutad>
2005-05-05 21:38:31 +0000

    added Related Activities Pager and changed $_GET usage to getGlobalVar

--- activities/one.php
+++ activities/one.php
@@ -2,7 +2,7 @@
 /**
  * Edit the details for a single Activity
  *
- * $Id: one.php,v 1.91 2005/05/04 14:33:37 braverock Exp $
+ * $Id: one.php,v 1.92 2005/05/05 21:38:31 daturaarutad Exp $
  *
  * @todo Fix fields to use CSS instead of absolute positioning
  */
@@ -16,14 +16,15 @@ require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
 require_once($include_directory . 'adodb-params.php');
 require_once($include_directory . 'confgoto.php');
-$activity_id = isset($_GET['activity_id']) ? $_GET['activity_id'] : '';
+
+getGlobalVar($activity_id, 'activity_id');
+getGlobalVar($msg, 'msg');
+getGlobalVar($return_url, 'return_url');
+if (!$return_url) $return_url='/activities/some.php';
+
 $on_what_id=$activity_id;
 $session_user_id = session_check();
 
-$msg         = isset($_GET['msg']) ? $_GET['msg'] : '';
-$activity_id = isset($_GET['activity_id']) ? $_GET['activity_id'] : '';
-$return_url  = isset($_GET['return_url']) ? $_GET['return_url'] : '';
-if (!$return_url) $return_url='/activities/some.php';
 
 $save_and_next = isset($_GET['save_and_next']) ? true : false;
 
@@ -264,6 +265,85 @@ if ( !isset($activity_content_bottom) ) {
 $param=NULL;
 $activity_content_bottom = do_hook_function('activity_content_bottom',$param);
 
+
+// related activities
+
+require_once($include_directory . 'classes/Pager/GUP_Pager.php');
+require_once($include_directory . 'classes/Pager/Pager_Columns.php');
+require_once('activities-pager-functions.php');
+
+if($on_what_table && $on_what_id) {
+	$on_what_where = "a.on_what_table = '$on_what_table' AND a.on_what_id = $on_what_id";
+} else {
+	$on_what_where = "1 = 2";
+}
+
+$sql = "SELECT
+  	(CASE WHEN (activity_status = 'o') AND (ends_at < " . $con->DBTimeStamp(time()) . ") THEN ". $con->qstr(_("Yes"),get_magic_quotes_gpc()) ." ELSE '-' END) AS overdue, "
+  	." at.activity_type_pretty_name AS type, "
+  	. $con->Concat("'<a id=\"'", "cont.last_name", "'_'" ,"cont.first_names","'\" href=\"../contacts/one.php?contact_id='", "cont.contact_id", "'\">'", "cont.first_names", "' '", "cont.last_name", "'</a>'") . " AS contact, "
+  	. $con->Concat("'<a id=\"'", "activity_title", "'\" href=\"one.php?activity_id='", "a.activity_id", "'&amp;return_url=/activities/some.php\">'", "activity_title", "'</a>'") . " AS title, "
+  	. $con->SQLDate('Y-m-d','a.scheduled_at') . " AS scheduled, "
+  	. $con->SQLDate('Y-m-d','a.ends_at') . " AS due, "
+  	. $con->Concat("'<a id=\"'", "c.company_name", "'\" href=\"../companies/one.php?company_id='", "c.company_id", "'\">'", "c.company_name", "'</a>'") . " AS company, "
+  	. "u.username AS owner, u.user_id, activity_id,"
+  	// these are to speed up the pager sorting
+  	. "cont.last_name, cont.first_names, activity_title, a.scheduled_at, a.ends_at, c.company_name "
+	. "FROM companies c, activity_types at, addresses addr, activities a 
+		LEFT OUTER JOIN contacts cont ON cont.contact_id = a.contact_id
+		LEFT OUTER JOIN users u ON a.user_id = u.user_id
+		WHERE $on_what_where
+		AND a.company_id = c.company_id
+		AND a.activity_record_status = 'a'
+  		AND at.activity_type_id = a.activity_type_id
+  		AND c.default_primary_address=addr.address_id";
+
+
+$columns = array();
+$columns[] = array('name' => _('Overdue'), 'index_sql' => 'overdue');
+$columns[] = array('name' => _('Type'), 'index_sql' => 'type');
+$columns[] = array('name' => _('Contact'), 'index_sql' => 'contact', 'sql_sort_column' => 'cont.last_name,cont.first_names', 'type' => 'url');
+$columns[] = array('name' => _('Title'), 'index_sql' => 'title', 'sql_sort_column' => 'activity_title', 'type' => 'url');
+$columns[] = array('name' => _('Scheduled'), 'index_sql' => 'scheduled', 'sql_sort_column' => 'a.scheduled_at');
+$columns[] = array('name' => _('Due'), 'index_sql' => 'due', 'default_sort' => 'desc', 'sql_sort_column' => 'a.ends_at');
+$columns[] = array('name' => _('Company'), 'index_sql' => 'company', 'sql_sort_column' => 'c.company_name', 'type' => 'url');
+$columns[] = array('name' => _('Owner'), 'index_sql' => 'owner');
+    
+
+// selects the columns this user is interested in
+// no reason to set this if you don't want all by default
+$default_columns = null;
+//$default_columns =  array('overdue','type','contact','title','scheduled','due','company','owner');
+
+$pager_columns = new Pager_Columns('SomeRelatedActivitiesPager', $columns, $default_columns, 'OneActivityForm');
+$pager_columns_button = $pager_columns->GetSelectableColumnsButton();
+$pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
+ 
+$columns = $pager_columns->GetUserColumns('default');
+
+
+// output the selectable columns widget
+echo $pager_columns_selects;
+
+// caching is disabled for this pager (since it's all sql)
+//$pager = new GUP_Pager($con, $sql, 'GetActivitiesPagerData', _('Search Results'), 'ActivitiesData', 'SomeActivitiesPager', $columns, false, true);
+$pager = new GUP_Pager($con, $sql, 'GetActivitiesPagerData', _('Related Activities'), 'OneActivityForm', 'SomeActivitiesPager', $columns, false, true);
+
+// set up the bottom row of buttons
+$endrows = "<tr><td class=widget_content_form_element colspan=10>
+            $pager_columns_button
+            " . $pager->GetAndUseExportButton() .  "
+            <input type=button class=button onclick=\"javascript: bulkEmail();\" value=\"" . _('Mail Merge') . "\"></td></tr>";
+
+$pager->AddEndRows($endrows);
+
+$related_activites_widget = $pager->Render($system_rows_per_page);
+
+// end related activities
+
+
+
+
 /*********************************/
 /*** Include the sidebar boxes ***/
 
@@ -488,7 +568,16 @@ function logTime() {
             </tr>
            <?php  echo $history_text; ?>
         </table>
+
+
         </form>
+
+        <form action=one.php name="OneActivityForm" method=post>
+			<input type=hidden name="activity_id" value="<?php echo $activity_id; ?>">
+			<input type=hidden name="return_url" value="<?php echo $return_url; ?>">
+			<?php echo $related_activites_widget; ?>
+		</form>
+
         <?php echo $activity_content_bottom; ?>
     </div>
 
@@ -539,6 +628,9 @@ function logTime() {
 
 /**
  * $Log: one.php,v $
+ * Revision 1.92  2005/05/05 21:38:31  daturaarutad
+ * added Related Activities Pager and changed $_GET usage to getGlobalVar
+ *
  * Revision 1.91  2005/05/04 14:33:37  braverock
  * - removed obsolete widget_label_right_166px CSS style, replaces w/ widget_label_right
  *
