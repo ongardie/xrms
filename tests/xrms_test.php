fd5a3190715b3511428fb1bfe9532a0093e62266
vanmer <vanmer>
2005-05-06 00:51:18 +0000

    - added tests for preferences system options

--- tests/xrms_test.php
+++ tests/xrms_test.php
@@ -6,10 +6,10 @@
  * All Rights Reserved.
  *
  * @todo
- * $Id: xrms_test.php,v 1.1 2005/03/09 16:16:52 vanmer Exp $
+ * $Id: xrms_test.php,v 1.2 2005/05/06 00:51:18 vanmer Exp $
  */
 
-require_once('../../include-locations.inc');
+require_once('../include-locations.inc');
 require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
@@ -47,6 +47,19 @@ Class XRMSTest extends PHPUnit_TestCase {
 	$this->assertTrue(true, "This should never fail.");
     }
     
+    function test_add_user_preference_type($preference_type_name='user_language',
+                                                                         $user_preference_pretty_name=false,
+                                                                    $user_preference_description=false, 
+                                                                    $allow_multiple=false,
+                                                                    $allow_user_edit=false) {
+        
+        $ret=add_user_preference_type($this->con, $preference_type_name, $user_preference_pretty_name, $user_preference_description, $allow_multiple, $allow_user_edit);
+        $this->assertTrue($ret, "Failed to add or re-add user preference for language");
+        return $ret;        
+                                                                    
+                                                                                                                                        
+    }
+    
     function test_set_user_preference($user_id=1, $preference_type='user_language', $preference_value='en_US', $preference_name=false, $set_default=false, $delete=true) {
         $con = $this->con;
         $ret=set_user_preference($con, $user_id, $preference_type, $preference_value, $preference_name, $set_default);
@@ -61,8 +74,38 @@ Class XRMSTest extends PHPUnit_TestCase {
         return $success;
     }
     
-    function test_set_multi_user_preference($user_id=1, $preference_type='random_multi_option', $preference_values=false) {
+    function test_get_user_preference_type($user_preference_type=false, $user_preference_type_id=false, $return_all=true) {
+        $ret=get_user_preference_type($this->con, $user_preference_type, $user_preference_type_id, $return_all);
+        $this->assertTrue($ret, "Failed to get user preferences for type $user_preference_type id $user_preference_type_id return all: $return_all");
+    }
+    
+    function test_user_preference_options($user_preference_type='user_language', $option_value='zz_QQ', $option_display=false, $sort_order=1) {
+        if (!is_numeric($user_preference_type)) {
+            $type_info=get_user_preference_type($this->con, $user_preference_type);
+            $user_preference_type=$type_info['user_preference_type_id'];
+        }
+        $ret=add_preference_option($this->con, $user_preference_type, $option_value,$option_display, $sort_order);
+        $this->assertTrue($ret, "Failed to add option for preference $user_preference_type value $option_value order $sort_order");
+        if ($ret) {
+            $options=get_preference_options($this->con, $user_preference_type);
+            $this->assertTrue($options, "Failed to get options to check preference option addition for type $user_preference_type");
+            if ($options) {
+                $option_display=get_preference_options($this->con,$user_preference_type, false, true);
+                $option_values=array_keys($option_display);
+                $this->assertTrue(in_array($option_value, $option_values), "Failed to find option in keys for possible value output");
+                $key=array_search($option_value, $options);
+                $this->assertTrue($key, "Failed to find value $option_value in options for type $user_preference_type");
+                $this->assertTrue($key==$ret, "Option ID is not equal to returned option ID from creation");
+                $del=delete_preference_option($this->con, $user_preference_type, $option_value, true);
+                $this->assertTrue($del, "Failed to delete option after this for type $user_preference_type with value $option_value");
+                return $key;
+            }
+        }
+    }
+    
+    function test_set_multi_user_preference($user_id=1, $preference_type=false, $preference_values=false) {
         $con = $this->con;    
+        if (!$preference_type) { $preference_type='random_multi_option'; $user_preference_type_id=$this->test_add_user_preference_type($preference_type, 'TEST MULTI OPTION', 'Test Type for Multiple Options: Ignore', true, false); $created=true; }
         if (!$preference_values) { $preference_values=array('size'=>'1','shape'=>'two and two is four','texture'=>'goddamn'); }
         foreach ($preference_values as $pkey=>$pval) {
             $ret = $this->test_set_user_preference($user_id, $preference_type, $pval, $pkey, true, false);
@@ -71,32 +114,35 @@ Class XRMSTest extends PHPUnit_TestCase {
         }
         $prefs = get_user_preference($con, $user_id, $preference_type, false, true);
 //        print_r($prefs);
-        foreach ($preference_values as $pkey=>$pval) {
-            $this->assertTrue(array_key_exists($pkey, $prefs), "Failed to find newly set preference $preference_type $pkey=$pval");
-            $this->assertTrue($pval==$prefs[$pkey], "Newly set option $preference_type $pkey={$prefs[$pkey]} not equal to stored option $pval");
+        $this->assertTrue($prefs, "Failed to get preferences for user $user_id with type $preference_type for multi preference test.");
+        if ($prefs) {
+            foreach ($preference_values as $pkey=>$pval) {
+                $this->assertTrue(array_key_exists($pkey, $prefs), "Failed to find newly set preference $preference_type $pkey=$pval");
+                $this->assertTrue($pval==$prefs[$pkey], "Newly set option $preference_type $pkey={$prefs[$pkey]} not equal to stored option $pval");
+            }
+            
+            foreach ($prefs as $pkey=>$pval) {
+                $ret = get_user_preference($con, $user_id, $preference_type, $pkey);
+                $this->assertTrue($pval==$ret, "Failed match user preference for $preference_type name $pkey: $pval!=$ret");
+            }
+            $default_pref_value=get_user_preference($con, $user_id, $preference_type);
+            $this->assertTrue($default_value==$default_pref_value, "Failed to set default value correctly, expected $default_value but returned $default_pref_value");
         }
-        foreach ($prefs as $pkey=>$pval) {
-            $ret = get_user_preference($con, $user_id, $preference_type, $pkey);
-            $this->assertTrue($pval==$ret, "Failed match user preference for $preference_type name $pkey: $pval!=$ret");
-        }
-        $default_pref_value=get_user_preference($con, $user_id, $preference_type);
-        $this->assertTrue($default_value==$default_pref_value, "Failed to set default value correctly, expected $default_value but returned $default_pref_value");
         $dret = delete_user_preference($con, $user_id, $preference_type, false, true, true);
+        if ($created) {
+            delete_user_preference_type($con, $user_preference_type_id, true);
+        }
     }
-    function test_xrms_insert() {
-	$con=$this->con;
-	$sql = "INSERT INTO Role ( ROLE_NAME ) VALUES ( 'blah' )";
-	$rs=$con->execute($sql);
-	if (!$rs) db_error_handler($con, $sql);
-	$sql = "SELECT * FROM Role";
-	$newrs=$con->execute($sql);
-	echo "<pre>";
-	while (!$newrs->EOF) {
-		print_r($newrs->fields);
-		$newrs->movenext();
-	}
-	echo "</pre>";
+    
+    function test_create_form_element($element_type='test', $element_name='testing', $element_value=3, $extra_element_attributes=" onclick=\"javascript:alert('blah');\"", $element_length=false, $element_height=false, $possible_values=false, $show_blank_first=false) {
+        if (!$possible_values){  $possible_values=array(1=>'blah',2=>'huzzah', 3=>'joe'); $show_blank_first=true; }
+        $ret=create_form_element($element_type, $element_name, $element_value, $extra_element_attributes, $element_length, $element_height, $possible_values, $show_blank_first);
+//        echo htmlspecialchars($ret).$ret;
+        $this->assertTrue($ret, "Failed to get element of type $element_type name $element_name value $element_value");
+        return $ret;
     }
+    
+    
 }
 
 $suite= new PHPUnit_TestSuite( "XRMSTest" );
@@ -120,6 +166,9 @@ $display->show();
  */
 /*
  * $Log: xrms_test.php,v $
+ * Revision 1.2  2005/05/06 00:51:18  vanmer
+ * - added tests for preferences system options
+ *
  * Revision 1.1  2005/03/09 16:16:52  vanmer
  * - moving test suite from admin/tests to basedir/tests
  *
