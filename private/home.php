f78f69b0441401d544b0a4b6a42b1c3a3a1ca0c7
daturaarutad <daturaarutad>
2005-05-20 23:13:43 +0000

    updated to use GUP_Pager for activities; small change for calendar which is not quite perfect but better at starting on a reasonable date

--- private/home.php
+++ private/home.php
@@ -6,7 +6,7 @@
  *       to create a 'personal dashboard'
  *
  *
- * $Id: home.php,v 1.48 2005/05/18 16:18:02 daturaarutad Exp $
+ * $Id: home.php,v 1.49 2005/05/20 23:13:43 daturaarutad Exp $
  */
 
 // include the common files
@@ -17,13 +17,19 @@ require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
 require_once($include_directory . 'adodb-params.php');
+require_once($include_directory . 'classes/Pager/Pager_Columns.php');
+require_once($include_directory . 'classes/Pager/GUP_Pager.php');
+require_once('../activities/activities-pager-functions.php');
+
 
 //see if we are logged in
 $session_user_id = session_check();
 
 // get call arguments
 $msg = isset($_GET['msg']) ? $_GET['msg'] : '';
-$results_view_type = isset($_GET['results_view_type']) ? $_GET['results_view_type'] : 'list';
+$msg = isset($_POST['msg']) ? $_POST['msg'] : '';
+$results_view_type = isset($_POST['results_view_type']) ? $_POST['results_view_type'] : 'list';
+getGlobalVar($calendar_start_date, 'calendar_start_date');
 
 
 //if phone browser detected redirect to phone plugin
@@ -70,126 +76,81 @@ $sidebar_rows = do_hook_function('private_sidebar_bottom', $sidebar_rows);
 //uncomment the debug line to see what's going on with the query
 //$con->debug = 1;
 
-$sql_activities = "
-SELECT
-  a.activity_id, a.activity_title, a.scheduled_at, a.ends_at, a.on_what_table, a.on_what_id,
-  a.entered_at, a.activity_status, a.activity_description, at.activity_type_pretty_name, c.company_id,
-  c.company_name, cont.contact_id, cont.first_names as contact_first_names,
-  cont.last_name as contact_last_name,
+
+// most recent activities
+$sql_activities = "SELECT " .
+$con->Concat("'<a id=\"'", "activity_title", "'\" href=\"$http_site_root/activities/one.php?activity_id='", "a.activity_id", "'&amp;return_url=/contacts/one.php%3Fcontact_id=$contact_id\">'", "activity_title", "'</a>'") .
+" AS activity_title_link, at.activity_type_pretty_name,
+a.scheduled_at, a.ends_at, a.entered_at, a.on_what_table, a.on_what_id, a.activity_status, a.activity_title,
+  cont.contact_id,
 CASE
-  WHEN ((a.activity_status = 'o') AND (a.ends_at <  ". $con->DBTimeStamp(time()) . " )) THEN 1
+  WHEN ((a.activity_status = 'o') AND (a.scheduled_at < " . $con->SQLDate('Y-m-d') . ")) THEN 1
   ELSE 0
-END AS is_overdue
-FROM activity_types at, companies c, activities a
-LEFT JOIN contacts cont on a.contact_id = cont.contact_id
-WHERE a.user_id = $session_user_id
+END AS is_overdue, " . 
+$con->Concat("'<a id=\"'", "cont.last_name", "'_'" ,"cont.first_names","'\" href=\"../contacts/one.php?contact_id='", "cont.contact_id", "'\">'", "cont.first_names", "' '", "cont.last_name", "'</a>'") . " AS contact, " . 
+$con->Concat("'<a id=\"'", "c.company_name", "'\" href=\"../companies/one.php?company_id='", "c.company_id", "'\">'", "c.company_name", "'</a>'") . " AS company
+FROM activities a, companies c
+LEFT OUTER JOIN activity_types at ON (at.activity_type_id = a.activity_type_id)
+LEFT OUTER JOIN contacts cont ON (a.contact_id = cont.contact_id)
+WHERE  a.user_id = $session_user_id
   AND a.activity_type_id = at.activity_type_id
   AND a.company_id = c.company_id
   AND a.activity_status = 'o'
-  AND a.activity_record_status = 'a'
-ORDER BY is_overdue DESC, a.scheduled_at, a.entered_at
-";
+  AND a.activity_record_status = 'a'";
+
+$list=acl_get_list($session_user_id, 'Read', false, 'activities');
+//print_r($list);
+if ($list) {
+    if ($list!==true) {
+        $list=implode(",",$list);
+        $sql_activities .= " and a.activity_id IN ($list) ";
+    }
+} else { $sql_activities .= ' AND 1 = 2 '; }
+
+//echo htmlentities($sql_activities);
 
 $activity_rows = '';
 
 if('list' == $results_view_type) {
-
-	$rst = $con->selectlimit($sql_activities, $display_how_many_activities_on_home_page);
 
 	$_SESSION['browse_start']=time();
 	$_SESSION["search_sql"]=$sql_activities;
 	$pos=1;
 	$_SESSION['pos']=$pos;
 	$next_to_check=array();
-	if ($rst) {
-	    while (!$rst->EOF) {
-	
-	        $company_id = $rst->fields['company_id'];
-	        $activity_id = $rst->fields['activity_id'];
-	        $company_name = $rst->fields['company_name'];
-	        $activity_title = $rst->fields['activity_title'];
-	        $activity_description = $rst->fields['activity_description'];
-	        $on_what_table = $rst->fields['on_what_table'];
-	        $on_what_id = $rst->fields['on_what_id'];
-	        $scheduled_at = $con->userdate($rst->fields['scheduled_at']);
-	        $ends_at = $con->userdate($rst->fields['ends_at']);
-	        $activity_status = $rst->fields['activity_status'];
-	        $next_to_check[]=$activity_id;        
-	
-	        $attached_to_link = '';
-	        $attached_to_name = '';
-	
-	        if ($on_what_table == 'opportunities') {
-	            $attached_to_link = "<a href='$http_site_root/opportunities/one.php?opportunity_id=$on_what_id'>";
-	            $sql = "select opportunity_title as attached_to_name from opportunities where opportunity_id = $on_what_id";
-	        } elseif ($on_what_table == 'cases') {
-	            $attached_to_link = "<a href='$http_site_root/cases/one.php?case_id=$on_what_id'>";
-	            $sql = "select case_title as attached_to_name from cases where case_id = $on_what_id";
-	        } else {
-	            $attached_to_link = "";
-	            $sql = "select * from users where 1 = 2";
-	        }
-	
-	        $rst2 = $con->execute($sql);
-	
-	        if ($rst2) {
-	            $attached_to_name = $rst2->fields['attached_to_name'];
-	            $rst2->close();
-	        }
-	
-	        $attached_to_link .= $attached_to_name . "</a>";
-	
-	        $open_p = $rst->fields['activity_status'];
-	        $scheduled_at = $rst->unixtimestamp($rst->fields['scheduled_at']);
-	        $is_overdue = $rst->fields['is_overdue'];
-	
-	        if ($open_p == 'o') {
-	            if ($is_overdue) {
-	                $classname = 'overdue_activity';
-	            } else if (mktime() < $scheduled_at){
-	                $classname = 'scheduled_activity';
-	            } else {
-	                $classname = 'open_activity';
-	                }
-	        } else {
-	            $classname = 'closed_activity';
-	        }
-	
-	        $activity_rows .= '<tr>';
-	        $activity_rows .= "<td class='$classname'><a href='$http_site_root/activities/one.php?save_and_next=true&activity_id=" . $rst->fields['activity_id']."&return_url=/private/home.php" . "'>" . $rst->fields['activity_title'] . '</a></td>';
-	        $activity_rows .= '<td class=' . $classname . '>' . $rst->fields['activity_type_pretty_name'] . '</td>';
-	        $activity_rows .= '<td class=' . $classname . "><a href='../companies/one.php?company_id=" . $rst->fields['company_id'] . "'>" . $rst->fields['company_name'] . '</a></td>';
-	        $activity_rows .= '<td class=' . $classname . "><a href='../contacts/one.php?contact_id=" . $rst->fields['contact_id'] . "'>" . $rst->fields['contact_first_names'] . ' ' .  $rst->fields['contact_last_name'] . '</a></td>';
-	        $activity_rows .= '<td class=' . $classname . '>' . $attached_to_link . '</td>';
-	        $activity_rows .= '<td class=' . $classname . '>' . $con->userdate($rst->fields['scheduled_at']) . '</td>';
-	        $activity_rows .= '<td class=' . $classname . '>' . $con->userdate($rst->fields['ends_at']) . '</td>';
-	        $activity_rows .= '</tr>';
-	        $rst->movenext();
-	    }
-	    $rst->close();
-	} else {
-	   db_error_handler ($con, $sql_activities);
-	}
 
-	$activities_widget = "
-        <!-- Activity Rows //-->
-        <table class=widget cellspacing=1 width=\"100%\">
-            <tr>
-                <td class=widget_header colspan=7>" .  _("Open Activities") . "</td>
-            </tr>
-            <tr>
-                <td class=widget_label>" . _("Activity") . "</td>
-                <td class=widget_label>" . _("Type") . "</td>
-                <td class=widget_label>" . _("Company") . "</td>
-                <td class=widget_label>" . _("Contact") . "</td>
-                <td class=widget_label>" . _("About") . "</td>
-                <td class=widget_label>" . _("Scheduled") . "</td>
-                <td class=widget_label>" . _("Due") . "</td>
-            </tr>
-            $activity_rows
-        </table>";
+    // begin Activities Pager
+	$form_name = 'ActivitiesView';
 
-	
+    $columns = array();
+    $columns[] = array('name' => _('Summary'), 'index_sql' => 'activity_title_link', 'sql_sort_column' => 'a.activity_title');
+    $columns[] = array('name' => _('Type'), 'index_sql' => 'activity_type_pretty_name');
+    $columns[] = array('name' => _('Company'), 'index_sql' => 'company');
+    $columns[] = array('name' => _('Contact'), 'index_sql' => 'contact');
+    $columns[] = array('name' => _('About'), 'index_calc' => 'activity_about');
+    $columns[] = array('name' => _('Scheduled'), 'index_sql' => 'scheduled_at', 'sql_sort_column' => 'a.scheduled_at', 'default_sort' => 'desc');
+    $columns[] = array('name' => _('Due'), 'index_sql' => 'ends_at', 'sql_sort_column' => 'a.ends_at');
+    $default_columns = null;
+    // $default_columns = array('activity_status', 'activity_title_link', 'username','activity_type_pretty_name','contact_name','activity_about','scheduled_at');
+
+
+    // selects the columns this user is interested in
+    $pager_columns = new Pager_Columns('Home_ActivitiesPager', $columns, $default_columns, $form_name);
+    $pager_columns_button = $pager_columns->GetSelectableColumnsButton();
+    $pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
+
+    $columns = $pager_columns->GetUserColumns('default');
+
+    $endrows = "<tr><td class=widget_content_form_element colspan=10>
+                $pager_columns_button
+                </td></tr>";
+
+    // this is the callback function that the pager uses to fill in the calculated data.
+    $pager = new GUP_Pager($con, $sql_activities, 'GetActivitiesPagerData', _('Open Activities'), $form_name, 'Home_ActivitiesPager', $columns, false, true);
+    $pager->AddEndRows($endrows);
+
+	$activities_widget = $pager->Render($system_rows_per_page);
+
 	$_SESSION['next_to_check']=$next_to_check;
 	
 } else {
@@ -216,6 +177,27 @@ if('list' == $results_view_type) {
         	$i++;
     	}
 	}
+
+	$search_date = date('Y-m-d');
+
+    switch($results_view_type) {
+        case 'week':
+            // align it to the week's start day (e.g. Monday)
+            if(empty($calendar_start_date)) {
+                $calendar_start_date = CalendarView::GetWeekStart($start_date, 'Monday');
+            } else {
+                $calendar_start_date = CalendarView::GetWeekStart($calendar_start_date, 'Monday');
+            }
+            break;
+        case 'month':
+            if(empty($calendar_start_date)) {
+            	$calendar_start_date = date("Y-m-", strtotime($date_modifier . $search_date));
+            	$calendar_start_date .= '01';
+			}
+            break;
+    }
+	$_POST['calendar_start_date'] = $calendar_start_date;
+
 
 	$calendar = new CalendarView($con, 'ActivitiesView', 'calendar_start_date', $results_view_type);
 
@@ -527,7 +509,7 @@ start_page($page_title,true,$msg);
 <div id="Main">
     <div id="Content">
 		<!-- Display Type -->
-		<form action="home.php" method="GET" name="ActivitiesView">
+		<form action="home.php" method="POST" name="ActivitiesView">
         <table class=widget cellspacing=1 width="100%">
             <tr>
                 <td class=widget_header colspan=7><?php echo _("Activities View"); ?></td>
@@ -549,6 +531,7 @@ start_page($page_title,true,$msg);
 			</tr>
 		</table>
 		<!-- List or Calendar View -->
+		<?php echo $pager_columns_selects; ?>
 		<?php echo $activities_widget; ?>
 		</form>
 
@@ -595,6 +578,9 @@ end_page();
 
 /**
  * $Log: home.php,v $
+ * Revision 1.49  2005/05/20 23:13:43  daturaarutad
+ * updated to use GUP_Pager for activities; small change for calendar which is not quite perfect but better at starting on a reasonable date
+ *
  * Revision 1.48  2005/05/18 16:18:02  daturaarutad
  * updated Calendar constructor and include location
  *
