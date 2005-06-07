cccc8671324d115a9423510a06c15af301ca789f
braverock <braverock>
2005-06-07 21:14:43 +0000

    - change to use fetch_default_address fn to set new business address for contact

--- contacts/transfer-3.php
+++ contacts/transfer-3.php
@@ -2,7 +2,7 @@
 /**
  * Database updates for transfer contact
  *
- * $Id: transfer-3.php,v 1.1 2004/07/19 22:18:09 neildogg Exp $
+ * $Id: transfer-3.php,v 1.2 2005/06/07 21:14:43 braverock Exp $
  */
 
 
@@ -25,14 +25,10 @@ $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
 // $con->debug = 1;
 
-//the order by ensure that the first address entered is used, this would normally be the main address except in unusual cirumstances
-//if no record if found then the transfer contact will have there new adddress_id set as 0 which is better than still showing up in the used by field of the old company
-$sql = "SELECT * FROM addresses where company_id = $new_company_id ORDER BY address_id ASC";
-$rst = $con->execute($sql);
+// set the business address for this contact to the primary address of the new company.
+// not perfect, but the user will have a chance to change it from the contacts/one.php page
 $new_address_id = 0;
-if (!$rst->EOF) {
-	$new_address_id = $rst->fields['address_id'];
-}
+$new_address_id = fetch_default_address($con, $new_company_id)
 
 $sql = "SELECT * FROM contacts WHERE contact_id = $contact_id";
 $rst = $con->execute($sql);
@@ -48,23 +44,23 @@ add_audit_item($con, $session_user_id, 'transferred', 'contacts', $contact_id, 1
 if($everywhere) {
     $sql = "SELECT * FROM activities WHERE contact_id = $contact_id";
     $rst = $con->execute($sql);
-    
+
     $rec = array();
     $rec['company_id'] = $new_company_id;
     $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
     $con->execute($upd);
-    
+
     $sql = "SELECT * FROM cases WHERE contact_id = $contact_id";
     $rst = $con->execute($sql);
-    
+
     $rec = array();
     $rec['company_id'] = $new_company_id;
     $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
     $con->execute($upd);
-    
+
     $sql = "SELECT * FROM opportunities WHERE contact_id = $contact_id";
     $rst = $con->execute($sql);
-    
+
     $rec = array();
     $rec['company_id'] = $new_company_id;
     $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
@@ -73,28 +69,16 @@ if($everywhere) {
 
 $con->close();
 
+//now go to the Contact Details page the user can make additional changes if they need to
 header("Location: one.php?msg=saved&contact_id=$contact_id");
-
 
 /**
  * $Log: transfer-3.php,v $
+ * Revision 1.2  2005/06/07 21:14:43  braverock
+ * - change to use fetch_default_address fn to set new business address for contact
+ *
  * Revision 1.1  2004/07/19 22:18:09  neildogg
  * - Added company search box
  *  - Added move all records with contact
- *
- * Revision 1.3  2004/06/30 11:38:16  braverock
- * - assign default address to contact after transfer
- *   - applies patch provided by David Uhlman
- *
- * Revision 1.2  2004/06/15 17:26:21  introspectshun
- * - Add adodb-params.php include for multi-db compatibility.
- * - Corrected order of arguments to implode() function.
- * - Now use ADODB GetInsertSQL, GetUpdateSQL and Concat functions.
- *
- * Revision 1.1  2004/06/09 19:25:11  gpowers
- * - database updates for transfer of contact to new company
- *
- *
  */
-
 ?>
