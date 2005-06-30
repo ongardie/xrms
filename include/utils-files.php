fc11fb53ff95130046ddcbfed30525ef1cd84f13
jswalter <jswalter>
2005-06-30 21:53:43 +0000

    - initial commit

---
+++ include/utils-files.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * utils-files.php - this file contains file and filesystem utility functions for XRMS
+ *
+ * Functions in this file may be used throughout XRMS.
+ * Almost all files in the system include this file.
+ *
+ * @author Walter Torres
+ *
+ * $Id: utils-files.php,v 1.1 2005/06/30 21:53:43 jswalter Exp $
+ */
+
+if ( !defined('IN_XRMS') )
+{
+  die('Hacking attempt');
+  exit;
+}
+
+// ************************************************************************
+// ************************************************************************
+
+
+
+// ************************************************************************
+// ************************************************************************
+
+/**
+ * $Log: utils-files.php,v $
+ * Revision 1.1  2005/06/30 21:53:43  jswalter
+ *  - initial commit
+ *
+ *
+ */
+?>
