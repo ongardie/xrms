66e1dbb3c616affc191b3e1d7da36ab908935030
vanmer <vanmer>
2005-07-08 18:50:05 +0000

    - changed to redirect when called directly - added case so that only user preferences will be saved, not system preferences

--- admin/users/user_prefs.php
+++ admin/users/user_prefs.php
@@ -5,7 +5,7 @@
  * Administration screen for managing user preferences
  *
  *
- * $Id: user_prefs.php,v 1.2 2005/07/06 17:23:01 vanmer Exp $
+ * $Id: user_prefs.php,v 1.3 2005/07/08 18:50:05 vanmer Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -19,7 +19,7 @@ $session_user_id = session_check();
 
 getGlobalVar($msg, 'msg');
 getGlobalVar($preference_action,'preference_action');
-if (!$preference_action) $preference_action='displayPrefs';
+if (!$preference_action) $preference_action='redirect';
 
 $user_id=$session_user_id;
 
@@ -27,7 +27,6 @@ $page_title="User Preferences";
 
 $con = &adonewconnection($xrms_db_dbtype);
 $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
-
 //get all user preference types
 $types=get_user_preference_type($con, false, false, true);
 if (!$types) { $msg="Failed to load an user preference types, no user preferences available"; $user_preferences_table='';}
@@ -35,7 +34,7 @@ else {
     switch ($preference_action) {
         case 'savePrefs':
             foreach ($types as $type_info) {
-                if ($type_info['allow_user_edit_flag']==0) next;
+                if ($type_info['allow_user_edit_flag']==0) continue;
                 if ($type_info['allow_multiple_flag']==1) {
                  //do a think of multiple options
                 } else {
@@ -51,32 +50,15 @@ else {
                    }
                 }
             }
+            unset($_SESSION['XRMS_function_cache']['get_user_preference']);
             if (!$msg) $msg=_("Preferences successfully saved");
 //            $msg=urlencode($msg);
+        case 'redirect':
             Header("Location: self.php?msg=$msg");
         break;
         default:
         case 'displayPrefs':
-            $user_preferences_table="<table class=widget>";
-            foreach ($types as $type_info) {
-                if ($type_info['allow_user_edit_flag']==0) next;
-                $user_preference_type_id=$type_info['user_preference_type_id'];
-                $type_desc=$type_info['user_preference_description'];
-                $type_pretty_name=$type_info['user_preference_pretty_name'];
-                if (!$type_pretty_name) $type_pretty_name=$type_info['user_preference_name'];
-                
-                if ($type_info['allow_multiple_flag']==1) {
-                    //branch for showing multiple options, fetch all user set options   
-                    $element_field=render_preference_form_multi_element($con, $user_id, $user_preference_type_id, $type_info);
-                } else {
-                //branch for showing single option
-                    $preference_value=get_user_preference($con, $user_id, $user_preference_type_id);
-                    $element_field=render_preference_form_element($con, $user_preference_type_id, $preference_value, $type_info);
-                }
-                $user_preferences_table.="<tr><td class=widget_content_label><b>$type_pretty_name</b><br>$type_desc</td><td class=widget_content_form_element>$element_field</td></tr>";
-            }
-            $user_preferences_table.="<tr><td class=widget_content_form_element><input type=hidden name=preference_action value=savePrefs><input class=button type=submit value=\""._("Save Preferences") . "\"></tr></td>";
-            $user_preferences_table.="</table>";
+            return false;
         break;
     }
 }
