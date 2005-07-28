0efeadbd6c625898d6b6d1cba4f38af3144532ab
vanmer <vanmer>
2005-07-28 15:44:40 +0000

    - changed sidebar to use GUP_Pager instead of direct HTML - changed from form submit for new case from sidebar to use javascript button with onclick

--- cases/sidebar.php
+++ cases/sidebar.php
@@ -2,13 +2,16 @@
 /**
  * Sidebar box for Cases
  *
- * $Id: sidebar.php,v 1.14 2005/07/11 22:48:19 vanmer Exp $
+ * $Id: sidebar.php,v 1.15 2005/07/28 15:44:40 vanmer Exp $
  */
 if ( !defined('IN_XRMS') )
 {
   die('Hacking attempt');
   exit;
 }
+
+require_once($include_directory . 'classes/Pager/GUP_Pager.php');
+require_once($include_directory . 'classes/Pager/Pager_Columns.php');
 
 /*  // Commented until ACL system is fully implemented    
     $caseList=get_list($session_user_id, 'Read', false, 'cases');
@@ -20,10 +23,106 @@ if ( !defined('IN_XRMS') )
     }
 */
 
-$case_rows = "<div id='case_sidebar'>
+$case_sidebar_header=_("Open Cases");
+$case_sidebar_form_id='SidebarCases';
+$target=$http_site_root.current_page();
+
+$case_rows = "<div id='case_sidebar'>";
+
+$case_rows.="<form action=\"$target\" name=\"$case_sidebar_form_id\" method=GET><input type=hidden name=contact_id value=$contact_id><input type=hidden name=company_id value=$company_id><input type=hidden name=division_id value=$division_id>";
+
+
+if (!$cases_sidebar_rows_per_page) {
+    $cases_sidebar_rows_per_page=5;
+}
+
+//build the cases sql query
+$cases_sql = "SELECT " . $con->Concat("'<a href=\"$http_site_root/cases/one.php?case_id='", "cases.case_id", "'\">'", "cases.case_title", "'</a>'") . " AS case_name, cases.*, " .$con->SQLDate('Y-m-d', 'cases.due_at') . " AS due, case_types.case_type_pretty_name as type,
+case_statuses.case_status_pretty_name as status,
+c.company_name as company, users.username as username, case_priorities.case_priority_pretty_name as priority
+              FROM cases, case_priorities, users, case_statuses, companies c, case_types
+              WHERE cases.case_priority_id = case_priorities.case_priority_id
+                and cases.user_id = users.user_id
+                and case_record_status = 'a'
+                and cases.company_id = c.company_id
+                and cases.case_status_id = case_statuses.case_status_id
+                and cases.case_type_id = case_types.case_type_id
+                and case_statuses.status_open_indicator = 'o'
+                $case_limit_sql";
+
+$columns = array();
+$columns[] = array('name' => _('Case'), 'index_sql' => 'case_name', 'sql_sort_column' => 'cases.case_title', 'type' => 'url');
+$columns[] = array('name' => _('Priority'), 'index_sql' => 'priority');
+$columns[] = array('name' => _('Type'), 'index_sql' => 'type');
+$columns[] = array('name' => _('Status'), 'index_sql' => 'status');
+$columns[] = array('name' => _('Due'), 'index_sql' => 'due');
+$columns[] = array('name' => _('Company'), 'index_sql' => 'company');
+$columns[] = array('name' => _('Owner'), 'index_sql' => 'username');
+
+// no reason to set this if you don't want all by default
+if (!$case_sidebar_default_columns) $case_sidebar_default_columns = array('case_name', 'priority','type', 'due');
+
+$pager_columns = new Pager_Columns('CasesSidebarPager', $columns, $case_sidebar_default_columns, $case_sidebar_form_id);
+$pager_columns_button = $pager_columns->GetSelectableColumnsButton();
+$pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
+
+$columns = $pager_columns->GetUserColumns('default');
+$colspan = count($columns);
+
+// output the selectable columns widget
+$case_rows.= $pager_columns_selects;
+
+// caching is disabled for this pager (since it's all sql)
+$pager = new GUP_Pager($con, $cases_sql, null,$case_sidebar_header, $case_sidebar_form_id, 'CasesSidebarPager', $columns, false, true);
+
+// set up the bottom row of buttons
+/*
+$endrows = "<tr><td class=widget_content_form_element colspan=10>
+            $pager_columns_button
+            " . $pager->GetAndUseExportButton() .  "
+            <input type=button class=button onclick=\"javascript: bulkEmail();\" value=\"" . _('Mail Merge') . "\"></td></tr>";
+*/
+
+//put in the new and search buttons
+if ( (isset($company_id) && (strlen($company_id) > 0))  or (isset($contact_id) && (strlen($contact_id) > 0))) {
+    $new_case_button=render_create_button("New",'button',"javascript:location.href='$http_site_root/cases/new.php?company_id=$company_id&division_id=$division_id&contact_id=$contact_id';", false, false, 'cases');
+    if ($new_case_button) {
+        $case_type_sql = "SELECT case_type_pretty_name,case_type_id
+                          FROM case_types
+                          WHERE case_type_record_status = 'a'
+                          ORDER BY case_type_pretty_name";
+        $type_rst=$con->execute($case_type_sql);
+        $new_case_types=$type_rst->getmenu2('case_type_id', '', false);
+        $new_case_button=$new_case_types.$new_case_button; 
+    }
+    
+    $endrows = "
+            <tr>
+                <td class=widget_content_form_element colspan=$colspan>
+                    $pager_columns_button<br>
+                    $new_case_button
+                    <input type=button class=button onclick=\"javascript:location.href='".$http_site_root."/cases/some.php';\" value='" . _("Search") . "'>
+                </td>
+                </form>
+            </tr>\n";
+} else {
+    $endrows ="
+            <tr>
+                <td class=widget_content_form_element colspan=$colspan>
+                    $pager_columns_button
+                    <input type=button class=button onclick=\"javascript:location.href='".$http_site_root."/cases/some.php';\" value='" . _("Search") . "'>
+                </td>
+            </tr>\n";
+}
+
+$pager->AddEndRows($endrows);
+
+$case_rows.=$pager->Render($cases_sidebar_rows_per_page);
+
+/*
         <table class=widget cellspacing=1 width=\"100%\">
             <tr>
-                <td class=widget_header colspan=5>" . _("Open Cases") . "</td>
+                <td class=widget_header colspan=5>" .  . "</td>
             </tr>
             <tr>
                 <td class=widget_label>" . _("Name") . "</td>
@@ -32,21 +131,12 @@ $case_rows = "<div id='case_sidebar'>
                 <td class=widget_label>" . _("Due") . "</td>
             </tr>\n";
 
-//build the cases sql query
-$cases_sql = "SELECT *
-              FROM cases, case_priorities, users, case_statuses
-              WHERE cases.case_priority_id = case_priorities.case_priority_id
-                and cases.user_id = users.user_id
-                and case_record_status = 'a'
-                and cases.case_status_id = case_statuses.case_status_id
-                and case_statuses.status_open_indicator = 'o'
-                $case_limit_sql
-              ORDER BY due_at";
-
+*/
 //uncomment the debug line to see what's going on with the query
 //$con->debug=1;
 
 //execute our query
+/*
 $rst = $con->SelectLimit($cases_sql, 5, 0);
 
 if (strlen($rst->fields['username'])>0) {
@@ -63,45 +153,18 @@ if (strlen($rst->fields['username'])>0) {
 } else {
     $case_rows .= "            <tr> <td class=widget_content colspan=5> " . _("No open cases") . " </td> </tr>\n";
 }
+*/
 
-//put in the new and search buttons
-if ( (isset($company_id) && (strlen($company_id) > 0))  or (isset($contact_id) && (strlen($contact_id) > 0))) {
-    $new_case_button=render_create_button("New",'submit', false, false, false, 'cases');
-    if ($new_case_button) {
-        $case_type_sql = "SELECT case_type_pretty_name,case_type_id
-                          FROM case_types
-                          WHERE case_type_record_status = 'a'
-                          ORDER BY case_type_pretty_name";
-        $type_rst=$con->execute($case_type_sql);
-        $new_case_types=$type_rst->getmenu2('case_type_id', '', false);
-        $new_case_button=$new_case_types.$new_case_button; 
-    }
-    $case_rows .= "
-            <tr>
-                <form action='".$http_site_root."/cases/new.php' method='post'>
-                <input type='hidden' name='company_id' value='$company_id'>
-                <input type='hidden' name='division_id' value='$division_id'>
-                <input type='hidden' name='contact_id' value='$contact_id'>
-                <td class=widget_content_form_element colspan=5>
-                    $new_case_button
-                    <input type=button class=button onclick=\"javascript:location.href='".$http_site_root."/cases/some.php';\" value='" . _("Search") . "'>
-                </td>
-                </form>
-            </tr>\n";
-} else {
-    $case_rows .="
-            <tr>
-                <td class=widget_content_form_element colspan=5>
-                    <input type=button class=button onclick=\"javascript:location.href='".$http_site_root."/cases/some.php';\" value='" . _("Search") . "'>
-                </td>
-            </tr>\n";
-}
 
 //now close the table, we're done
-$case_rows .= "        </table>\n</div>";
+$case_rows .= "</form></div>";
 
 /**
  * $Log: sidebar.php,v $
+ * Revision 1.15  2005/07/28 15:44:40  vanmer
+ * - changed sidebar to use GUP_Pager instead of direct HTML
+ * - changed from form submit for new case from sidebar to use javascript button with onclick
+ *
  * Revision 1.14  2005/07/11 22:48:19  vanmer
  * - added table name to explicitly check permissions on cases instead of on parent (company)
  *
