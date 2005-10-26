da832e5733dbe7cabcf18225e597033d84f8ea91
niclowe <niclowe>
2005-10-26 21:05:25 +0000

    added auto install of database

--- plugins/teamnotice/setup.php
+++ plugins/teamnotice/setup.php
@@ -4,7 +4,7 @@
   *
   * Copyright (c) 2004 The XRMS Project Team
   *
-  * $Id: setup.php,v 1.1 2005/09/29 19:35:27 niclowe Exp $
+  * $Id: setup.php,v 1.2 2005/10/26 21:05:25 niclowe Exp $
 
   @todo - have option to show notices on frontpage on logon - maybe add hook to private/home.php
 
@@ -29,6 +29,7 @@
         </tr>
         <tr>
         <td>';
+
 				
     $sql = "select * from  teamnotices where status='a'";
     //$con->debug=1;
@@ -79,11 +80,30 @@
   }
 
   function teamnotice_setup() {
-    global $http_site_root, $xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname;
+    global $http_site_root, $xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname,$xrms_file_root;
     $con = &adonewconnection($xrms_db_dbtype);
     $con->connect($xrms_db_server, $xrms_db_username, $xrms_db_password, $xrms_db_dbname);
-    // $con->debug = 1;
 
+    // $con->debug = 1;
+		//'check if it exists
+		$sql = "DESCRIBE teamnotices";
+    //$con->debug=1;
+    $rst = $con->execute($sql);
+		if (!$rst){
+			 //'run the sql that creates the DB
+			 $sql="CREATE TABLE `teamnotices` (
+            `teamnotice_id` int(11) NOT NULL auto_increment,
+            `notice_heading` text NOT NULL,
+            `notice_text` longtext NOT NULL,
+            `status` enum('a','d') NOT NULL default 'a',
+            PRIMARY KEY  (`teamnotice_id`)
+          )";
+      $con->execute($sql);
+      $sql="INSERT INTO `teamnotices` VALUES (1, 'Test of Team Notice Header', 'Test of Team Notice Text:\r\nYou might put in here:\r\nSALESPEOPLE PLEASE WORK HARDER!', 'a')";
+      $con->execute($sql);
+      $sql="INSERT INTO `teamnotices` VALUES (2, 'Where to Find Team Notices', '<a href=../plugins/teamnotice/teamnotice_list.php>Create or Delete your team notices here</a> ', 'a')";
+      $con->execute($sql);
+		}
     echo "<tr><td class=widget_content>\n<a href='$http_site_root/plugins/teamnotice/teamnotice_list.php'>Manage Team Notices</a>\n</td>\n</tr>\n";
   }
 
