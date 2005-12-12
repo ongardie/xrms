9a0a3351903973dbfd06d6f11f4487ba636b36d3
daturaarutad <daturaarutad>
2005-12-12 17:36:12 +0000

    Wrapper for GUP_Pager export

---
+++ export/export.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Wrapper for GU_Pager export
+ *
+ * $Id: export.php,v 1.1 2005/12/12 17:36:12 daturaarutad Exp $
+ */
+
+require_once ('../include-locations.inc';)
+
+require_once ($include_directory.'classes/Pager/pager-export.php');
+
+/**
+ * $Log: export.php,v $
+ * Revision 1.1  2005/12/12 17:36:12  daturaarutad
+ * Wrapper for GUP_Pager export
+ *
+ */
+?>
