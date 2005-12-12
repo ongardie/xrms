e5865ec2e8a1e1b4c0efb3b9c71ae21184d753b6
vanmer <vanmer>
2005-12-12 20:00:40 +0000

    - changed to use GUP_Pager instead of deprecated adodb pager

--- admin/acl/ControlledObject_list.php
+++ admin/acl/ControlledObject_list.php
@@ -6,7 +6,7 @@
  * All Rights Reserved.
  *
  * @todo
- * $Id: ControlledObject_list.php,v 1.4 2005/08/11 22:52:15 vanmer Exp $
+ * $Id: ControlledObject_list.php,v 1.5 2005/12/12 20:00:40 vanmer Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -15,7 +15,8 @@ require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
 require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
-require_once($include_directory . 'adodb/adodb-pager.inc.php');
+require_once($include_directory . 'classes/Pager/GUP_Pager.php');
+require_once($include_directory . 'classes/Pager/Pager_Columns.php');
 
 global $http_site_root;
 
@@ -25,90 +26,58 @@ require_once ($include_directory.'classes/acl/xrms_acl_config.php');
 
 $con = get_acl_dbconnection();
 
+getGlobalVar($msg, 'msg');
 
 $page_title = _("Manage Controlled Objects");
 
-// begin sorted columns stuff
-getGlobalVar($sort_column, 'sort_column'); 
-getGlobalVar($current_sort_column, 'current_sort_column'); 
-getGlobalVar($sort_order, 'sort_order'); 
-getGlobalVar($current_sort_order, 'current_sort_order'); 
-getGlobalVar($ControlledObject_next_page, 'ControlledObject_next_page'); 
-getGlobalVar($resort, 'resort'); 
+$sql="SELECT " . $con->Concat($con->qstr("<input type=\"button\" class=\"button\" value=\"Edit\" onclick=\"javascript: location.href='one_ControlledObject.php?form_action=edit&return_url=ControlledObject_list.php&ControlledObject_id="), 'ControlledObject_id', $con->qstr("'\">")) . "AS LINK, ControlledObject_name, on_what_table, on_what_field, data_source.data_source_name FROM ControlledObject  JOIN data_source ON data_source.data_source_id=ControlledObject.data_source_id";
 
-if (!strlen($sort_column) > 0) {
-    $sort_column = 1;
-		$current_sort_column = $sort_column;
-    $sort_order = "asc";
-}
+$form_id="ControlledObjectsForm";
+
+    $columns = array();
+    $columns[] = array('name' => _("Edit"), 'index_sql' => 'LINK');
+    $columns[] = array('name' => _("Object Name"), 'index_sql' => 'ControlledObject_name');
+    $columns[] = array('name' => _("Source Table"), 'index_sql' => 'on_what_table');
+    $columns[] = array('name' => _("Identifying Field"), 'index_sql' => 'on_what_field');
+    $columns[] = array('name' => _("Data Source"), 'index_sql' => 'data_source_name');
+
+    $default_columns=array('LINK','ControlledObject_name', 'on_what_table', 'data_source_name');
     
-if (!($sort_column == $current_sort_column)) {
-    $sort_order = "asc";
-}
+    $pager_columns = new Pager_Columns('ControlledObjectsPager', $columns, $default_columns, $form_id);
+    $pager_columns_button = $pager_columns->GetSelectableColumnsButton();
+    $pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
+
+    $columns = $pager_columns->GetUserColumns('default');
+    $colspan = count($columns);
+
+        $endrows =  "
+            <tr>
+                <td colspan=$colspan class=widget_content_form_element>
+                    <input type=\"button\" class=\"button\" value=\"". _("Add New") . "\" onclick=\"javascript: location.href='one_ControlledObject.php?form_action=new&return_url=ControlledObject_list.php'\">
+                    $pager_columns_button
+                </td>
+            </tr>";
+
+   $pager = new GUP_Pager($con, $sql,false, 'Controlled Objects', $form_id, 'ControlledObjects', $columns);
+
+    $pager->AddEndRows($endrows);
 
 
-$opposite_sort_order = ($sort_order == "asc") ? "desc" : "asc";
-$sort_order = (($resort) && ($current_sort_column == $sort_column)) ? $opposite_sort_order : $sort_order;
-
-$ascending_order_image = ' <img border=0 height=10 width=10 src="' . $http_site_root . '/img/asc.gif" alt="">';
-$descending_order_image = ' <img border=0 height=10 width=10 src="' . $http_site_root . '/img/desc.gif" alt="">';
-$pretty_sort_order = ($sort_order == "asc") ? $ascending_order_image : $descending_order_image;
-
-$order_by = $sort_column;
-
-
-
-$order_by .= " $sort_order";
-// end sorted columns stuff
-
-
-$sql="SELECT " . $con->Concat($con->qstr("<input type=\"button\" class=\"button\" value=\"Edit\" onclick=\"javascript: location.href='one_ControlledObject.php?form_action=edit&return_url=ControlledObject_list.php&ControlledObject_id="), 'ControlledObject_id', $con->qstr("'\">")) . "AS LINK, ControlledObject_name as 'Object Name', on_what_table as 'Source Table', on_what_field as 'Identifying Field', data_source.data_source_name as 'Data Source' FROM ControlledObject  JOIN data_source ON data_source.data_source_id=ControlledObject.data_source_id order by $order_by";
 
 $css_theme='basic-left';
-start_page($page_title);
-?>
-
-<script language="JavaScript" type="text/javascript">
-<!--
-
-function submitForm(nextPage) {
-    document.forms[0].ControlledObject_next_page.value = nextPage;
-    document.forms[0].submit();
-}
-
-function resort(sortColumn) {
-    document.forms[0].sort_column.value = sortColumn + 1;
-    document.forms[0].ControlledObject_next_page.value = '';
-    document.forms[0].resort.value = 1;
-    document.forms[0].submit();
-}
-
-//-->
-</script>
-
-
-<form method="POST">
-<input type=hidden name=use_post_vars value=1>
-<input type=hidden name=ControlledObject_next_page value="<?php  echo $ControlledObject_next_page; ?>">
-<input type=hidden name=resort value="0">
-<input type=hidden name=current_sort_column value="<?php  echo $sort_column; ?>">
-<input type=hidden name=sort_column value="<?php  echo $sort_column; ?>">
-<input type=hidden name=current_sort_order value="<?php  echo $sort_order; ?>">
-<input type=hidden name=sort_order value="<?php  echo $sort_order; ?>">
-
-
-
-<?php
-
+start_page($page_title, true, $msg);
+echo '<form method="POST" name="'.$form_id . '">';
 echo "<div id='Main'>";
 require_once('xrms_acl_nav.php');
 echo '<div id=Content>';
 
-$pager = new ADODB_Pager($con, $sql, 'ControlledObject', false, $sort_column-1, $pretty_sort_order);
+
+
+echo $pager_columns_selects;
 $pager->Render();
 
 ?>
-<input type="button" class="button" value="<?php echo _("Add New"); ?>" onclick="javascript: location.href='one_ControlledObject.php?form_action=new&return_url=ControlledObject_list.php'">
+
 </div></div></form>
 
 <?php
@@ -116,6 +85,9 @@ end_page();
 
 /**
  * $Log: ControlledObject_list.php,v $
+ * Revision 1.5  2005/12/12 20:00:40  vanmer
+ * - changed to use GUP_Pager instead of deprecated adodb pager
+ *
  * Revision 1.4  2005/08/11 22:52:15  vanmer
  * - changed to use ACL dbconnection
  *
