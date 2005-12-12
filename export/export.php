14743eda6421968cfe20a886768464245d62ee98
daturaarutad <daturaarutad>
2005-12-12 17:39:36 +0000

    fix syntax error

--- export/export.php
+++ export/export.php
@@ -2,15 +2,18 @@
 /**
  * Wrapper for GU_Pager export
  *
- * $Id: export.php,v 1.1 2005/12/12 17:36:12 daturaarutad Exp $
+ * $Id: export.php,v 1.2 2005/12/12 17:39:36 daturaarutad Exp $
  */
 
-require_once ('../include-locations.inc';)
+require_once ('../include-locations.inc');
 
 require_once ($include_directory.'classes/Pager/pager-export.php');
 
 /**
  * $Log: export.php,v $
+ * Revision 1.2  2005/12/12 17:39:36  daturaarutad
+ * fix syntax error
+ *
  * Revision 1.1  2005/12/12 17:36:12  daturaarutad
  * Wrapper for GUP_Pager export
  *
