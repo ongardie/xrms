5d748b4a05440d9ccdcda0dbf89af7d7651018ab
daturaarutad <daturaarutad>
2006-02-01 21:41:00 +0000

    big update to use HTML_QuickForm_advmultiselect class for widget and allow saveable views

--- include/classes/Pager/Pager_Columns.php
+++ include/classes/Pager/Pager_Columns.php
@@ -2,24 +2,7 @@
 /**
  * Pager_Columns class
  *
- * This class works with the $columns parameter of XRMS_Pager to allow the user to change the pager layout
- *
- * How it works:
- *
- *  - user loads script including pager
- *  - script creates Pager_Columns object
- *  - Pager_Columns object checks to see if new columns have been selected by checking for a CGI var Portfolio1_New_Columns_View,
- *    adding them to session
- *  - pager_columns checks to see if there is a column list in the session (for Portfolio1)
- *  - if there is, it does the thing to drop teh columns
- *  - if not, it does the same, but with the default list
- *  - page renders
- *
- *  - user clicks 'display selectable columns'
- *  - div hidden toggles and changes text to 'hide selectable columns'
- *  - user clicks around and at some point is happy with their columns list and
- *    clicks 'update column view for this session' button which triggers a javascript function
- *  - javascript packs the column list into a variable along with the pager name, submits form
+ * This class works with the $columns parameter of GUP_Pager to allow the user to change the pager layout
  *
  * Click the link below to run the example code (you may need to modify the URL of this link)
  *
@@ -27,21 +10,33 @@
  *
  * @example Pager_Columns.doc.1.php check out
  *
- * $Id: Pager_Columns.php,v 1.16 2006/01/28 13:07:16 vanmer Exp $
+ * $Id: Pager_Columns.php,v 1.17 2006/02/01 21:41:00 daturaarutad Exp $
  */
 
 class Pager_Columns {
 
     var $pager_name;
     var $pager_columns;
+    var $prefs_columns;
     var $default_columns;
     var $form_id;
     var $visible_column_size;
 
-    function Pager_Columns($pager_name, $pager_columns, $default_columns, $form_id, $visible_column_size=4) {
+    /**
+    * @param string pager name/identifier
+    * @param array pager columns to modify
+    * @param array column names to use as 'default' view
+    * @param string needed for JS to modify form
+    * @param integer vertical height of multi-select widget
+    */
+    function Pager_Columns($pager_name, $pager_columns, $default_columns, $form_id, $visible_column_size=6) {
 
-        getGlobalVar($new_columns_view, $pager_name . '_New_Columns_View');
+        global $session_user_id;
+        $con = get_xrms_dbconnection();
 
+        getGlobalVar($pager_columns_action, $pager_name . '_pager_columns_action');
+        getGlobalVar($view_name, $pager_name . '_view_name');
+        getGlobalVar($view_name_new, $pager_name . '_view_name_new');
 
         $columns = array();
 
@@ -61,42 +56,86 @@ class Pager_Columns {
 
         $pager_columns = $columns;
 
-        if($new_columns_view) {
-
-            // getGlobalVar for the user_columns, pack into an array $user_columns
-            getGlobalVar($new_columns, $pager_name . '_New_Columns');
-
-            $user_columns = explode(',', $new_columns);
-            //$user_columns = array(explode(',' $new_columns));
-
-            // set it in the session
-            $_SESSION[$pager_name . '_columns_view'] = $user_columns;
-        }
-
-
         if(!$default_columns) {
-            foreach($pager_columns as $index => $pager_column) {
-                $default_columns[] = $index;
-            }
+            $default_columns = array_keys($pager_columns);
         }
+
+
+        // this function checks for existance first and creates if it does not exist
+        add_user_preference_type($con, 'pager_columns');
+
+
+        // read this user's pager_columns preference 
+        $this->prefs_columns = unserialize(get_user_preference($con, $session_user_id, 'pager_columns', false, false, false));
+
+        // look for this pager in the preferences array, initialize one (default view) and write if it does not exist.
+        if(!isset($this->prefs_columns[$pager_name]) || !is_array($this->prefs_columns[$pager_name])) {
+
+            $this->prefs_columns[$pager_name]['default'] = $default_columns;
+
+            set_user_preference($con, $session_user_id, 'pager_columns', serialize($this->prefs_columns), false, true);
+        }
+ 
+
+        // if we are saving user's prefs..
+        if('save' == $pager_columns_action) {
+
+            // use new name first or selected name
+            $view_name = $view_name_new ? $view_name_new : $view_name;
+
+            // getGlobalVar for the user_columns, as array
+            getGlobalVar($user_columns, $pager_name . '_pager_columns');
+            // set this view-name's columns list
+            $this->prefs_columns[$pager_name][$view_name] = $user_columns;
+            // write
+            set_user_preference($con, $session_user_id, 'pager_columns', serialize($this->prefs_columns), false, true);
+
+        } elseif ('delete' == $pager_columns_action) {
+            // remove from array 
+            unset($this->prefs_columns[$pager_name][$view_name]);
+            // write
+            set_user_preference($con, $session_user_id, 'pager_columns', serialize($this->prefs_columns), false, true);
+            // default always exists (see above)
+            $view_name = 'default';
+        }
+
+        // store view name in the session
+        $_SESSION[$pager_name . '_columns_view'] = $view_name;
+
+        // create <option> tags for views list
+        foreach(array_keys($this->prefs_columns[$pager_name]) as $option_view_name ) {
+            $this->view_options .= "<option value=\"$option_view_name\"" . ($view_name == $option_view_name ? ' selected="selected"' : '') . ">$option_view_name</option>\n";
+        }
+
 
         $this->pager_name = $pager_name;
         $this->pager_columns = $pager_columns;
         $this->default_columns = $default_columns;
         $this->form_id = $form_id;
-	$this->visible_column_size=$visible_column_size;
+	    $this->visible_column_size=$visible_column_size;
     }
 
+    /**
+    * Returns an array of column names for this session's view (uses 'default' if no view set)
+    *
+    * @return array Column Names
+    */
     function GetUserColumnNames() {
 
         // User Columns will come from the session or default list passed in
-        if($_SESSION[$this->pager_name . '_columns_view']) {
-            return $_SESSION[$this->pager_name . '_columns_view'];
-        } else {
-            return $this->default_columns;
-        }
+        $view_name = isset($_SESSION[$this->pager_name . '_columns_view']) ? $_SESSION[$this->pager_name . '_columns_view'] : 'default';
+            
+        $columns =  $this->prefs_columns[$this->pager_name][$view_name];
+
+        return $columns;
     }
 
+
+    /**
+    * Returns the sub-array of the initial pager_columns array to be used with the pager
+    *
+    * @return array pager_columns 
+    */
     function GetUserColumns() {
 
         $return_pager_columns = array();
@@ -109,85 +148,128 @@ class Pager_Columns {
         }
         return $return_pager_columns;
     }
-
+    /**
+    * Returns the button to un-hide the widget
+    *
+    * @return string HTML for Select Column Layouts button
+    */
     function GetSelectableColumnsButton() {
 
         return '<input type="button" class="button" onclick="document.getElementById(\'' . $this->pager_name . '_widget\').style.display=\'block\'; location.href=\'#' . $this->pager_name . '_select_columns\';" value="' . _("Select Column Layouts") . '">';
     }
+
+    /**
+    * Returns the Selectable Columns Widget
+    *
+    * It is assumed that this widget will be placed within a <form> and the form_id is passed in the constructor.
+    *
+    * @return string HTML for Select Column Layouts widget
+    */
     function GetSelectableColumnsWidget() {
 
-        global $http_site_root;
-
-
+        // Get the column names
         $user_columns = $this->GetUserColumnNames();
-        $avail_columns_options = '';
-        $display_columns_options = '';
-        $reset_columns_rows = '';
-
-        $text_update_columns = _("Update View");
-        $text_reset_columns = _("Reset View");
-        $text_cancel_columns = _("Cancel Changes");
 
         $widget_name = $this->pager_name . '_widget';
 
-        // create localized strings for move buttons
-        $moveone = htmlspecialchars(_("one"));
-        $moveall = htmlspecialchars(_("move all"));
+        //store element names for later use ("__ContactPager_pager_columns[]")
+        $select_name = $this->pager_name . '_pager_columns';
+        $select_name_displayed = '_' . $select_name;
+        $select_name_avail = '__' . $select_name;
 
-        // add currently displayed columns
-        foreach($user_columns as $user_column) {
-            if(isset($user_column)) {
-                $display_columns_options .= "<option value=\"$user_column\">{$this->pager_columns[$user_column]['name']}</option>\n";
-            }
-        }
-
-        // add available columns
+        // compile available columns (name-displayname)
         foreach($this->pager_columns as $pager_column_index => $pager_column) {
-
-
-            if(!in_array($pager_column_index, $user_columns)) {
-                $avail_columns_options .= "<option value=\"$pager_column_index\">{$pager_column['name']}</option>\n";
-            }
-
-            // if in the default columns list, add to displayed, otherwise add to avail when reset button pressed
-            if(true === in_array($pager_column_index, $this->default_columns)) {
-                $reset_columns_rows .= "\t\taddOption('{$this->pager_name}_displayColumns', '{$pager_column['name']}', '$pager_column_index');\n";
-            } else {
-                $reset_columns_rows .= "\t\taddOption('{$this->pager_name}_availColumns', '{$pager_column['name']}', '$pager_column_index');\n";
-            }
+            $avail_columns[$pager_column_index] = $pager_column['name'];
         }
 
-        $s = <<<END
+        // Include PEAR advmultiselect files
+        require_once 'HTML/QuickForm.php';
+        require_once 'HTML/QuickForm/advmultiselect.php';
+        require_once 'HTML/QuickForm/Renderer/Default.php';
+
+        // amsBasic1 and name are ignored since we use our own FormTemplate
+		$form = new HTML_QuickForm('amsBasic1');
+		$form->removeAttribute('name');        // XHTML compliance
+
+	
+		$ams = $form->addElement('advmultiselect', $select_name, null, $avail_columns, array('size' => $this->visible_column_size));
+
+        // This determines which items appear in the "Displayed Columns" side
+        $form->setConstants(array($select_name => $user_columns));
+
+        $widget_caption = _('Select Columns for Display');
+        $displayed_text = _('Displayed Columns');
+        $avail_text = _('Available Columns');
+        $text_save_columns = _("Save");
+        $text_load_columns = _("Load");
+        $text_cancel_columns = _("Hide");
+        $text_delete_columns = _("Delete");
+
+        $ams->setButtonAttributes('add'     , 'class=button');
+        $ams->setButtonAttributes('remove'  , 'class=button');
+        $ams->setButtonAttributes('moveup'  , 'class=button');
+        $ams->setButtonAttributes('movedown', 'class=button');
+        
+        // template for a dual multi-select element shape
+        $template = "
+        <table class=widget>
+        <tr><td class=widget_header colspan=4>$widget_caption <div class=\"right\"><a href='' onclick=\"document.getElementById('{$widget_name}').style.display = 'none'\" >$text_cancel_columns</a></div></td></tr>
+        <tr>
+            <td class=widget_content>$avail_text</td>
+            <td class=widget_content>&nbsp;</td>
+            <td class=widget_content>$displayed_text </td>
+            <td class=widget_content>"._('Views')."</td>
+        </tr>
+        <tr>
+        <td>{unselected}</td>
+        <td align=\"center\">
+            {add}<br />{remove}<br /><br />{moveup}<br />{movedown}<br />
+        </td>
+        <td>{selected}</td>
+        <td class=\"widget_content_center\">
+            <table>
+                <tr><td> 
+            <input type=\"button\" class=\"button\" name=\"button\" onclick=\"{$this->pager_name}_save_view(this)\" value=\"$text_save_columns\"><br/>
+            <input type=\"button\" class=\"button\" name=\"button\" onclick=\"{$this->pager_name}_load_view(this)\" value=\"$text_load_columns\"><br/>
+            <input type=\"button\" class=\"button\" name=\"button\" onclick=\"{$this->pager_name}_delete_view(this)\" value=\"$text_delete_columns\"><br/>
+                </td></tr>
+                <tr><td><input type=text size=10 name={$this->pager_name}_view_name_new><br/>
+                        <select name={$this->pager_name}_view_name>
+                            {$this->view_options}
+                        </select>
+                    </td>
+                </tr>
+
+            </table>
+        </td>
+        </tr>
+        </table>
+        ";
+
+        $ams->setElementTemplate($template);
+
+		
+        $s = $ams->getElementJs(false);
+
+        $renderer =& new HTML_QuickForm_Renderer_Default();
+
+        $form_template = <<<END
         <script language="JavaScript" src="{$http_site_root}/js/jsSelect.js"></script>
 
-        <input type="hidden" name="{$this->pager_name}_New_Columns_View">
-        <input type="hidden" name="{$this->pager_name}_New_Columns">
+        <input type="hidden" name="{$this->pager_name}_pager_columns_action">
 
         <script language="JavaScript" type="text/javascript">
-
-            function {$this->pager_name}_columns_change(obj) {
-
-                document.{$this->form_id}.{$this->pager_name}_New_Columns_View.value = true;
-
-                var objList = document.getElementById('{$this->pager_name}_displayColumns');
-                var strCurItemValue = '';
-
-                for (var i=0; i<objList.options.length; i++ )
-                {
-                    strCurItemValue += objList.options[i].value + ",";
-                }
-                document.{$this->form_id}.{$this->pager_name}_New_Columns.value = strCurItemValue;
-
+            function {$this->pager_name}_save_view(obj) {
+                document.{$this->form_id}.{$this->pager_name}_pager_columns_action.value = 'save';
                 document.{$this->form_id}.submit();
             }
-
-            function {$this->pager_name}_reset_default(obj) {
-                deleteAllOption(document.getElementById('{$this->pager_name}_displayColumns'));
-                deleteAllOption(document.getElementById('{$this->pager_name}_availColumns'));
-
-                $reset_columns_rows;
-
-                {$this->pager_name}_columns_change(obj);
+            function {$this->pager_name}_load_view(obj) {
+                document.{$this->form_id}.{$this->pager_name}_pager_columns_action.value = 'load';
+                document.{$this->form_id}.submit();
+            }
+            function {$this->pager_name}_delete_view(obj) {
+                document.{$this->form_id}.{$this->pager_name}_pager_columns_action.value = 'delete';
+                document.{$this->form_id}.submit();
             }
         </script>
 
@@ -195,76 +277,32 @@ class Pager_Columns {
         <!-- the hidden div -->
         <div class="PagerSelectableColumns" id="{$this->pager_name}_widget">
         <a name="{$this->pager_name}_select_columns"></a>
-        <table class=widget cellspacing=1>
-            <tr>
-                <td class="widget_header" colspan=5>
-                    Select Columns
-                </td>
-            </tr>
-            <tr>
-                <td class="widget_content" colspan=2 align="center">
-                    <b>Displayed Columns</b>
-                </td>
-                <td class="widget_content" colspan=2 align="center">
-                    <b>Available Columns</b>
-                </td>
-                <td class="widget_content" align="center">
-                    <b>Layouts</b>
-                </td>
-            </tr>
-            <tr align="center">
-
-                <td class="widget_content">
-                    <input type="button" class="button" onClick="shiftListItem('up', '{$this->pager_name}_displayColumns')" value="/\"><br/><input type="button" class="button" onClick="shiftListItem('dn', '{$this->pager_name}_displayColumns')" value="\/">
-                </td>
-                <td class="widget_content">
-                    <select name="{$this->pager_name}_displayColumns"
-                            id="{$this->pager_name}_displayColumns"
-			    size="{$this->visible_column_size}"
-                            class="myList"
-                            onFocus="objFocusList = this"
-                            onChange="setDisplay(this)">
-            $display_columns_options
-            </select>
-<input type="button" class="button" onClick="moveListItemAll( '{$this->pager_name}_displayColumns', '{$this->pager_name}_availColumns' )" value="$moveall ->">
-
-        </td>
-
-        <td class="widget_content">
-            <input type="button" class="button" onClick="moveListItem( '{$this->pager_name}_availColumns', '{$this->pager_name}_displayColumns' )" value="<- $moveone"><br/><input type="button" class="button" onClick="moveListItem( '{$this->pager_name}_displayColumns', '{$this->pager_name}_availColumns' )" value="$moveone ->"><br/>        </td>
-
-        <td class="widget_content">
-            <select name="{$this->pager_name}_availColumns"
-                    id="{$this->pager_name}_availColumns"
-		    size="{$this->visible_column_size}"
-                    class="myList"
-                    onFocus="objFocusList = this"
-                    onChange="setDisplay(this)">
-            $avail_columns_options
-            </select>
-<input type="button" class="button" onClick="moveListItemAll( '{$this->pager_name}_availColumns', '{$this->pager_name}_displayColumns' )" value="<- $moveall">
-        </td>
-
-        <td class="widget_content_center">
-
-            <input type="button" class="button" name="button" onclick="{$this->pager_name}_columns_change(this)" value="$text_update_columns"><br/>
-            <input type="button" class="button" name="button" onclick="{$this->pager_name}_reset_default(this)" value="$text_reset_columns"><br/>
-            <input type="button" class="button" name="button" onclick="document.getElementById('{$widget_name}').style.display = 'none'" value="$text_cancel_columns"><br/>
-        </td>
-    </tr>
-    </table>
-    </div>
+{hidden}
+<table border=\"0\">\n{content}\n</table>\n
+        </div> <!-- PagerSelectableColumns -->
 
 
 <script language="JavaScript" type="text/javascript">
     document.getElementById('{$widget_name}').style.display = 'none';
 </script>
 END;
+
+        $renderer->setFormTemplate($form_template);
+        //$renderer->setFormTemplate("\n\n<div>\n{hidden}<table border=\"0\">\n{content}\n</table>\n</div>\n");
+
+        $form->accept($renderer);
+
+        $s .= $renderer->toHtml();
+
         return $s;
     }
+
 }
 /**
  * $Log: Pager_Columns.php,v $
+ * Revision 1.17  2006/02/01 21:41:00  daturaarutad
+ * big update to use HTML_QuickForm_advmultiselect class for widget and allow saveable views
+ *
  * Revision 1.16  2006/01/28 13:07:16  vanmer
  * - added parameter to control the number of rows in each selectable column box
  *
