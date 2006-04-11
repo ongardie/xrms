a7679024ec672e98d8506fe7e6a21fb556d8b26a
vanmer <vanmer>
2006-04-11 05:18:12 +0000

    - updated version in system tables to 1.99

--- admin/updateto2.0.php
+++ admin/updateto2.0.php
@@ -9,7 +9,7 @@
  * @author Beth Macknik
  * @author XRMS Development Team
  *
- * $Id: updateto2.0.php,v 1.10 2006/03/17 00:14:34 vanmer Exp $
+ * $Id: updateto2.0.php,v 1.11 2006/04/11 05:18:12 vanmer Exp $
  */
 
 // where do we include from
@@ -4756,7 +4756,7 @@ $msg .= update_unknown_company($con);
 
 
 //FINAL STEP BEFORE WE ARE AT 2.0.0, SET XRMS VERSION TO 2.0.0 IN PREFERENCES TABLE
-//set_admin_preference($con, 'xrms_version', '2.0.0');
+set_admin_preference($con, 'xrms_version', '1.99');
 
 do_hook_function('xrms_update', $con);
 
@@ -4782,6 +4782,9 @@ end_page();
 
 /**
  * $Log: updateto2.0.php,v $
+ * Revision 1.11  2006/04/11 05:18:12  vanmer
+ * - updated version in system tables to 1.99
+ *
  * Revision 1.10  2006/03/17 00:14:34  vanmer
  * - added preference type option to hide preference from system preferences menu
  *
