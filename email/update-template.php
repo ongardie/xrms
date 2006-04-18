bb256292532acf614e0133fc3bdb3b9acacf4c0c
braverock <braverock>
2006-04-18 15:36:48 +0000

    - localize Template for i18n - add phpdoc

--- email/update-template.php
+++ email/update-template.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * ave Updated Email Template to the database
+ *
+ * $Id: update-template.php,v 1.6 2006/04/18 15:36:48 braverock Exp $
+ */
 require_once('include-locations-location.inc');
 
 require_once($include_directory . 'vars.php');
@@ -12,7 +16,7 @@ $session_user_id = session_check();
 $msg = $_GET['msg'];
 
 $email_template_id = $_POST['email_template_id'];
-$email_template_title = (strlen($_POST['email_template_title']) > 0) ? $_POST['email_template_title'] : "Template $email_template_id";
+$email_template_title = (strlen($_POST['email_template_title']) > 0) ? $_POST['email_template_title'] : _("Template").'&nbsp;'.$email_template_id;
 $email_template_body = $_POST['email_template_body'];
 
 $con = get_xrms_dbconnection();
@@ -31,4 +35,11 @@ $con->close();
 
 header("Location: email-2.php?email_template_id=$email_template_id&msg=saved");
 
+/**
+ * $Log: update-template.php,v $
+ * Revision 1.6  2006/04/18 15:36:48  braverock
+ * - localize Template for i18n
+ * - add phpdoc
+ *
+ */
 ?>
