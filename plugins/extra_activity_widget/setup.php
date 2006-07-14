803470c728ac6b5d31df86a52a0a0d2deb286ea8
vanmer <vanmer>
2006-07-14 03:52:28 +0000

    - Initial revision of the example plugin for an activities widget on the companies/one.php page

---
+++ plugins/extra_activity_widget/setup.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Example plugin to demonstrate adding extra activities widgets to pages
+ *
+ * This example adds an activities widget to the companies/one.php page, directly after the existing activities widget (and in fact using the same form)
+ * 
+ * $Id: setup.php,v 1.1 2006/07/14 03:52:28 vanmer Exp $
+**/
+
+require_once($include_directory.'../activities/activities-widget.php');
+
+/**
+  * Plugin Initialization function.  Intended to register all needed hooks into XRMS
+  *
+**/
+function xrms_plugin_init_extra_activity_widget() {
+    global $xrms_plugin_hooks;
+    $xrms_plugin_hooks['company_activities']['extra_activity_widget']='extra_activity_widget_company';
+}
+
+
+/**
+ * Plugin hook function to display activities widget
+**/
+function extra_activity_widget_company(&$string) {
+    global $session_user_id;
+    global $return_url;
+    global $company_id;
+    global $division_id;
+    global $activities_form_name;
+    global $con;
+
+    //set up which columns to display as system default
+    $default_columns = array('title', 'owner', 'type', 'contact', 'activity_about', 'scheduled', 'due');
+
+    //set search terms.  This example shows all closed activities for a company and division
+    //minisearch terms are added to this to create the criteria in the SQL
+    $search_terms = array( 'company_id'            => $company_id,
+                       'division_id'           => $division_id,
+                       'activity_status' => "'c'"
+
+                        );
+    //enable the mini search filter for activities
+    $show_mini_search=true;
+
+    //no extra where clauses for the SQL statement
+    $extra_where='';
+
+    //no end rows to add to the widget
+    $end_rows='';
+
+    //don't set default sort
+    $default_sort=null;
+    
+    //this must be unique to allow proper saved search and form operations
+    $instance='Plugin';
+
+    $caption=_("Closed Activities");
+
+    //retrieve activities widget
+    $activities_widget =  GetActivitiesWidget($con, $search_terms, $activities_form_name, $caption, $session_user_id, $return_url, $extra_where, $end_rows, $default_columns, $show_mini_search, $default_sort, $instance);
+
+    //assign output to string
+    $string.= $activities_widget['content'];
+    $string.= $activities_widget['sidebar'];
+    $string.= $activities_widget['js'];
+
+}
+
+/**
+ * $Log: setup.php,v $
+ * Revision 1.1  2006/07/14 03:52:28  vanmer
+ * - Initial revision of the example plugin for an activities widget on the companies/one.php page
+ *
+**/
+
+?>
