c67abded7bc852bb1f865889c0299b18e60e443a
niclowe <niclowe>
2006-11-29 20:09:38 +0000

    initial upload of script

---
+++ include/tinymce/jscripts/tiny_mce/plugins/cleanup/editor_plugin_src.js
@@ -0,0 +1,10 @@
+/**
+ * $Id: editor_plugin_src.js,v 1.1 2006/11/29 20:09:45 niclowe Exp $
+ *
+ * Experimental plugin for new Cleanup routine, this logic will be moved into the core ones it's stable enougth.
+ *
+ * @author Moxiecode
+ * @copyright Copyright © 2004-2006, Moxiecode Systems AB, All rights reserved.
+ */
+
+/* Dummy file since cleanup is now moved to core */
