4474ecda1d5f30e13846c66238f3b108a34634fa
gpowers <gpowers>
2007-12-10 21:30:54 +0000

    - basic sidebar box which lists available RSS feeds

---
+++ rss/sidebar.php
@@ -0,0 +1,37 @@
+       <table class=widget cellspacing=1 width="100%">
+            <tr>
+                <td class=widget_header colspan=4>
+                    <?php echo _("RSS Feeds"); ?>
+                  
+                </td>
+            </tr>
+          <tr>
+                <td class=widget_content>
+                    <a href="../rss/activities.php"><?php echo _("Activities"); ?></a>
+                </td>
+            </tr>
+            <tr>
+                <td class=widget_content>
+                    <a href="../rss/companies.php"><?php echo _("Companies"); ?></a>
+                </td>
+            </tr>
+            <tr>
+                <td class=widget_content>
+                    <a href="../rss/contacts.php"><?php echo _("Contacts"); ?></a>
+                </td>
+            </tr>
+                        <tr>
+                <td class=widget_content>
+                    <a href="../rss/Opportunities.php"><?php echo _("Opportunities"); ?></a>
+                </td>
+            </tr>
+            <tr>
+                <td class=widget_content>
+                    <a href="../rss/cases.php"><?php echo _("Cases"); ?></a>
+                </td>
+            </tr>
+        </table>
+
+        
+        
+        
