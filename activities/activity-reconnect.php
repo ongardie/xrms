e6cc018ea3cd5da92078e7da3ab52819be400757
gpowers <gpowers>
2008-01-30 21:10:06 +0000

    - added functionality to transfer activities to other entities. worksforme

--- activities/activity-reconnect.php
+++ activities/activity-reconnect.php
@@ -20,6 +20,7 @@ $session_user_id = session_check();
 GetGlobalVar($return_url,'return_url');
 GetGlobalVar($msg, 'msg');
 GetGlobalVar($activity_id,'activity_id');
+GetGlobalVar($on_what_entity,'on_what_entity');
 getGlobalVar($on_what_table, 'on_what_table');
 GetGlobalVar($on_what_id, 'on_what_id');
 getGlobalVar($restrict_string, 'restrict_string');
@@ -28,40 +29,83 @@ getGlobalVar($reconnect_action,'reconnect_action');
 
 
 $con = get_xrms_dbconnection();
+$con->debug=0;
 
-$possible_types=array( 'companies' => _("Companies"),
+if (!$on_what_entity) {
+    $possible_types=array( 'companies' => _("Companies"),
                                             'opportunities' => _("Opportunities"),
                                             'contacts'=>_("Contacts"),
                                             'cases' => _("Cases"),
-                                            'campaigns' => _("Campaigns"),
-                                            'company_division' => _("Divisions")
+                                            'campaigns' => _("Campaigns")//,
+                       //                     'company_division' => _("Divisions")
                                            );
-
         asort($possible_types);
+        $on_what_table_select = create_select_from_array($possible_types, 'on_what_table', $on_what_table, '', true);
+}
 
-        $search_button=_("Search");
-        $search_title=_("Search Entities");
-        $for_label=_("Search For");
-        $restrict_label=_("Containing");
-        $restrict_label1=_("Company");
+$search_button=_("Search");
+$search_title=_("Search Entities");
+$for_label=_("Search For");
+$restrict_label=_("Containing");
+$restrict_label1=_("Limit to Company");
 
-        $on_what_table_select= create_select_from_array($possible_types, 'on_what_table', $on_what_table, '', true);
+                    if ($on_what_entity == 'company') {
+                        $body_entity .= "Company";
+                        $on_what_table = 'companies';
+                        // $reconnect_action = 'showSelectEntity';
+                    } elseif ($on_what_entity == 'contact') {
+                        $body_entity .= "Contact";
+                        $on_what_table = 'contacts';
+                        // $reconnect_action = 'showSelectEntity';
+                    } else {
+                        $body_entity .= $on_what_table_select;
+                    }
 
+$body_content = "
+        <form action=\"activity-reconnect.php\" name=searchNewEntity method=POST>
+            <input type=hidden name=reconnect_action value=showSelectEntity>
+            <input type=hidden name=activity_id value=\"$activity_id\">
+            <input type=hidden name=on_what_entity value=\"$on_what_entity\">
+            <input type=hidden name=on_what_table value=\"$on_what_table\">
+            <input type=hidden name=on_what_id value=\"$on_what_id\">
 
-
-        $body_content=<<<TILLEND
-            <form action="activity-reconnect.php" name=searchNewEntity method=POST>
-            <table class=widget cellspacing=1><tr><td class=widget_header colspan=2>$search_title</tr><tr>
-                <tr><td class=widget_label>$for_label</td><td class=widget_content_form_element>$on_what_table_select</td></tr>
-                <tr><td class=widget_label>$restrict_label</td><td class=widget_content_form_element><input type=text name=restrict_string value=$restrict_string></td></tr>
-                <tr><td class=widget_label>$restrict_label1</td><td class=widget_content_form_element><input type=text name=restrict_string1 value=$restrict_string1></td></tr>
-
-                <tr><td class=widget_content_form_element colspan=2><input type=submit class=button name=btSearch value="$search_button">
+            <table class=widget cellspacing=1>
+                <tr>
+                    <td class=widget_header colspan=2>
+                        $search_title
+                    </td>
+                </tr>
+                <tr>
+                    <td class=widget_label>
+                        $for_label
+                    </td>
+                    <td class=widget_content_form_element>         
+                        $body_entity
+                    </td>
+                </tr>
+                <tr>
+                    <td class=widget_label>
+                        $restrict_label
+                    </td>
+                    <td class=widget_content_form_element>
+                        <input type=text name=restrict_string value=$restrict_string>
+                    </td>
+                </tr>
+                <tr>
+                    <td class=widget_label>
+                        $restrict_label1
+                    </td>
+                    <td class=widget_content_form_element>
+                        <input type=text name=restrict_string1 value=$restrict_string1>
+                    </td>
+                </tr>
+                <tr>
+                    <td class=widget_content_form_element colspan=2>
+                        <input type=submit class=button name=btSearch value=\"$search_button\">
+                    </td>
+                </tr>
             </table>
-TILLEND;
-                $body_content.="<input type=hidden name=reconnect_action value=showSelectEntity>";
-                $body_content.="<input type=hidden name=activity_id value=$activity_id>";
-                $body_content.="</form>";
+</form>";
 
 switch ($reconnect_action) {
     case 'showTypeSearch':
@@ -73,7 +117,7 @@ switch ($reconnect_action) {
         require_once ($include_directory . 'classes/Pager/GUP_Pager.php');
         if (!$on_what_table) {
             $msg=urlencode(_("Please select an entity type to search for"));
-            Header("Location: activity-reconnect.php?activity_id=$activity_id&reconnect_action=showTypeSearch&msg=$msg");
+            Header("Location: activity-reconnect.php?activity_id=$activity_id&reconnect_action=showTypeSearch&msg=$msg&on_what_entity=$on_what_entity");
             exit();
         }
         $page_title=_("Select Entity");
@@ -101,20 +145,32 @@ switch ($reconnect_action) {
         $entity_pager = $pager->Render($system_rows_per_page);
         $body_content .=<<<TILLEND
         <p><form action="activity-reconnect.php" name=EntityPagerForm method=POST>
-            <input type=hidden name=reconnect_action value=showSelectEntity>
+            <input type=hidden name=reconnect_action value="reconnectActivity">
             <input type=hidden name=on_what_table value="$on_what_table">
             <input type=hidden name=restrict_string value="$restrict_string">
             <input type=hidden name=restrict_string1 value="$restrict_string1">
-            <input type=hidden name=activity_id value=$activity_id>
+            <input type=hidden name=activity_id value="$activity_id">
+            <input type=hidden name=on_what_entity value="$on_what_entity">
             $entity_pager
         </form>
 TILLEND;
     break;
     case 'reconnectActivity':
-        $rec=array();
-        if ($on_what_table AND $on_what_id) {
+    $rec=array();
+        if ($on_what_entity == 'company' ) {
+            $rec['company_id']=$on_what_id;            
+        } elseif ($on_what_entity == 'contact') {
+            $rec['contact_id']=$on_what_id;
+        } elseif ($on_what_table AND $on_what_id) {
             $rec['on_what_table']=$on_what_table;
-            $rec['on_what_id']=$on_what_id;
+            $rec['on_what_id']=$on_what_id;            
+        } else {
+            $msg=urlencode(_("Please select an entity"));
+            $restrict_string=urlencode($restrict_string);
+            $return_url="activity-reconnect.php?reconnect_action=showSelectEntity&restrict_string=$restrict_string&on_what_table=$on_what_table&activity_id=$activity_id&on_what_entity=$on_what_entity&msg=$msg";
+            header("Location: $return_url");
+            exit;
+        }
             $ret=update_activity($con, $rec, $activity_id);
             if ($ret) {
                 $msg=urlencode(_("Successfully re-attached activity"));
@@ -123,12 +179,7 @@ TILLEND;
                 $msg=urlencode(_("Failed to update activity"));
                 $return_url="$http_site_root/activities/one.php?activity_id=$activity_id&msg=$msg";
             }
-        } else {
-            $msg=urlencode(_("Please select an entity"));
-            $restrict_string=urlencode($restrict_string);
-            $return_url="activity-reconnect.php?reconnect_action=showSelectEntity&restrict_string=$restrict_string&on_what_table=$on_what_table&activity_id=$activity_id&msg=$msg";
-        }
-        Header("Location: $return_url");
+        header("Location: $return_url");
         exit;
     break;
 }
@@ -156,6 +207,9 @@ function GetEntityPagerData($row) {
 
 /**
  * $Log: activity-reconnect.php,v $
+ * Revision 1.5  2008/01/30 21:10:06  gpowers
+ * - added functionality to transfer activities to other entities. worksforme
+ *
  * Revision 1.4  2006/09/30 18:12:09  braverock
  * - patch 2006/07/31 from  dbaudone
  *  -- added code to display company name along with contact name
