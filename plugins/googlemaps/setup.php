8c359c219bdbba91251b0a4bf085d62f1e163193
polyformal_sp <polyformal_sp>
2008-10-07 08:57:24 +0000

    updated to recent version, fix sql statements to on_what_* layout

--- plugins/googlemaps/setup.php
+++ plugins/googlemaps/setup.php
@@ -6,37 +6,70 @@
 * polyformal ( http://www.polyformal.de/ )
 * (c) 2007 (GNU GPL - see ../../COPYING)
 * 
-* $Id: setup.php,v 1.2 2008/08/29 14:51:45 polyformal_sp Exp $
+* $Id: setup.php,v 1.3 2008/10/07 08:57:24 polyformal_sp Exp $
 *
 * This plugin allows show the location of the selected company in the sidebar
 * within google maps.
 * 
 * There could be some improvements like:
 * - show map on request (click) only
-* - calculate a route, based an the company_id/address_id of the current user
+* - calculate a route, based an the company_id/address_id of the current user DONE
+* - need modified companies/one.php because of trouble with company init 
 */
 
 
 function xrms_plugin_init_googlemaps () {
-    global $xrms_plugin_hooks;
-    $xrms_plugin_hooks['company_sidebar_bottom']['googlemaps'] = 'googlemaps';
-    }
+    global $xrms_plugin_hooks; $division_id;
+   
+    $xrms_plugin_hooks['division_sidebar_bottom']['googlemaps'] = 'googlemaps_sidebar_division';
+    $xrms_plugin_hooks['company_sidebar_bottom']['googlemaps'] = 'googlemaps_sidebar_company';
+}
     
-function googlemaps () {
-    global $con, $contact_id, $sidebar, $company_id, $company_name, $my_company_id;
-	$google_maps_apikey ="";
-    $sql = "select * from addresses inner join countries on addresses.country_id=countries.country_id where company_id=". $company_id;
-    $rst = $con->execute($sql);
-    if ($rst) {
-        if (!$rst->EOF) {
-            $address_line1 = $rst->fields['line1'];
-			$address_line2 = $rst->fields['line2'];
-			$address_postal_code = $rst->fields['postal_code'];
-			$address_city = $rst->fields['city'];
-			$address_country = $rst->fields['country_name'];
-			$address=$address_line1 . ", " . $address_postal_code . ", " . $address_city . ", " .$address_country ;
-			$html_address=urlencode($address);
-			$sidebar_string = '<div id="idphoto_sidebar">
+function googlemaps_sidebar_company () {
+  return googlemaps_sidebar_rows_bottom();
+}
+
+function googlemaps_sidebar_division () {
+  return googlemaps_sidebar_rows_bottom();
+}
+function googlemaps_sidebar_rows_bottom () {
+  global $contact_id, $sidebar_rows_bottom, $company_id, $user_id, $division_id, $company_name, $my_company_id, $division_sidebar_bottom;
+  // use logged in useres address instead account owner
+  $session_user_id = session_check();
+
+	// you'll need your own api_key
+  $google_maps_apikey ="ABQIAAAA_XPEGP-AFVnM2VL6tVt25hT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQPjktfc-0qMosjSbFfqeK1xiJYoQ";
+
+	// query the useres address, needed to displsy the 'Route'-Link
+	$sql="select * from addresses 
+		inner join contacts on contacts.address_id = addresses.address_id
+		inner join countries on addresses.country_id=countries.country_id 
+		inner join users on users.user_contact_id = contacts.contact_id
+		where users.user_id ={$session_user_id}";
+
+	$user_address = get_gm_address($sql);
+	$user_html_address = urlencode($user_address);
+	
+	// query the address, use diffferent query and filter if we see an division
+	if ($division_id) {
+	  $sql_filter = 'inner join company_division on addresses.address_id=company_division.address_id';
+	  $sql_crit = ' and company_division.division_id='.$division_id;
+	}
+	 else {
+	   $sql_filter='inner join companies on addresses.address_id=companies.default_primary_address';
+	   $sql_crit='';
+	 }
+	$sql = "select * from addresses 
+		inner join countries on addresses.country_id=countries.country_id 
+		".$sql_filter."
+		where addresses.on_what_table='companies' and addresses.on_what_id=". $company_id.$sql_crit;
+    
+	$address = get_gm_address($sql);
+  #if no address is given, then don't start the whole process.
+  if ($address !=', , , ') {
+	$html_address=urlencode($address);
+	// assembling the html stuff
+			$sidebar_string = '<div id="googlemaps_sidebar">
 			<table class=widget cellspacing=1 width="100%">
             <tr>
                 <td class=widget_header colspan=4>'
@@ -115,7 +148,7 @@ if (window.attachEvent) {
 // END NEW CODE TO AVOID USING BODY TAG FOR LOAD/UNLOAD
 
     </script>
-     <a target="_blank" href="http://maps.google.de/maps?daddr='. $html_address .'&geocode=&saddr=kaesenstra%C3%9Fe+k%C3%B6ln&f=d&hl=de&sll=50.796386,7.206228&sspn=0.009725,0.020084&ie=UTF8&z=12&om=1">Link hierher</a>
+     <a target="_blank" href="http://maps.google.de/maps?daddr='. $html_address .'&geocode=&saddr='.$user_html_address.'&f=d&hl=de&ie=UTF8&z=12&om=1">Show Route</a>
       <div id="map" style="width: 100%; height: 300px"></div>
     </form>          
 
@@ -123,13 +156,32 @@ if (window.attachEvent) {
 </tr>
 </table>
 ';
-        }
+    // return the result
+   return $sidebar_rows_bottom.=$sidebar_string.$sql;
+      }//EOF if
+}
+
+function get_gm_address($sql) {
+    global $con;
+    $rst = $con->execute($sql);
+    if ($rst) {
+        if (!$rst->EOF) {
+            $address_line1 = $rst->fields['line1'];
+			$address_line2 = $rst->fields['line2'];
+			$address_postal_code = $rst->fields['postal_code'];
+			$address_city = $rst->fields['city'];
+			$address_country = $rst->fields['country_name'];
+			$address=$address_line1 . ", " . $address_postal_code . ", " . $address_city . ", " .$address_country ;
+                }
             $rst->close();
     }
-    return $sidebar.=$sidebar_string;
+    return $address;
 }
 /*
  * $Log: setup.php,v $
+ * Revision 1.3  2008/10/07 08:57:24  polyformal_sp
+ * updated to recent version, fix sql statements to on_what_* layout
+ *
  * Revision 1.2  2008/08/29 14:51:45  polyformal_sp
  * midify xing to googlemaps in comments (mistype)
  *
