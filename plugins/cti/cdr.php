723599a9b1f47c30f37028a2928cb83d05a67825
gopherit <gopherit>
2009-04-14 07:09:30 +0000

    Fixed missing </div> around line 318.

--- plugins/cti/cdr.php
+++ plugins/cti/cdr.php
@@ -4,7 +4,7 @@
  *
  * Search for and View a list of calls
  *
- * $Id: cdr.php,v 1.3 2009/04/09 06:30:39 gopherit Exp $
+ * $Id: cdr.php,v 1.4 2009/04/14 07:09:30 gopherit Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -163,160 +163,159 @@ arr_vars_session_set ( $arr_vars );
                 </tr>
             </table>
         </form>
-<?php
+        <?php
 
-add_audit_item($con, $session_user_id, 'viewed', 'cti_cdr', $account_id, 4);
+        add_audit_item($con, $session_user_id, 'viewed', 'cti_cdr', $account_id, 4);
 
-$sql = "SELECT * FROM cdr";
+        $sql = "SELECT * FROM cdr";
 
-$where = array();
+        $where = array();
 
-if ($uniqueid) {
-    $criteria_count++;
-    $where .= "uniqueid = '" . $uniqueid . "' ";
-}
-if ($date_start) {
-    $criteria_count++;
-    $where .= "UNIX_TIMESTAMP(calldate) >= $date_start";
-}
-if ($date_end) {
-    $criteria_count++;
-    $where .= "UNIX_TIMESTAMP(calldate) <= $date_end";
-}
+        if ($uniqueid) {
+            $criteria_count++;
+            $where .= "uniqueid = '" . $uniqueid . "' ";
+        }
+        if ($date_start) {
+            $criteria_count++;
+            $where .= "UNIX_TIMESTAMP(calldate) >= $date_start";
+        }
+        if ($date_end) {
+            $criteria_count++;
+            $where .= "UNIX_TIMESTAMP(calldate) <= $date_end";
+        }
 
-if ($userfield) {
-    $criteria_count++;
-    $where .= "userfield = '$userfield'";
-}
+        if ($userfield) {
+            $criteria_count++;
+            $where .= "userfield = '$userfield'";
+        }
 
-if ($accountcode) {
-    $criteria_count++;
-    $where .= "accountcode ='$accountcode'";
-}
+        if ($accountcode) {
+            $criteria_count++;
+            $where .= "accountcode ='$accountcode'";
+        }
 
-if ($src) {
-    $criteria_count++;
-    $where .= "src LIKE '%" . $src . "%'";
-}
+        if ($src) {
+            $criteria_count++;
+            $where .= "src LIKE '%" . $src . "%'";
+        }
 
-if ($dst) {
-    $criteria_count++;
-    $where .= "dst LIKE '%" . $dst . "%'";
-}
+        if ($dst) {
+            $criteria_count++;
+            $where .= "dst LIKE '%" . $dst . "%'";
+        }
 
-if ($dcontext) {
-    $criteria_count++;
-    $where .= "dcontext = '$dcontext'";
-}
+        if ($dcontext) {
+            $criteria_count++;
+            $where .= "dcontext = '$dcontext'";
+        }
 
-if ($clid) {
-    $criteria_count++;
-    $where .= "clid LIKE '%" . $clid . "%'";
-}
+        if ($clid) {
+            $criteria_count++;
+            $where .= "clid LIKE '%" . $clid . "%'";
+        }
 
-if ($channel) {
-    $criteria_count++;
-    $where .= "channel = '$channel'";
-}
+        if ($channel) {
+            $criteria_count++;
+            $where .= "channel = '$channel'";
+        }
 
-if ($dstchannel) {
-    $criteria_count++;
-    $where .= "dstchannel = '$dstchannel'";
-}
+        if ($dstchannel) {
+            $criteria_count++;
+            $where .= "dstchannel = '$dstchannel'";
+        }
 
-if ($lastapp) {
-    $criteria_count++;
-    $where .= "lastapp = '$lastapp'";
-}
+        if ($lastapp) {
+            $criteria_count++;
+            $where .= "lastapp = '$lastapp'";
+        }
 
-if ($lastdata) {
-    $criteria_count++;
-    $where .= "lastdata = '$lastdata'";
-}
+        if ($lastdata) {
+            $criteria_count++;
+            $where .= "lastdata = '$lastdata'";
+        }
 
-if ($duration) {
-    $criteria_count++;
-    $where .= "duration = '$duration'";
-}
+        if ($duration) {
+            $criteria_count++;
+            $where .= "duration = '$duration'";
+        }
 
-if ($billsec) {
-    $criteria_count++;
-    $where .= "billsec = '$billsec'";
-}
+        if ($billsec) {
+            $criteria_count++;
+            $where .= "billsec = '$billsec'";
+        }
 
-if ($disposition) {
-    $criteria_count++;
-    $where .= "disposition = '$disposition'";
-}
+        if ($disposition) {
+            $criteria_count++;
+            $where .= "disposition = '$disposition'";
+        }
 
-if ($amaflags) {
-    $criteria_count++;
-    $where .= "amaflags = '$amaflags'";
-}
+        if ($amaflags) {
+            $criteria_count++;
+            $where .= "amaflags = '$amaflags'";
+        }
 
-if ($criteria_count) {
-    $sql .= "WHERE " . $where;
-}
-
-
-$columns = array();
-$columns[] = array('name' => _('Record #'), 'index_sql' => 'uniqueid');
-$columns[] = array('name' => _('User Field'), 'index_sql' => 'userfield');
-$columns[] = array('name' => _('Account Code'), 'index_sql' => 'accountcode');
-$columns[] = array('name' => _('Src'), 'index_sql' => 'src');
-$columns[] = array('name' => _('Dest'), 'index_sql' => 'dst');
-$columns[] = array('name' => _('Dest Context'), 'index_sql' => 'dcontext');
-$columns[] = array('name' => _('Caller ID'), 'index_sql' => 'clid');
-$columns[] = array('name' => _('Src Channel'), 'index_sql' => 'channel');
-$columns[] = array('name' => _('Dest Channel'), 'index_sql' => 'dstchannel');
-$columns[] = array('name' => _('Last App'), 'index_sql' => 'lastapp');
-$columns[] = array('name' => _('Last Data'), 'index_sql' => 'lastdata');
-$columns[] = array('name' => _('Call Date'), 'index_sql' => 'calldate');
-$columns[] = array('name' => _('Duration'), 'index_sql' => 'duration');
-$columns[] = array('name' => _('Seconds Billed'), 'index_sql' => 'billsec');
-$columns[] = array('name' => _('Duration'), 'index_sql' => 'duration');
-$columns[] = array('name' => _('AMA Flags'), 'index_sql' => 'amaflags');
+        if ($criteria_count) {
+            $sql .= "WHERE " . $where;
+        }
 
 
-// selects the columns this user is interested in
-$default_columns =  array();
-$default_columns[] = 'calldate';
-$default_columns[] = 'clid';
-$default_columns[] = 'src';
-$default_columns[] = 'dst';
-$default_columns[] = 'duration';
+        $columns = array();
+        $columns[] = array('name' => _('Record #'), 'index_sql' => 'uniqueid');
+        $columns[] = array('name' => _('User Field'), 'index_sql' => 'userfield');
+        $columns[] = array('name' => _('Account Code'), 'index_sql' => 'accountcode');
+        $columns[] = array('name' => _('Src'), 'index_sql' => 'src');
+        $columns[] = array('name' => _('Dest'), 'index_sql' => 'dst');
+        $columns[] = array('name' => _('Dest Context'), 'index_sql' => 'dcontext');
+        $columns[] = array('name' => _('Caller ID'), 'index_sql' => 'clid');
+        $columns[] = array('name' => _('Src Channel'), 'index_sql' => 'channel');
+        $columns[] = array('name' => _('Dest Channel'), 'index_sql' => 'dstchannel');
+        $columns[] = array('name' => _('Last App'), 'index_sql' => 'lastapp');
+        $columns[] = array('name' => _('Last Data'), 'index_sql' => 'lastdata');
+        $columns[] = array('name' => _('Call Date'), 'index_sql' => 'calldate');
+        $columns[] = array('name' => _('Duration'), 'index_sql' => 'duration');
+        $columns[] = array('name' => _('Seconds Billed'), 'index_sql' => 'billsec');
+        $columns[] = array('name' => _('Duration'), 'index_sql' => 'duration');
+        $columns[] = array('name' => _('AMA Flags'), 'index_sql' => 'amaflags');
 
-$pager_columns = new Pager_Columns('CDRPager', $columns, $default_columns, 'CDRPager');
-$pager_columns_button = $pager_columns->GetSelectableColumnsButton();
-$pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
 
-$columns = $pager_columns->GetUserColumns('default');
+        // selects the columns this user is interested in
+        $default_columns =  array();
+        $default_columns[] = 'calldate';
+        $default_columns[] = 'clid';
+        $default_columns[] = 'src';
+        $default_columns[] = 'dst';
+        $default_columns[] = 'duration';
 
-$disp_lbl_export = _('Export');
+        $pager_columns = new Pager_Columns('CDRPager', $columns, $default_columns, 'CDRPager');
+        $pager_columns_button = $pager_columns->GetSelectableColumnsButton();
+        $pager_columns_selects = $pager_columns->GetSelectableColumnsWidget();
 
-// caching is disabled for this pager (since it's all sql)
-$pager = new GUP_Pager($con, $sql, null, _('Call Detail Records'), 'CDRPager', 'CDRPager', $columns, false);
+        $columns = $pager_columns->GetUserColumns('default');
 
-//Attempting to start moving towards having all the html at the bottom
-    ?>
+        $disp_lbl_export = _('Export');
+
+        // caching is disabled for this pager (since it's all sql)
+        $pager = new GUP_Pager($con, $sql, null, _('Call Detail Records'), 'CDRPager', 'CDRPager', $columns, false);
+
+        //Attempting to start moving towards having all the html at the bottom
+        ?>
 
         <form action=cdr.php class=print method=post name="CDRPager">
 	        <input type=hidden name=use_post_vars value=1>
-    <?php  echo $pager_columns_selects; ?>
-    <?php
-    $pager->AddEndRows("
-        <tr>
-            <td class=widget_content_form_element colspan=10>
-                $pager_columns_button
-                <input type=button class=button onclick=\"javascript: exportIt();\" value=\"$disp_lbl_export\">
-            </td>
-        </tr>");
-    $pager->Render($system_rows_per_page);
-    echo "  </form>";
-
-?>
-    
+            <?php  echo $pager_columns_selects; ?>
+            <?php
+            $pager->AddEndRows("
+                <tr>
+                    <td class=widget_content_form_element colspan=10>
+                        $pager_columns_button
+                        <input type=button class=button onclick=\"javascript: exportIt();\" value=\"$disp_lbl_export\">
+                    </td>
+                </tr>");
+            $pager->Render($system_rows_per_page);
+            ?>
+        </form>
     </div>
+</div>
 
 <script language="JavaScript" type="text/javascript">
 
@@ -370,6 +369,9 @@ end_page();
 
 /**
  * $Log: cdr.php,v $
+ * Revision 1.4  2009/04/14 07:09:30  gopherit
+ * Fixed missing </div> around line 318.
+ *
  * Revision 1.3  2009/04/09 06:30:39  gopherit
  * Changed CSS "CURSOR: hand" to "CURSOR: pointer".  "hand" is largely unsupported outside of IE and "pointer" is universally supported in all browsers except <= IE 5.5.
  *
