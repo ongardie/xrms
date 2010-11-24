130e1288970a6efe25f9d25db67927d5b4cacb58
gopherit <gopherit>
2010-11-24 22:39:37 +0000

    Revised the store script for creating Templates attached to an Opportunity: - provided support for the new start_delay field which allows workflow activities to have gaps between them, measured in seconds by start_delay - finished the fixed_date functionality which lay dormant in the code base until now

--- admin/activity-templates/new.php
+++ admin/activity-templates/new.php
@@ -4,7 +4,7 @@
  *
  * @author Brad Marshall
  *
- * $Id: new.php,v 1.10 2008/02/27 01:44:57 randym56 Exp $
+ * $Id: new.php,v 1.11 2010/11/24 22:39:37 gopherit Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -16,55 +16,66 @@ require_once($include_directory . 'adodb-params.php');
 
 $session_user_id = session_check( 'Admin' );
 
-$activity_title = $_POST['title'];
-$duration = $_POST['duration'];
-$role_id = $_POST['role_id'];
-$sort_order = $_POST['sort_order'];
-$activity_type_id = $_POST['activity_type_id'];
-$on_what_id = $_POST['on_what_id'];
-$on_what_table = $_POST['on_what_table'];
+$on_what_id             = $_POST['on_what_id'];
+$on_what_table          = $_POST['on_what_table'];
+$activity_title         = $_POST['title'];
+$start_delay            = $_POST['start_delay_days'] * 86400 +
+                          $_POST['start_delay_hrs'] * 3600 +
+                          $_POST['start_delay_mins'] * 60;
+$fixed_date             = $_POST['fixed_date'];
+$duration               = $_POST['duration_days'] * 86400 +
+                          $_POST['duration_hrs'] * 3600 +
+                          $_POST['duration_mins'] * 60;
+$activity_type_id       = $_POST['activity_type_id'];
+$role_id                = $_POST['role_id'];
+$sort_order             = $_POST['sort_order'];
 
 //set defaults if we didn't get everything we need
 if (strlen($activity_title) == 0) {
     $activity_title = ucwords(str_replace("_", " ", $on_what_table));
 }
 
-if (strlen($duration) == 0) {
-    $duration = 1;
+// Force a duration of 15 minute.
+// @TODO: Should be set at the default activity duration preference value instead.
+if ($duration == 0) {
+    $duration = 900;
 }
 
 $con = get_xrms_dbconnection();
 //$con->debug = 1;
 
-//get next sort_order value, put it at the bottom of the list
-$sql = "select sort_order from activity_templates
-        where on_what_id=$on_what_id
-        and on_what_table='$on_what_table'
-        order by sort_order desc";
+// Get the last sort_order value, so we can insert the new record after it
+$sql = "SELECT sort_order FROM activity_templates
+        WHERE on_what_id=$on_what_id
+        AND on_what_table='$on_what_table'
+        AND activity_template_record_status = 'a'
+        ORDER BY sort_order DESC";
 $rst = $con->execute($sql);
-$sort_order = $rst->rowcount() + 1;
+$sort_order = $rst->fields['sort_order'] + 1;
 $rst->close();
 
 //save to database
 $rec = array();
-$rec['activity_title'] = $activity_title;
-$rec['on_what_id'] = $on_what_id;
-$rec['on_what_table'] = $on_what_table;
+$rec['role_id'] = $role_id;
 $rec['activity_type_id'] = $activity_type_id;
+$rec['on_what_table'] = $on_what_table;
+$rec['on_what_id'] = $on_what_id;
+$rec['activity_title'] = $activity_title;
+$rec['activity_description'] = "";
+$rec['start_delay'] = $start_delay;
+$rec['fixed_date'] = $fixed_date;
 $rec['duration'] = $duration;
 $rec['sort_order'] = $sort_order;
-$rec['role_id'] = $role_id;
-$rec['activity_description'] = "";
 $tbl = "activity_templates";
 $ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
 $con->execute($ins);
 
 $activity_template_id=$con->Insert_ID();
 
-$on_what_table = str_replace("_", "-", $on_what_table);
-$table_name = substr($on_what_table, 0, strpos($on_what_table, "-"));
+$on_what_table_url = str_replace("_", "-", $on_what_table);
+$table_name = substr($on_what_table_url, 0, strpos($on_what_table_url, "-"));
 
-$return_url="/admin/$on_what_table/one.php?{$table_name}_status_id=$on_what_id";
+$return_url="/admin/$on_what_table_url/one.php?{$table_name}_status_id=$on_what_id";
 
 $sql = "SELECT activity_type_short_name FROM activity_types WHERE activity_type_id=$activity_type_id";
 $rst=$con->execute($sql);
@@ -73,16 +84,27 @@ if (!$rst) { db_error_handler($con, $sql); }
 
 $con->close();
 
+/*
+ * @TODO: The part of the code which handles activites of the type PRO (process)
+ * is, as of yet, incomplete
 if ((!$rst->EOF) AND ($rst->fields['activity_type_short_name']=='PRO')) {
     $return_url=urlencode($return_url);
     $msg=urlencode(_("Please select a workflow entity and workflow entity type for the new activity template"));
-    $return_url="/admin/activity-templates/edit.php?on_what_table=$on_what_table&on_what_id=$on_what_d&activity_template_id=$activity_template_id&msg=$msg&return_url=$return_url";
-} 
+    $return_url="/admin/activity-templates/edit.php?on_what_table=$on_what_table&on_what_id=$on_what_id&activity_template_id=$activity_template_id&msg=$msg&return_url=$return_url";
+}
+ * 
+ */
+
 //go back to the status edit page after updating
 header("Location: $http_site_root$return_url");
 
 /**
  * $Log: new.php,v $
+ * Revision 1.11  2010/11/24 22:39:37  gopherit
+ * Revised the store script for creating Templates attached to an Opportunity:
+ * - provided support for the new start_delay field which allows workflow activities to have gaps between them, measured in seconds by start_delay
+ * - finished the fixed_date functionality which lay dormant in the code base until now
+ *
  * Revision 1.10  2008/02/27 01:44:57  randym56
  * Turned off debug set by other developers.  Required updates to tables (/admin/updateto2.1.php) to fix errors.
  *
