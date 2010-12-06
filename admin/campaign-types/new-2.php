0057dd391cbce61d48143f0fcff0868815e20a05
gopherit <gopherit>
2010-12-06 22:18:54 +0000

    Added input validation to ensure that when a new workflow type is created, it has at least a *_type_short_name or a*_type_pretty_name.  If neither is provided, no record will be inserted in the database.

--- admin/campaign-types/new-2.php
+++ admin/campaign-types/new-2.php
@@ -18,20 +18,38 @@ $campaign_type_pretty_name = $_POST['campaign_type_pretty_name'];
 $campaign_type_pretty_plural = $_POST['campaign_type_pretty_plural'];
 $campaign_type_display_html = $_POST['campaign_type_display_html'];
 
-$con = get_xrms_dbconnection();
+// Only insert the record if we have at least a short or pretty name
+// @TODO: Should send a message to the user here giving them a clue if we are
+// doing nothing
+if ((strlen($campaign_type_short_name) > 0) OR (strlen($campaign_type_pretty_name) > 0)) {
 
-//save to database
-$rec = array();
-$rec['campaign_type_short_name'] = $campaign_type_short_name;
-$rec['campaign_type_pretty_name'] = $campaign_type_pretty_name;
-$rec['campaign_type_pretty_plural'] = $campaign_type_pretty_plural;
-$rec['campaign_type_display_html'] = $campaign_type_display_html;
+    // Set defaults if we didn't get everything we need
+    if (strlen($campaign_type_pretty_name) == 0) {
+        $campaign_type_pretty_name = $campaign_type_short_name;
+    }
+    if (strlen($campaign_type_pretty_plural) == 0) {
+        $campaign_type_pretty_plural = $campaign_type_pretty_name;
+    }
+    if (strlen($campaign_type_display_html) == 0) {
+        $campaign_type_display_html = $campaign_type_pretty_name;
+    }
 
-$tbl = "campaign_types";
-$ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
-$con->execute($ins);
+    $con = get_xrms_dbconnection();
 
-$con->close();
+    //save to database
+    $rec = array();
+    $rec['campaign_type_short_name'] = $campaign_type_short_name;
+    $rec['campaign_type_pretty_name'] = $campaign_type_pretty_name;
+    $rec['campaign_type_pretty_plural'] = $campaign_type_pretty_plural;
+    $rec['campaign_type_display_html'] = $campaign_type_display_html;
+
+    $tbl = "campaign_types";
+    $ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
+    $con->execute($ins);
+
+    $con->close();
+
+}
 
 header("Location: some.php");
 
--- admin/opportunity-types/new-2.php
+++ admin/opportunity-types/new-2.php
@@ -2,7 +2,7 @@
 /**
  * Insert a new opportunity type the database
  *
- * $Id: new-2.php,v 1.2 2006/01/02 21:59:08 vanmer Exp $
+ * $Id: new-2.php,v 1.3 2010/12/06 22:18:55 gopherit Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -19,25 +19,46 @@ $opportunity_type_pretty_name = $_POST['opportunity_type_pretty_name'];
 $opportunity_type_pretty_plural = $_POST['opportunity_type_pretty_plural'];
 $opportunity_type_display_html = $_POST['opportunity_type_display_html'];
 
-$con = get_xrms_dbconnection();
+// Only insert the record if we have at least a short or pretty name
+// @TODO: Should send a message to the user here giving them a clue if we are
+// doing nothing
+if ((strlen($opportunity_type_short_name) > 0) OR (strlen($opportunity_type_pretty_name) > 0)) {
 
-//save to database
-$rec = array();
-$rec['opportunity_type_short_name'] = $opportunity_type_short_name;
-$rec['opportunity_type_pretty_name'] = $opportunity_type_pretty_name;
-$rec['opportunity_type_pretty_plural'] = $opportunity_type_pretty_plural;
-$rec['opportunity_type_display_html'] = $opportunity_type_display_html;
+    // Set defaults if we didn't get everything we need
+    if (strlen($opportunity_type_pretty_name) == 0) {
+        $opportunity_type_pretty_name = $opportunity_type_short_name;
+    }
+    if (strlen($opportunity_type_pretty_plural) == 0) {
+        $opportunity_type_pretty_plural = $opportunity_type_pretty_name;
+    }
+    if (strlen($opportunity_type_display_html) == 0) {
+        $opportunity_type_display_html = $opportunity_type_pretty_name;
+    }
 
-$tbl = "opportunity_types";
-$ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
-$con->execute($ins);
+    $con = get_xrms_dbconnection();
 
-$con->close();
+    //save to database
+    $rec = array();
+    $rec['opportunity_type_short_name'] = $opportunity_type_short_name;
+    $rec['opportunity_type_pretty_name'] = $opportunity_type_pretty_name;
+    $rec['opportunity_type_pretty_plural'] = $opportunity_type_pretty_plural;
+    $rec['opportunity_type_display_html'] = $opportunity_type_display_html;
+
+    $tbl = "opportunity_types";
+    $ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
+    $con->execute($ins);
+
+    $con->close();
+
+}
 
 header("Location: some.php");
 
 /**
  * $Log: new-2.php,v $
+ * Revision 1.3  2010/12/06 22:18:55  gopherit
+ * Added input validation to ensure that when a new workflow type is created, it has at least a *_type_short_name or a*_type_pretty_name.  If neither is provided, no record will be inserted in the database.
+ *
  * Revision 1.2  2006/01/02 21:59:08  vanmer
  * - changed to use centralized database connection function
  *
