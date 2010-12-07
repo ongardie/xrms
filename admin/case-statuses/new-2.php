291eba82088f545920806c555c773e17974f843a
gopherit <gopherit>
2010-12-07 22:34:41 +0000

    The user can now specify the sort order of a newly created workflow status.  XRMS will also attempt to assign some reasonable values to pretty_name; if no sufficient input is supplied, no record will be inserted in the database.

--- admin/case-statuses/new-2.php
+++ admin/case-statuses/new-2.php
@@ -21,44 +21,63 @@ $case_status_display_html = $_POST['case_status_display_html'];
 $case_status_long_desc = $_POST['case_status_long_desc'];
 $status_open_indicator = $_POST['status_open_indicator'];
 $case_type_id = (int)$_POST['case_type_id'];
+$sort_order             = $_POST['sort_order'];
 
-//set defaults if we didn't get everything we need
-if (strlen($case_status_pretty_plural) == 0) {
-    $case_status_pretty_plural = $case_status_pretty_name;
+// Only insert the record if we have at least a short name or pretty name
+// @TODO: Should send a message to the user here giving them a clue if we are
+// doing nothing
+if ((strlen($case_status_short_name) > 0) OR (strlen($case_status_pretty_name))) {
+
+    //set defaults if we didn't get everything we need
+    if (strlen($case_status_pretty_name) == 0) {
+        $case_status_pretty_name = $case_status_short_name;
+    }
+    if (strlen($case_status_pretty_plural) == 0) {
+        $case_status_pretty_plural = $case_status_pretty_name;
+    }
+    if (strlen($case_status_display_html) == 0) {
+        $case_status_display_html = $case_status_pretty_name;
+    }
+
+    $con = get_xrms_dbconnection();
+
+    // It is useful to have $sort_order as a string so we can validate it here
+    if ($sort_order == '') {
+        // Get the last sort_order value so we can put the new record at the bottom of the list
+        $sql = "SELECT sort_order
+                FROM case_statuses
+                WHERE case_status_record_status='a'
+                AND case_type_id = $case_type_id
+                ORDER BY sort_order DESC";
+        $rst = $con->execute($sql);
+        if (!$rst) {
+            db_error_handler($con, $sql);
+        } else {
+            $sort_order = $rst->fields['sort_order'] + 1;
+            $rst->close();
+        }
+    } else {
+        $sort_order = (int)$sort_order;
+    }
+
+    //save to database
+    $rec = array();
+    $rec['case_status_short_name'] = $case_status_short_name;
+    $rec['case_status_pretty_name'] = $case_status_pretty_name;
+    $rec['case_status_pretty_plural'] = $case_status_pretty_plural;
+    $rec['case_status_display_html'] = $case_status_display_html;
+    $rec['case_status_long_desc'] = $case_status_long_desc;
+    $rec['status_open_indicator'] = $status_open_indicator;
+    $rec['case_type_id'] = $case_type_id;
+    $rec['sort_order'] = $sort_order;
+
+    $tbl = 'case_statuses';
+    $ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
+    $con->execute($ins);
+
+    $con->close();
 }
-if (strlen($case_status_display_html) == 0) {
-    $case_status_display_html = $case_status_pretty_name;
-}
-
-$con = get_xrms_dbconnection();
-
-// Get the last sort_order value so we can put the new record at the bottom of the list
-$sql = "SELECT sort_order
-        FROM case_statuses
-        WHERE case_status_record_status='a'
-        AND case_type_id=$case_type_id
-        ORDER BY sort_order DESC";
-$rst = $con->execute($sql);
-$sort_order = $rst->fields['sort_order'] + 1;
-
-//save to database
-$rec = array();
-$rec['case_status_short_name'] = $case_status_short_name;
-$rec['case_status_pretty_name'] = $case_status_pretty_name;
-$rec['case_status_pretty_plural'] = $case_status_pretty_plural;
-$rec['case_status_display_html'] = $case_status_display_html;
-$rec['case_status_long_desc'] = $case_status_long_desc;
-$rec['status_open_indicator'] = $status_open_indicator;
-$rec['case_type_id'] = $case_type_id;
-$rec['sort_order'] = $sort_order;
-
-$tbl = 'case_statuses';
-$ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
-$con->execute($ins);
-
-$con->close();
-
-//go back to the main case status page after updating
-header("Location: some.php?acase_type_id=$case_type_id");
+// Go back to the main case status page after updating
+header("Location: some.php?case_type_id=$case_type_id");
 
 ?>
--- admin/opportunity-statuses/new-2.php
+++ admin/opportunity-statuses/new-2.php
@@ -2,7 +2,7 @@
 /**
  * Insert a new opportunity status into the database
  *
- * $Id: new-2.php,v 1.12 2010/11/30 21:40:40 gopherit Exp $
+ * $Id: new-2.php,v 1.13 2010/12/07 22:34:41 gopherit Exp $
  */
 
 require_once('../../include-locations.inc');
@@ -21,48 +21,70 @@ $opportunity_status_display_html = $_POST['opportunity_status_display_html'];
 $opportunity_status_long_desc = $_POST['opportunity_status_long_desc'];
 $status_open_indicator = $_POST['status_open_indicator'];
 $opportunity_type_id = (int)$_POST['opportunity_type_id'];
+$sort_order             = $_POST['sort_order'];
 
-//set defaults if we didn't get everything we need
-if (strlen($opportunity_status_pretty_plural) == 0) {
-    $opportunity_status_pretty_plural = $opportunity_status_pretty_name;
+// Only insert the record if we have at least a short name or pretty name
+// @TODO: Should send a message to the user here giving them a clue if we are
+// doing nothing
+if ((strlen($opportunity_status_short_name) > 0) OR (strlen($opportunity_status_pretty_name))) {
+
+    //set defaults if we didn't get everything we need
+    if (strlen($opportunity_status_pretty_name) == 0) {
+        $opportunity_status_pretty_name = $opportunity_status_short_name;
+    }
+    if (strlen($opportunity_status_pretty_plural) == 0) {
+        $opportunity_status_pretty_plural = $opportunity_status_pretty_name;
+    }
+    if (strlen($opportunity_status_display_html) == 0) {
+        $opportunity_status_display_html = $opportunity_status_pretty_name;
+    }
+
+    $con = get_xrms_dbconnection();
+
+    // It is useful to have $sort_order as a string so we can validate it here
+    if ($sort_order == '') {
+        // Get the last sort_order value so we can put the new record at the bottom of the list
+        $sql = "SELECT sort_order
+                FROM opportunity_statuses
+                WHERE opportunity_status_record_status='a'
+                AND opportunity_type_id = $opportunity_type_id
+                ORDER BY sort_order DESC";
+        $rst = $con->execute($sql);
+        if (!$rst) {
+            db_error_handler($con, $sql);
+        } else {
+            $sort_order = $rst->fields['sort_order'] + 1;
+            $rst->close();
+        }
+    } else {
+        $sort_order = (int)$sort_order;
+    }
+
+    //save to database
+    $rec = array();
+    $rec['opportunity_status_short_name'] = $opportunity_status_short_name;
+    $rec['opportunity_status_pretty_name'] = $opportunity_status_pretty_name;
+    $rec['opportunity_status_pretty_plural'] = $opportunity_status_pretty_plural;
+    $rec['opportunity_status_display_html'] = $opportunity_status_display_html;
+    $rec['opportunity_status_long_desc'] = $opportunity_status_long_desc;
+    $rec['status_open_indicator'] = $status_open_indicator;
+    $rec['opportunity_type_id'] = $opportunity_type_id;
+    $rec['sort_order'] = $sort_order;
+
+    $tbl = 'opportunity_statuses';
+    $ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
+    $con->execute($ins);
+
+    $con->close();
 }
-if (strlen($opportunity_status_display_html) == 0) {
-    $opportunity_status_display_html = $opportunity_status_pretty_name;
-}
-
-$con = get_xrms_dbconnection();
-
-// Get the last sort_order value so we can put the new record at the bottom of the list
-$sql = "SELECT sort_order
-        FROM opportunity_statuses
-        WHERE opportunity_status_record_status='a'
-        AND opportunity_type_id = $opportunity_type_id
-        ORDER BY sort_order DESC";
-$rst = $con->execute($sql);
-$sort_order = $rst->fields['sort_order'] + 1;
-
-//save to database
-$rec = array();
-$rec['opportunity_status_short_name'] = $opportunity_status_short_name;
-$rec['opportunity_status_pretty_name'] = $opportunity_status_pretty_name;
-$rec['opportunity_status_pretty_plural'] = $opportunity_status_pretty_plural;
-$rec['opportunity_status_display_html'] = $opportunity_status_display_html;
-$rec['opportunity_status_long_desc'] = $opportunity_status_long_desc;
-$rec['status_open_indicator'] = $status_open_indicator;
-$rec['opportunity_type_id'] = $opportunity_type_id;
-$rec['sort_order'] = $sort_order;
-
-$tbl = 'opportunity_statuses';
-$ins = $con->GetInsertSQL($tbl, $rec, get_magic_quotes_gpc());
-$con->execute($ins);
-
-$con->close();
-
 // Go back to the main opportunity status page after updating
-header("Location: some.php?aopportunity_type_id=$opportunity_type_id");
+header("Location: some.php?opportunity_type_id=$opportunity_type_id");
 
 /**
  * $Log: new-2.php,v $
+ * Revision 1.13  2010/12/07 22:34:41  gopherit
+ * The user can now specify the sort order of a newly created workflow status.  XRMS will also attempt to assign some reasonable values to pretty_name; if no sufficient input is supplied, no record will be inserted in the database.
+ *
  * Revision 1.12  2010/11/30 21:40:40  gopherit
  * Removed trailing whitespace.
  *
