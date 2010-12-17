bb8e997d786f3742a91b3561222128ede3eea46f
gopherit <gopherit>
2010-12-17 21:29:05 +0000

    General code cleanup.

--- bulkactivity/bulkassignment-1.php
+++ bulkactivity/bulkassignment-1.php
@@ -2,7 +2,7 @@
 /**
 *
 *
-* $Id: bulkassignment-1.php,v 1.2 2007/10/19 18:31:34 randym56 Exp $
+* $Id: bulkassignment-1.php,v 1.3 2010/12/17 21:29:05 gopherit Exp $
 *
 */
 
@@ -19,78 +19,52 @@ require_once($include_directory . 'adodb-params.php');
 $session_user_id = session_check();
 $msg = $_GET['msg'];
 
+$return_url = $_POST['return_url'];
+
 $array_of_companies = $_POST['array_of_companies'];
 
-$return_url = $_POST['return_url'];
 $crm_status_id = $_POST['crm_status_id'];
+$company_type_id = $_POST['company_type_id'];
 $company_source_id = $_POST['company_source_id'];
+$credit_limit = $_POST['credit_limit'];
+$industry_id = $_POST['industry_id'];
+$account_status_id = $_POST['account_status_id'];
+$user_id = $_POST['user_id'];
+$rating_id = $_POST['rating_id'];
+
 $company_category_id = $_POST['company_category_id'];
 $unlink_category = $_POST['unlink_category'];
-$unlink_campaign = $_POST['unlink_campaign'];
-
-$company_type_id = $_POST['company_type_id'];
-$credit_limit = $_POST['credit_limit'];
-
-$industry_id = $_POST['industry_id'];
-$rating_id = $_POST['rating_id'];
-$user_id = $_POST['user_id'];
-$account_status_id = $_POST['account_status_id'];
-
-$custom1 = $_POST['custom1'];
-$custom2 = $_POST['custom2'];
-$custom3 = $_POST['custom3'];
-$custom4 = $_POST['custom4'];
 
 $campaign_id = $_POST['campaign_id'];
+$unlink_campaign = $_POST['unlink_campaign'];
 
-if (!($crm_status_id || $company_source_id || $company_category_id || $company_type_id
-      || $industry_id || $rating_id || $user_id || $account_status_id
-      || $custom1 || $custom2 || $custom3 || $custom4 || $campaign_id || $credit_limit)){
-    echo _("WARNING: No changes selected!") . "<br>";
+//$custom1 = $_POST['custom1'];
+//$custom2 = $_POST['custom2'];
+//$custom3 = $_POST['custom3'];
+//$custom4 = $_POST['custom4'];
+
+// Did the user supply any values?
+$update_company_record =   ($crm_status_id ||
+                            $company_type_id ||
+                            $company_source_id ||
+                            $credit_limit || 
+                            $industry_id || 
+                            $account_status_id || 
+                            $user_id || 
+                            $rating_id ||
+                            $company_category_id ||
+                            $campaign_id ||
+                            $custom1 || 
+                            $custom2 || 
+                            $custom3 || 
+                            $custom4);
+if (!$update_company_record){
+    $msg = urlencode(_("No changes selected!"));
+    header("Location: bulkassignment.php?return_url=$return_url&msg=$msg");
     exit;
 }
 
-$update_company_record = ($crm_status_id || $company_source_id || $company_type_id
-      || $industry_id || $rating_id || $user_id || $account_status_id
-      || $custom1 || $custom2 || $custom3 || $custom4 || $campaign_id || $credit_limit) ;
-
-/*
-echo $return_url;
-echo "<br>";
-echo "crm_status: "; echo $crm_status_id;
-echo "<br>";
-echo "company_source_id: "; echo $company_source_id;
-echo "<br>";
-echo "category_id: "; echo $company_category_id;
-echo "<br>";
-echo "company_type_id: "; echo $company_type_id;
-echo "<br>";
-echo "industry_id: "; echo $industry_id;
-echo "<br>";
-echo "rating_id: "; echo $rating_id;
-echo "<br>";
-echo "account_status_id: "; echo $account_status_id;
-echo "<br>";
-echo "user_id: "; echo $user_id;
-echo "<br>";
-echo $custom1;
-echo "<br>";
-echo $custom2;
-echo "<br>";
-echo $custom3;
-echo "<br>";
-echo $custom4;
-echo "<br>";
-echo $credit_limit;
-echo "<br>";
-echo $unlink_category;
-echo "<br>";
-echo $unlink_campaign;
-echo "<br>";
-exit;
-*/
-
-
+// Get the database connection
 $con = get_xrms_dbconnection();
 //$con->debug = 1;
 
@@ -99,102 +73,104 @@ if (is_array($array_of_companies)) {
 } elseif (is_numeric($array_of_companies)) {
     $imploded_contacts= $array_of_companies;
 }else {
-    echo _("WARNING: No array of companies!") . "<br>";
+    $msg = urlencode(_("No companies selected!"));
+    header("Location: bulkassignment.php?return_url=$return_url&msg=$msg");
+    exit;
 }
 
 // loop through the contacts and send each one a copy of the message
-$sql = "select * from companies where company_id in (" . $imploded_contacts . ")";
+$sql = "SELECT * FROM companies WHERE company_id IN (" . $imploded_contacts . ")";
 $rst = $con->execute($sql);
 
 if ($rst) {
 
-        while (!$rst->EOF)
-        {
+    while (!$rst->EOF) {
 
-            $company_id = $rst->fields['company_id'];
-            if (($campaign_id)&&(!$unlink_campaign)){
-               $rec1['company_id'] = $company_id;
-               $rec1['campaign_id'] = $campaign_id;
-               $tbl1 = 'company_campaign_map';
-               $ins1 = $con->GetInsertSQL($tbl1, $rec1, get_magic_quotes_gpc());
-               $con->execute($ins1);
+        $company_id = $rst->fields['company_id'];
+        if (($campaign_id)&&(!$unlink_campaign)){
+           $rec1['company_id'] = $company_id;
+           $rec1['campaign_id'] = $campaign_id;
+           $tbl1 = 'company_campaign_map';
+           $ins1 = $con->GetInsertSQL($tbl1, $rec1, get_magic_quotes_gpc());
+           $con->execute($ins1);
+        }
+        if (($campaign_id)&&($unlink_campaign)){
+           $del ="DELETE FROM company_campaign_map WHERE campaign_id = $campaign_id AND company_id = $company_id LIMIT 1";
+           //echo $del; echo "<br>";
+           $con->execute($del);
+           add_audit_item($con, $session_user_id, 'deleted', 'company_campaign_map', $campaign_id, $company_id);
+        }
+
+        $sql = "SELECT * FROM companies WHERE company_id = $company_id";
+        $rst1 = $con->execute($sql);
+
+        $rec = array();
+        $rec['last_modified_by'] = $session_user_id;
+        $rec['last_modified_at'] = mktime();
+        if ($crm_status_id) $rec['crm_status_id'] = $crm_status_id;
+        if ($company_source_id) $rec['company_source_id'] = $company_source_id;
+        if ($industry_id) $rec['industry_id'] = $industry_id;
+        if ($rating_id) $rec['rating_id'] = $rating_id;
+        if ($user_id) $rec['user_id'] = $user_id;
+        if ($credit_limit) $rec['credit_limit'] = $credit_limit;
+        if ($account_status_id) $rec['account_status_id'] = $account_status_id;
+        if ($custom1) $rec['custom1'] = $custom1;
+        if ($custom2) $rec['custom2'] = $custom2;
+        if ($custom3) $rec['custom3'] = $custom3;
+        if ($custom4) $rec['custom4'] = $custom4;
+        if ($company_type_id)  $rec['company_type_id'] = $company_type_id;
+        if (($company_category_id)&&(!$unlink_category)){
+
+            $rec1 = array();
+            $rec1['category_id'] = $company_category_id;
+            $rec1['on_what_table'] = 'companies';
+            $rec1['on_what_id'] = $company_id;
+            $tbl = 'entity_category_map';
+            $ins = $con->GetInsertSQL($tbl, $rec1, get_magic_quotes_gpc());
+            //echo $ins; exit;
+
+            $con->execute($ins);
+            add_audit_item($con, $session_user_id, 'created', 'entity_category_map', $company_category_id, $company_id);
+
+        }
+        if (($company_category_id)&&($unlink_category)){
+            $del ="DELETE FROM entity_category_map WHERE on_what_table ='companies' AND category_id = $company_category_id AND on_what_id = $company_id LIMIT 1";
+            //echo $del; echo "<br>";
+            $con->execute($del);
+            add_audit_item($con, $session_user_id, 'deleted', 'entity_category_map', $company_category_id, $company_id);
+        }
+        if ($update_company_record){
+            $upd = $con->GetUpdateSQL($rst1, $rec, false, get_magic_quotes_gpc());
+            //echo $upd; exit;
+            $sysst = $con->execute($upd);
+            if (!$sysst){
+                 //there was a problem, notify the user
+                 db_error_handler ($con, $upd);
             }
-            if (($campaign_id)&&($unlink_campaign)){
-               $del ="delete from company_campaign_map where campaign_id = $campaign_id and company_id = $company_id limit 1";
-               //echo $del; echo "<br>";
-               $con->execute($del);
-               add_audit_item($con, $session_user_id, 'deleted', 'company_campaign_map', $campaign_id, $company_id);
-            }
+            $param = array($rst1, $rec);
+            do_hook_function('company_edit_2', $param);
+            $accounting_rows = do_hook_function('company_accounting_inline_edit_2', $accounting_rows);
+            add_audit_item($con, $session_user_id, 'updated', 'companies', $company_id, 1);
+        }
+        $rst->movenext();
+    }   // WHILE
 
-            $sql = "SELECT * FROM companies WHERE company_id = $company_id";
-            $rst1 = $con->execute($sql);
-
-            $rec = array();
-            $rec['last_modified_by'] = $session_user_id;
-            $rec['last_modified_at'] = mktime();
-            if ($crm_status_id) $rec['crm_status_id'] = $crm_status_id;
-            if ($company_source_id) $rec['company_source_id'] = $company_source_id;
-            if ($industry_id) $rec['industry_id'] = $industry_id;
-            if ($rating_id) $rec['rating_id'] = $rating_id;
-            if ($user_id) $rec['user_id'] = $user_id;
-            if ($credit_limit) $rec['credit_limit'] = $credit_limit;
-            if ($account_status_id) $rec['account_status_id'] = $account_status_id;
-            if ($custom1) $rec['custom1'] = $custom1;
-            if ($custom2) $rec['custom2'] = $custom2;
-            if ($custom3) $rec['custom3'] = $custom3;
-            if ($custom4) $rec['custom4'] = $custom4;
-            if ($company_type_id)  $rec['company_type_id'] = $company_type_id;
-            if (($company_category_id)&&(!$unlink_category)){
-
-                $rec1 = array();
-                $rec1['category_id'] = $company_category_id;
-                $rec1['on_what_table'] = 'companies';
-                $rec1['on_what_id'] = $company_id;
-                $tbl = 'entity_category_map';
-                $ins = $con->GetInsertSQL($tbl, $rec1, get_magic_quotes_gpc());
-                //echo $ins; exit;
-
-                $con->execute($ins);
-                add_audit_item($con, $session_user_id, 'created', 'entity_category_map', $company_category_id, $company_id);
-
-            }
-            if (($company_category_id)&&($unlink_category)){
-                $del ="delete from entity_category_map where on_what_table ='companies' and category_id = $company_category_id and on_what_id = $company_id limit 1";
-                //echo $del; echo "<br>";
-                $con->execute($del);
-                add_audit_item($con, $session_user_id, 'deleted', 'entity_category_map', $company_category_id, $company_id);
-            }
-            if ($update_company_record){
-                $upd = $con->GetUpdateSQL($rst1, $rec, false, get_magic_quotes_gpc());
-                //echo $upd; exit;
-                $sysst = $con->execute($upd);
-                if (!$sysst){
-	             //there was a problem, notify the user
-	             db_error_handler ($con, $upd);
-                }
-                $param = array($rst1, $rec);
-                do_hook_function('company_edit_2', $param);
-                $accounting_rows = do_hook_function('company_accounting_inline_edit_2', $accounting_rows);
-                add_audit_item($con, $session_user_id, 'updated', 'companies', $company_id, 1);
-            }
-            $rst->movenext();
-        }   // WHILE
-
-        $rst->close();
-        $feedback = '<p /><b>' . _("Bulk update done") . '.</b>';
-    }
+    $rst->close();
+    $feedback = '<p /><b>' . _("Bulk update done") . '.</b>';
+} else {
     // Failed to create contact list
-    else
-    {
-        db_error_handler($con, $sql);
-    }
+    db_error_handler($con, $sql);
+}
 
-    $con->close();
+$con->close();
 
 header("Location: " . $http_site_root . $return_url);
 
 /**
  * $Log: bulkassignment-1.php,v $
+ * Revision 1.3  2010/12/17 21:29:05  gopherit
+ * General code cleanup.
+ *
  * Revision 1.2  2007/10/19 18:31:34  randym56
  * - Fixed bugs preventing bulk updates to companies
  *
--- bulkactivity/bulkassignment.php
+++ bulkactivity/bulkassignment.php
@@ -3,7 +3,7 @@
   *
   * bulkassignment.
   *
-  * $Id: bulkassignment.php,v 1.7 2009/01/23 00:19:31 randym56 Exp $
+  * $Id: bulkassignment.php,v 1.8 2010/12/17 21:29:05 gopherit Exp $
   */
 
   require_once('include-locations-location.inc');
@@ -23,21 +23,24 @@
   if (!$return_url) $return_url = '/' . $scope . '/some.php';
   //echo $scope; echo "<br>"; echo $return_url; exit;
 
-  //get the database connection
-  if (!$xcon) {
-      $con=get_xrms_dbconnection();
-  }
+  // Get the database connection
+  $con=get_xrms_dbconnection();
   //$con->debug = 1;
-  //get the menus
+
+  /////////////////// Get the UI Menus ///////////////////
+
+  // Get the CRM Status Menu
   $crm_status_menu = build_crm_status_menu($con, $crm_status_id, true);
+  // Get the Company Type Menu
   $company_type_menu = build_company_type_menu($con, $company_type_id, true);
 
-
+  // Get the Company Source Menu
   $sql2 = "select company_source_pretty_name, company_source_id from company_sources where company_source_record_status = 'a' order by company_source_pretty_name";
   $rst = $con->execute($sql2);
   $company_source_menu = $rst->getmenu2('company_source_id', $company_source_id, true);
   $rst->close();
 
+  // Get the Company Category Menu
   $sql2 = "select category_pretty_name, c.category_id
   from categories c, category_scopes cs, category_category_scope_map ccsm
   where c.category_id = ccsm.category_id
@@ -49,23 +52,28 @@
   $company_category_menu = $rst->getmenu2('company_category_id', $company_category_id, true);
   $rst->close();
 
+  // Get the Company Industry Menu
   $sql2 = "select industry_pretty_name, industry_id from industries where industry_record_status = 'a' order by industry_pretty_name";
   $rst = $con->execute($sql2);
   $industry_menu = translate_menu($rst->getmenu2('industry_id', $industry_id, true, false, 0, 'style="font-size: x-small; width: 150px; height: 20px"'));
   $rst->close();
 
+  // Get the Company Rating Menu
   $sql2 = "select rating_pretty_name, rating_id from ratings where rating_record_status = 'a' order by rating_pretty_name";
   $rst = $con->execute($sql2);
   $rating_menu = $rst->getmenu2('rating_id', $rating_id, true);
   $rst->close();
 
+  // Get the Company Account Status Menu
   $sql2 = "select account_status_pretty_name, account_status_id from account_statuses where account_status_record_status = 'a'";
   $rst = $con->execute($sql2);
   $account_status_menu = $rst->getmenu2('account_status_id', $account_status_id, true);
   $rst->close();
 
+  // Get the User Menu
   $user_menu = get_user_menu($con, $user_id, true, 'user_id', false);
 
+  // Get the Campaign Menu
   $sql2 = "select campaign_title, campaign_id from campaigns, campaign_statuses
          where campaign_record_status = 'a' and
          campaign_statuses.campaign_status_id = campaigns.campaign_status_id and
@@ -81,6 +89,7 @@
   $rst->close();
 
 
+  // Build the Company Search Results Array
   $array_of_companies = array();
 
   $from=$_SESSION["search_sql"]["from"];
@@ -108,10 +117,7 @@
   else
     echo _("WARNING: No array of companies!") . "<br>";
 
-  $con = get_xrms_dbconnection();
-  //$con->debug = 1;
-
-
+  // Buils the Company Search Results table row set
   $sql = "SELECT "
         . $con->Concat("'<a id=\"'" , "c.company_name", "'\" href=\"../companies/one.php?company_id='","c.company_id","'\">'","c.company_name","'</a>'") . ' AS "name",
         c.company_id, u.username, i.industry_pretty_name,
@@ -134,8 +140,7 @@
   if ($rst) {
       while (!$rst->EOF) {
         $contact_rows .= '<tr>';
-        $contact_rows .= '<td class="widget_content_form_element"></td><td>';
-        $contact_rows .= '<input type="checkbox" name="array_of_companies[]" id="array_of_companies_' . $_x++ . '" value="' . $rst->fields['company_id'] . '" checked="checked"></td>';
+        $contact_rows .= '<td class="widget_content_form_element"><input type="checkbox" name="array_of_companies[]" id="array_of_companies_' . $_x++ . '" value="' . $rst->fields['company_id'] . '" checked="checked"></td>';
         $contact_rows .= '<td class="widget_content">' . $rst->fields['name'] . '</td>';
         $contact_rows .= '<td class="widget_content">' . $rst->fields['industry_pretty_name'] . '</td>';
         $contact_rows .= '<td class="widget_content">' . $rst->fields['city'] . '</td>';
@@ -156,98 +161,99 @@
 
 ?>
 
+<div id="Main">
+    <form action="bulkassignment-1.php" method="post">
+    <input type=hidden name=return_url value="<?php  echo $return_url; ?>">
+    <input type=hidden name=user_id value="<?php  echo $user_id; ?>">
+    <table class="widget" cellspacing="1">
+    <tr>
+        <td class=widget_label_right><?php echo _("CRM Status"); ?></td>
+        <td class=widget_content_form_element><?php echo $crm_status_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Company Type"); ?></td>
+        <td class=widget_content_form_element><?php echo $company_type_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Company Source"); ?></td>
+        <td class=widget_content_form_element><?php echo $company_source_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Credit limit"); ?></td>
+        <td class=widget_content_form_element><input type=text size=10 name=credit_limit value="<?php echo $credit_limit; ?>"></td>
+    </tr>
+    <tr>
+        <td class=widget_label_right><?php echo _("Industry"); ?></td>
+        <td class=widget_content_form_element><?php echo $industry_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Account Status"); ?></td>
+        <td class=widget_content_form_element><?php echo $account_status_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Owner"); ?></td>
+        <td class=widget_content_form_element><?php echo $user_menu; ?></td>
+        <td class=widget_label_right><?php echo _("Rating"); ?></td>
+        <td class=widget_content_form_element><?php echo $rating_menu; ?></td>
+    </tr>
+    <!---
+    <tr>
+        <?php /* if ($company_custom1_label!='(Custom 1)') { ?>
+            <td class=widget_label_right><?php echo $company_custom1_label ?></td><td>
+            <?php echo ' <input type=text name=custom1 size=20 value=' . $custom1 . '>';} ?></td>
+        <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
 
-        <form action="bulkassignment-1.php" method="post">
-        <input type=hidden name=return_url value="<?php  echo $return_url; ?>">
-        <input type=hidden name=user_id value="<?php  echo $user_id; ?>">
-        <table class="widget" cellspacing="1">
+          <?php if ($company_custom2_label!='(Custom 2)') { ?>
+            <td class=widget_label_right><?php echo $company_custom2_label ?></td><td>
+            <?php echo ' <input type=text name=custom2 size=20 value=' . $custom2 . '>';} ?></td>
+        <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
+
+        <?php  if ($company_custom3_label!='(Custom 3)') { ?>
+            <td class=widget_label_right><?php echo $company_custom3_label ?></td><td>
+            <?php echo ' <input type=text name=custom3 size=20 value=' . $custom3 . '>';} ?></td>
+        <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
+
+        <?php  if ($company_custom4_label!='(Custom 4)') { ?>
+            <td class=widget_label_right><?php echo $company_custom4_label ?></td><td>
+            <?php echo ' <input type=text name=custom4 size=20 value=' . $custom4 . '>';} ?></td>
+        <?php } else {echo '<td class=clear>'; echo '</td><td>';} */ ?></td>
+
+    </tr>
+    --->
+    </table>
+    <table class="widget" cellspacing="1">
+    <tr>
+           <td class=widget_label style="width: 10%;"><?php echo _("Set category"); ?></td>
+           <td class=widget_content_form_element style="width: 20%;"><?php echo $company_category_menu; ?></td>
+           <td class=widget_label>
+                <input type=checkbox id=unlink_category name=unlink_category value=1>
+                <?php echo _("Select check box to unlink selected Companies from the Category"); ?>
+            </td>
+    </tr>
+    </table>
+
+    <table class="widget" cellspacing="1">
+    <tr>
+           <td class=widget_label style="width: 10%;"><?php echo _("Set campaign"); ?></td>
+           <td class=widget_content_form_element style="width: 20%;"><?php echo $campaign_menu; ?></td>
+           <td class=widget_label>
+                <input type=checkbox id=unlink_campaign name=unlink_campaign value=1>
+                <?php echo _("Select check box to unlink selected Companies from the Campaign"); ?>
+            </td>
+    </tr>
+    </table>
+    <table class="widget" cellspacing="1">
         <tr>
-        </tr>
-        </table>
-        <table class="widget" cellspacing="1">
-        <tr>
-            <td class=widget_label_right><?php echo _("CRM Status"); ?></td>
-            <td class=widget_content_form_element><?php echo $crm_status_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Company Type"); ?></td>
-            <td class=widget_content_form_element><?php echo $company_type_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Company Source"); ?></td>
-            <td class=widget_content_form_element><?php echo $company_source_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Credit limit"); ?></td>
-            <td class=widget_content_form_element><input type=text size=10 name=credit_limit value="<?php echo $credit_limit; ?>"></td>
+            <td class=widget_header colspan=6><?php echo _("Confirm Selection"); ?></td>
         </tr>
         <tr>
-            <td class=widget_label_right><?php echo _("Industry"); ?></td>
-            <td class=widget_content_form_element><?php echo $industry_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Account Status"); ?></td>
-            <td class=widget_content_form_element><?php echo $account_status_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Owner"); ?></td>
-            <td class=widget_content_form_element><?php echo $user_menu; ?></td>
-            <td class=widget_label_right><?php echo _("Rating"); ?></td>
-            <td class=widget_content_form_element><?php echo $rating_menu; ?></td>
+            <td class="widget_label"><?php echo _("Select"); ?></td>
+            <td class="widget_label"><?php echo _("Company"); ?></td>
+            <td class="widget_label"><?php echo _("Industry"); ?></td>
+            <td class="widget_label"><?php echo _("City"); ?></td>
+            <td class="widget_label"><?php echo _("State"); ?></td>
+            <td class="widget_label"><?php echo _("Country"); ?></td>
         </tr>
-        <!---
+        <?php  echo $contact_rows ?>
         <tr>
-            <?php /* if ($company_custom1_label!='(Custom 1)') { ?>
-                <td class=widget_label_right><?php echo $company_custom1_label ?></td><td>
-                <?php echo ' <input type=text name=custom1 size=20 value=' . $custom1 . '>';} ?></td>
-            <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
-
-	      <?php if ($company_custom2_label!='(Custom 2)') { ?>
-                <td class=widget_label_right><?php echo $company_custom2_label ?></td><td>
-                <?php echo ' <input type=text name=custom2 size=20 value=' . $custom2 . '>';} ?></td>
-            <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
-
-            <?php  if ($company_custom3_label!='(Custom 3)') { ?>
-                <td class=widget_label_right><?php echo $company_custom3_label ?></td><td>
-                <?php echo ' <input type=text name=custom3 size=20 value=' . $custom3 . '>';} ?></td>
-            <?php } else {echo '<td class=clear>'; echo '</td><td>';}  ?></td>
-
-            <?php  if ($company_custom4_label!='(Custom 4)') { ?>
-                <td class=widget_label_right><?php echo $company_custom4_label ?></td><td>
-                <?php echo ' <input type=text name=custom4 size=20 value=' . $custom4 . '>';} ?></td>
-            <?php } else {echo '<td class=clear>'; echo '</td><td>';} */ ?></td>
-
+            <td class="widget_content_form_element" colspan="6">
+                <input type="submit" class="button" value="<?php echo _("Continue"); ?>">
+                <input class="button" type="button" value="<?php echo _('Cancel'); ?>" onclick="location.href='<?php echo $http_site_root . $return_url; ?>'">
+            </td>
         </tr>
-        --->
-        </table>
-        <table class="widget" cellspacing="1">
-        <tr>
-               <td class=widget_label><?php echo _("Set category"); ?></td>
-               <td class=widget_content_form_element><?php echo $company_category_menu; ?>
-               <class=widget_label_right><?php echo _("Select check box to unlink selected Companies from the Category"); ?>
-               <class=widget_content_form_element><input type=checkbox id=unlink_category name=unlink_category value=1> </td>
-        </tr>
-        </table>
-
-        <table class="widget" cellspacing="1">
-        <tr>
-               <td class=widget_label><?php echo _("Set campaign"); ?></td>
-               <td class=widget_content_form_element><?php echo $campaign_menu; ?>
-               <class=widget_label_right><?php echo _("Select check box to unlink selected Companies from the Campaign"); ?>
-               <class=widget_content_form_element><input type=checkbox id=unlink_campaign name=unlink_campaign value=1> </td>
-        </tr>
-        </table>
-        <table class="widget" cellspacing="1">
-            <tr>
-                <td class=widget_header colspan=7><?php echo _("Confirm Selection"); ?></td>
-            </tr>
-            <tr>
-                <td class="widget_label">&nbsp;</td><td></td>
-                <td class="widget_label"><?php echo _("Company"); ?></td>
-                <td class="widget_label"><?php echo _("Industry"); ?></td>
-                <td class="widget_label"><?php echo _("City"); ?></td>
-                <td class="widget_label"><?php echo _("State"); ?></td>
-                <td class="widget_label"><?php echo _("Country"); ?></td>
-            </tr>
-            <?php  echo $contact_rows ?>
-            <tr>
-                <td class="widget_content_form_element" colspan="7">
-                    <input type="submit" class="button" value="<?php echo _("Continue"); ?>">
-                </td>
-            </tr>
-        </table>
-        </form>
-
+    </table>
+    </form>
+</div>
 
 <?php
 
@@ -255,6 +261,9 @@ end_page();
 
  /**
   * $Log: bulkassignment.php,v $
+  * Revision 1.8  2010/12/17 21:29:05  gopherit
+  * General code cleanup.
+  *
   * Revision 1.7  2009/01/23 00:19:31  randym56
   * Fix form format
   *
