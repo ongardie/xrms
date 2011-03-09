539ec25155617c6c6d5e3305dee5780cc77b2570
gopherit <gopherit>
2011-03-09 17:00:43 +0000

    FIXED Bug Artifact #3204309  When a contact is transfered, the user now has two choices:  - move all records with the contact to the new company  - leave all the records with another contact at the old company.  In this case, the activity will now also properly track its additional participants.

--- contacts/transfer-2.php
+++ contacts/transfer-2.php
@@ -2,7 +2,7 @@
 /**
  * Transfer a Contact to Another Company
  *
- * $Id: transfer-2.php,v 1.15 2010/08/17 22:09:04 gopherit Exp $
+ * $Id: transfer-2.php,v 1.16 2011/03/09 17:00:43 gopherit Exp $
  */
 
 require_once('include-locations-location.inc');
@@ -15,70 +15,131 @@ require_once($include_directory . 'adodb-params.php');
 
 $session_user_id = session_check();
 $msg = $_POST['msg'];
-$contact_id = $_POST['contact_id'];
-$company_name = $_POST['company_name'];
+$contact_id = (int)$_POST['contact_id'];
+$new_company_name = $_POST['company_name'];
 
 $con = get_xrms_dbconnection();
 // $con->debug = 1;
 
 // This query is done separately in case there is no current address
-$sql = "select * from contacts where contact_id = $contact_id";
+$sql = "SELECT  first_names, 
+                last_name,
+                company_id
+        FROM contacts
+        WHERE contact_id = $contact_id";
 $rst = $con->execute($sql);
 
 $contact_name = $rst->fields['first_names'] . ' ' . $rst->fields['last_name'];
-$company_id =  $rst->fields['company_id'];
 
-if($company_name) {
-    $sql = "SELECT company_name, company_id FROM companies
-            WHERE company_name LIKE ". $con->qstr(company_search_string($company_name),get_magic_quotes_gpc()) . " AND company_record_status = 'a' ORDER BY company_name";
-    $company_name_rst = $con->execute($sql);
+// Fetch the old company_id
+$old_company_id =  $rst->fields['company_id'];
+$rst->Close();
+
+// Fetch a list of the rest of the contacts from the old company
+$sql = 'SELECT '. $con->Concat('first_names',"' '",'last_name') ." AS contact_name, contact_id
+        FROM contacts
+        WHERE contact_record_status = 'a'
+            AND company_id = $old_company_id
+            AND contact_id <> $contact_id
+        ORDER BY last_name, first_names";
+$rst = $con->Execute($sql);
+
+if ( $rst AND !$rst->EOF ) {
+    $new_contact_menu = $rst->getmenu2('new_contact_id', FALSE, TRUE);
 } else {
-    $sql = "SELECT company_name, company_id FROM companies WHERE company_id = " . $company_id . " AND company_record_status = 'a'";
+    $new_contact_menu = FALSE;
 }
 
-if($company_name_rst && $company_name_rst->rowcount()) {
-    $company_menu = $company_name_rst->getmenu2('company_id', false, false);
-    $company_menu .= "&nbsp; <input type=button class=button value='More Info' onclick='document.forms[0].company_id.value=document.forms[1].company_id.options[document.forms[1].company_id.selectedIndex].value; document.forms[0].submit();'>";
-    $company_name_rst->close();
-    $con->close();
+if($new_company_name) {
+    $sql = 'SELECT company_name, company_id
+            FROM companies
+            WHERE ((company_name LIKE '. $con->qstr(company_search_string($new_company_name),get_magic_quotes_gpc()) .')
+                    OR (company_id = '. $con->qstr($new_company_name) ."))
+                AND company_record_status = 'a'
+            ORDER BY company_name";
+    $rst = $con->execute($sql);
 } else {
-    if (!$company_menu) {
-        $company_name=urlencode($company_name);
-        Header("Location: transfer.php?company_name=$company_name&contact_id=$contact_id&msg=".urlencode(_("No Companies Found, please try another search")));
-        exit;
-    }
+    Header("Location: transfer.php?contact_id=$contact_id&msg=".urlencode(_("Please enter a company name to search for")));
+    exit;
 }
-$page_title = $contact_name . " - " . _("Transfer to Another Company");
+
+if($rst AND !$rst->EOF) {
+    $new_company_menu = $rst->getmenu2('new_company_id', false, false);
+    $new_company_menu .= '&nbsp; <input type=button class=button value="More Info" onclick="document.forms[0].company_id.value=document.forms[\'TransferForm\'].company_id.options[document.forms[\'TransferForm\'].company_id.selectedIndex].value; document.forms[0].submit();">';
+    $rst->close();
+}
+$con->close();
+
+if (!$new_company_menu) {
+    $new_company_name=urlencode($new_company_name);
+    Header("Location: transfer.php?company_name=$new_company_name&contact_id=$contact_id&msg=".urlencode(_("No Companies Found, please try another search")));
+    exit;
+}
+
+$page_title = $contact_name . " - " . _("Transfer Contact to Another Company");
 start_page($page_title, true, $msg);
 
 ?>
 
+<script type="text/javascript">
+    function confirmSubmitTransfer() {
+        if (confirm('<?php echo _('You are about to modify all of the records attached to this contact') .'.\n\n'. _('Do you wish to continue') .'?'; ?>')) {
+            document.forms['TransferForm'].submit();
+        }
+    }
+
+    function validateTransferForm() {
+        if (document.forms['TransferForm']['transfer_mode'][0].checked) {
+            confirmSubmitTransfer();
+        } else if (document.forms['TransferForm']['transfer_mode'][1].checked
+                    && document.forms['TransferForm']['new_contact_id'].value > 0) {
+            confirmSubmitTransfer();
+        } else {
+            alert ('<?php echo _('Please select how you would like the contact records to be handled'); ?>');
+        }
+    }
+
+</script>
+
 <div id="Main">
     <div id="Content">
-
         <form action="<?php echo $http_site_root; ?>/companies/one.php" method=get target="_blank">
             <input type="hidden" name="company_id">
         </form>
-        <form action=transfer-3.php method=post>
+        <form action=transfer-3.php method=post id="TransferForm">
         <input type=hidden name=contact_id value=<?php echo $contact_id; ?>>
-        <input type=hidden name=old_company_id value=<?php echo $company_id; ?>>
+        <input type=hidden name=old_company_id value=<?php echo $old_company_id; ?>>
         <table class=widget cellspacing=1>
 
             <tr>
-                <td class=widget_header colspan=2><?php echo _("Transfer to Another Company"); ?></td>
+                <td class="widget_header" colspan="2"><?php echo _('Transfer') .' '. $contact_name .' '. _('to') .':'; ?></td>
             </tr>
 
             <tr>
-                <td class=widget_content><?php  echo ($company_menu) ? $company_menu : _("No companies found"); ?></td>
+                <td class="widget_content" colspan="2"><?php  echo ($new_company_menu) ? $new_company_menu : _("No companies found"); ?></td>
             </tr>
 
             <tr>
-                <td class=widget_content_form_element colspan=2>
-                    <?php if($company_menu) { ?>
-                        <input type="checkbox" name=everywhere /><font color="#FF0000"><strong><?php echo _("Also transfer all records attached to this contact to the new company"); ?></strong></font>&nbsp;&nbsp;
-                        <input class=button type=submit name=save value="<?php echo _("Save"); ?>">
-                    <?php } ?>
+                <td class="widget_header" colspan="2" style="font-weight: bold; color: red;">
+                    <?php echo _('Please select how you want the records attached to') .' '. $contact_name .' '. _('to be handled') .':'; ?>
                 </td>
+            </tr>
+
+            <tr>
+                <td class="widget_content_form_element">
+                    <input type="radio" name="transfer_mode" value="company"/><?php echo _("Move all records together with the contact to the new company"); ?>
+                </td>
+                <td class="widget_content_form_element"<?php if($new_contact_menu) { ?> rowspan="2"<?php } ?>>
+                    <input class=button type="button" name=save value="<?php echo _("Transfer"); ?>" onclick="return validateTransferForm();">
+                </td>
+            </tr>
+
+            <tr>
+                <?php if($new_contact_menu) { ?>
+                    <td class="widget_content_form_element">
+                        <input type="radio" name="transfer_mode" value="contact" /><?php echo _("Retain all records in the old company and transfer them to") .':'. $new_contact_menu; ?>
+                    </td>
+                <?php } ?>
             </tr>
 
         </table>
@@ -100,6 +161,11 @@ end_page();
 
 /**
  * $Log: transfer-2.php,v $
+ * Revision 1.16  2011/03/09 17:00:43  gopherit
+ * FIXED Bug Artifact #3204309  When a contact is transfered, the user now has two choices:
+ *  - move all records with the contact to the new company
+ *  - leave all the records with another contact at the old company.  In this case, the activity will now also properly track its additional participants.
+ *
  * Revision 1.15  2010/08/17 22:09:04  gopherit
  * Fixed Bug Artifact # 3047297: /contacts/transfer-2.php dies when the search string of the company we are looking to transfer to is blank or contains non-alpha characters.
  *
--- contacts/transfer-3.php
+++ contacts/transfer-3.php
@@ -2,7 +2,7 @@
 /**
  * Database updates for transfer contact
  *
- * $Id: transfer-3.php,v 1.8 2010/08/17 18:40:16 gopherit Exp $
+ * $Id: transfer-3.php,v 1.9 2011/03/09 17:00:43 gopherit Exp $
  */
 
 
@@ -10,73 +10,97 @@ require_once('../include-locations.inc');
 
 require_once($include_directory . 'vars.php');
 require_once($include_directory . 'utils-interface.php');
+require_once($include_directory . 'utils-activities.php');
 require_once($include_directory . 'utils-misc.php');
 require_once($include_directory . 'adodb/adodb.inc.php');
 require_once($include_directory . 'adodb-params.php');
 
 $session_user_id = session_check();
 
-$new_company_id = $_POST['company_id'];
-$old_company_id = $_POST['old_company_id'];
-$contact_id = $_POST['contact_id'];
+$old_company_id = (int)$_POST['old_company_id'];
+$contact_id     = (int)$_POST['contact_id'];
+$new_company_id = (int)$_POST['new_company_id'];
+$transfer_mode = $_POST['transfer_mode'];
 
-$everywhere = (isset($_POST['everywhere'])) ? TRUE : FALSE;
-
+// Get the database connection
 $con = get_xrms_dbconnection();
 // $con->debug = 1;
 
+// Prepare to transfer all the records of this contact to the new company or to another contact in the old company
+switch ($transfer_mode) {
+    case 'company':  // We are transfering the contact and its records to the new company
+        $update_record_fields = "company_id = $new_company_id";
+        break;
+    case 'contact':  // We are transfering the contact to a new company but we are transfering its records to a new contact in the old company
+        $new_contact_id = (int)$_POST['new_contact_id'];
+        $update_record_fields = "contact_id = $new_contact_id";
+        break;
+    default:
+        header("Location: one.php?contact_id=$contact_id&msg=". urlencode(_('ERROR') .': '. _('Cound not transfer contact')));
+        exit;
+        break;
+}
+
 // set the business address for this contact to the primary address of the new company.
 // not perfect, but the user will have a chance to change it from the contacts/one.php page
-$new_address_id = 0;
-$new_address_id = fetch_default_address($con, $new_company_id);
-
-$sql = "SELECT * FROM contacts WHERE contact_id = $contact_id";
-$rst = $con->execute($sql);
-
 $rec = array();
 $rec['company_id'] = $new_company_id;
-$rec['address_id'] = $new_address_id;
-$upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
-$con->execute($upd);
+$rec['address_id'] = fetch_default_address($con, $new_company_id);
+$con->AutoExecute('contacts', $rec, 'UPDATE', "contact_id = $contact_id");
 
+// Leave audit trail for the contact transfer
 add_audit_item($con, $session_user_id, 'transferred', 'contacts', $contact_id, 1);
+// Record the transfer in the contact_former_companies table
 add_contact_company_history($con, $contact_id, $old_company_id);
 
-if($everywhere) {
-    $sql = "SELECT * FROM activities WHERE contact_id = $contact_id";
-    $rst = $con->execute($sql);
+// Transfer all the records over
+if($transfer_mode) {
 
-    $rec = array();
-    $rec['company_id'] = $new_company_id;
-    $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
-    $con->execute($upd);
+    // If we are transferring the activities to a new contact, we should add the new contact as a participant to all of them
+    if($transfer_mode == 'contact') {
+        // We'll have to use the API add_activity_participant() method and do this one activity at a time
+        // to ensure we don't mess up the activity_participants table
+        $sql = "SELECT activity_id
+                FROM activities
+                WHERE contact_id = $contact_id";
+        $activity_id_arrays = $con->GetAll($sql);
 
-    $sql = "SELECT * FROM cases WHERE contact_id = $contact_id";
-    $rst = $con->execute($sql);
+        if (is_array($activity_id_arrays) AND count($activity_id_arrays)) {
+            foreach ($activity_id_arrays as $activity_id_array) {
+                add_activity_participant($con, $activity_id_array['activity_id'], $new_contact_id);
+            }
+        }
+    }
 
-    $rec = array();
-    $rec['company_id'] = $new_company_id;
-    $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
-    $con->execute($upd);
+    // Now proceed to update all the records
+    $records_tables = array( 'activities',
+                            'cases',
+                            'opportunities');
 
-    $sql = "SELECT * FROM opportunities WHERE contact_id = $contact_id";
-    $rst = $con->execute($sql);
+    foreach ($records_tables as $records_table) {
+        $sql = "UPDATE $records_table
+                SET $update_record_fields
+                WHERE contact_id = $contact_id";
+        $con->Execute($sql);
+    }
 
-    $rec = array();
-    $rec['company_id'] = $new_company_id;
-    $upd = $con->GetUpdateSQL($rst, $rec, false, get_magic_quotes_gpc());
-    $con->execute($upd);
 }
 
 do_hook('contact_transfer_3');
 
 $con->close();
 
-//now go to the Contact Address select  page so the user can make address changes
-$final_return_url="one.php?msg=saved&contact_id=$contact_id";
-$return_url="$http_site_root/companies/addresses.php?company_id=$new_company_id&edit_contact_id=$contact_id&final_return_url=".urlencode($final_return_url);
-header("Location: $return_url");
+$final_return_url="one.php?contact_id=$contact_id&msg=saved";
+if ($transfer_mode == 'company') {
+    // If we have transferred the contact to a new company, let the user make address changes, if necessary
+    $return_url="$http_site_root/companies/addresses.php?company_id=$new_company_id&edit_contact_id=$contact_id&final_return_url=".urlencode($final_return_url);
+    header("Location: $return_url");
+} else {
+    // Otherwise, send the user back to the contact page
+    header("Location: $final_return_url");
+}
 exit;
+    
 function add_contact_company_history($con, $contact_id, $company_id) {
     //save to database
     $rec = array();
@@ -91,6 +115,11 @@ function add_contact_company_history($con, $contact_id, $company_id) {
 }
 /**
  * $Log: transfer-3.php,v $
+ * Revision 1.9  2011/03/09 17:00:43  gopherit
+ * FIXED Bug Artifact #3204309  When a contact is transfered, the user now has two choices:
+ *  - move all records with the contact to the new company
+ *  - leave all the records with another contact at the old company.  In this case, the activity will now also properly track its additional participants.
+ *
  * Revision 1.8  2010/08/17 18:40:16  gopherit
  * Minor interface improvements
  *
